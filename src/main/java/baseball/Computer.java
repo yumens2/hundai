a1c60a5632c9f44129e1b0afc23bedf1895a6c0e
JaeBin <201924576@pusan.ac.kr>
2024-04-28 08:48:23 +0900

    fix(computer): change makeRandomNum() function public to private
    change makeRandomNum() function public to private cause this function must be use only in Computer

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -10,7 +10,7 @@ public class Computer {
         this.problemNum = makeRandomNum();
     }
 
-    public int makeRandomNum() {
+    private int makeRandomNum() {
         // 111 + random 값을 통해 111 ~ 999 숫자 중 0이 포함된 값을 제외한다
         return 111 + random.nextInt(889);
     }
