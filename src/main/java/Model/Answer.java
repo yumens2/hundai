1d12ff637a79a84b9b81834ed497256b51d55654
Park <pswaao88@gmail.com>
2024-04-27 23:07:15 +0900

    <feat>: Computer클래스 구현
    ìboolean배열을 이용해 중복을 체크 하면서 Math의 random함수를 통해 랜덤한 수 3자리를 만듦

--- src/main/java/Model/Answer.java
+++ src/main/java/Model/Answer.java
@@ -1,12 +1,12 @@
 package Model;
 
 public class Answer {
-    private int answerNum;
-    public Answer(int answerNum){
+    private int answerNum;//컴퓨터가 생성한 정답
+    public Answer(int answerNum){//생성자로 초기화
         this.answerNum = answerNum;
     }
 
-    public int getAnswerNum() {
+    public int getAnswerNum() {//getter로만 접근가능
         return answerNum;
     }
 }
