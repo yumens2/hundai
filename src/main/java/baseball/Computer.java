a7db020635ceac2900badb17cb124a868e3e8f21
JaeBin <201924576@pusan.ac.kr>
2024-04-28 08:44:14 +0900

    feat(computer): add getter to problem number
    add getter to problem number for later use

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -14,4 +14,8 @@ public class Computer {
         // 111 + random 값을 통해 111 ~ 999 숫자 중 0이 포함된 값을 제외한다
         return 111 + random.nextInt(889);
     }
+
+    public int getProblemNum() {
+        return problemNum;
+    }
 }
