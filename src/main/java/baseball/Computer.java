66b2a1432786e0a0673c11435a5e506b00188849
JaeBin <201924576@pusan.ac.kr>
2024-04-28 08:42:48 +0900

    feat(computer): init problem number
    initalize problem number by using makeRandomNum() function

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,17 @@
+package baseball;
+
+import java.util.Random;
+
+public class Computer {
+    private int problemNum;
+    private static final Random random = new Random();
+
+    public Computer() {
+        this.problemNum = makeRandomNum();
+    }
+
+    public int makeRandomNum() {
+        // 111 + random 값을 통해 111 ~ 999 숫자 중 0이 포함된 값을 제외한다
+        return 111 + random.nextInt(889);
+    }
+}
