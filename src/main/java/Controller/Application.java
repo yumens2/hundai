82a95b6cf16a200ace8c8a858ede75ce4b67eaca
Park <pswaao88@gmail.com>
2024-04-27 22:55:53 +0900

    <feat: 입출력 inputView ouputView작성 ëㄱreadme 이미지 등록
    ãinputView에 BufferedReader로 데이터 받는 메소드 작성
    outputView에 BufferedWriteããr로 데이터 출력 메소드 작성
    readme에 이미 ì지 추가

---
+++ src/main/java/Controller/Application.java
@@ -0,0 +1,7 @@
+package Controller;
+
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/View/InputView.java
@@ -0,0 +1,13 @@
+package View;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class InputView {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private InputView(){}
+    public static String Input()throws IOException {//입력받는 함수
+        return br.readLine();
+    }
+}
