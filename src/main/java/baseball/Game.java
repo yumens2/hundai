ce88197a7c90b6403dae3f8b1ff9c6584740280f
JaeBin <201924576@pusan.ac.kr>
2024-04-28 15:12:04 +0900

    feat(game): add game play function
    add game play function

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,12 +1,52 @@
 package baseball;
 
+import java.util.Scanner;
+
 public class Game {
     private User user;
     private Computer computer;
+    private final Scanner scanner;
 
-    public Game(User user, Computer computer) {
+    public Game(User user, Computer computer, Scanner scanner) {
         this.user = user;
         this.computer = computer;
+        this.scanner = scanner;
+    }
+
+    public void play() {
+        boolean option = true;
+        computer.initProblemNum();
+
+        while (option) {
+            System.out.print("숫자를 입력해 주세요 : ");
+            GameResult gameResult = getGameResult(user.makeGuess(), computer.getProblemNum());
+            printGameResult(gameResult);
+
+            if (gameResult.isCorrect())
+                option = getOption();
+        }
+    }
+
+    private boolean getOption() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
+                "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String input = scanner.nextLine();
+
+        boolean result;
+        switch (input) {
+            case "1" -> {
+                result = true;
+                computer.initProblemNum();
+            }
+            case "2" -> result = false;
+            default -> throw new IllegalArgumentException();
+        }
+
+        return result;
+    }
+
+    private void printGameResult(GameResult gameResult) {
+        System.out.println(gameResult);
     }
 
     private GameResult getGameResult(String guessNum, String answer) {
