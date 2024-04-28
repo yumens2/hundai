3dedb48fc91ea7249a8a929c2c46a6073629fd39
JaeBin <201924576@pusan.ac.kr>
2024-04-28 14:39:01 +0900

    fix(computer): change data type of problem number int to string
    change data type of problem number int to string

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,4 +1,26 @@
 package baseball;
 
 public class Game {
+    private User user;
+    private Computer computer;
+    private int answer;
+    private
+
+    public Game(User user, Computer computer) {
+        this.user = user;
+        this.computer = computer;
+        this.answer = computer.getProblemNum();
+    }
+
+    private GameResult getGameResult() {
+        int guessNum = user.makeGuess();
+        int ballCount = calculateBall(guessNum, answer);
+    }
+
+    private int calculateBall(String guessNum, String answer) {
+        int ballCount = 0;
+        for (int i = 0; i < guessNum.length(); i++) {
+
+        }
+    }
 }
