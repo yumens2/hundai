fd21734010b46f02d1613852e75d497b618d70af
JaeBin <201924576@pusan.ac.kr>
2024-04-28 12:07:35 +0900

    feat(user): add setter to guess number
    add setter to guess number

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -1,8 +1,16 @@
 package baseball;
 
+import java.util.Scanner;
+
 public class User {
     private int guessNum;
+    private final Scanner scanner;
 
-    public User() {
+    public User(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public void setGuessNum() {
+        this.guessNum = scanner.nextInt();
     }
 }
