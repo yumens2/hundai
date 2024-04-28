09f689f4c337ed849c70114f7745214e68e1656f
JaeBin <201924576@pusan.ac.kr>
2024-04-28 14:55:00 +0900

    feat(game): add get game result function
    add get game result function

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -3,24 +3,35 @@ package baseball;
 public class Game {
     private User user;
     private Computer computer;
-    private int answer;
-    private
 
     public Game(User user, Computer computer) {
         this.user = user;
         this.computer = computer;
-        this.answer = computer.getProblemNum();
     }
 
-    private GameResult getGameResult() {
-        int guessNum = user.makeGuess();
-        int ballCount = calculateBall(guessNum, answer);
-    }
-
-    private int calculateBall(String guessNum, String answer) {
+    private GameResult getGameResult(String guessNum, String answer) {
         int ballCount = 0;
-        for (int i = 0; i < guessNum.length(); i++) {
+        int strikeCount = 0;
 
+        for (int i = 0; i < guessNum.length(); ++i) {
+            if (isStrike(guessNum, answer, i))
+                ++strikeCount;
+            if (getBallCount(guessNum, answer, i))
+                ++ballCount;
         }
+
+        return new GameResult(ballCount, strikeCount);
+    }
+
+    private boolean isStrike(String guessNum, String answer, int i) {
+        return guessNum.charAt(i) == answer.charAt(i);
+    }
+
+    private boolean getBallCount(String guessNum, String answer, int i) {
+        for (int j = 0; j < guessNum.length(); ++j) {
+            if (i != j && guessNum.charAt(i) == answer.charAt(j))
+                return true;
+        }
+        return false;
     }
 }
