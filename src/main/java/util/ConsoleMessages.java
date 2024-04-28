e3fe1e7a8550f035a5a1ed09bac9efc344905adf
SeongHun <tjdgns5506@gmail.com>
2024-04-28 14:47:32 +0900

    feat: 'console에 출력되는 메세지 출력 기능' 구현

---
+++ src/main/java/util/ConsoleMessages.java
@@ -0,0 +1,25 @@
+package util;
+
+public class ConsoleMessages {
+    private ConsoleMessages() {
+    }
+
+    static public void requestNumberInput(){
+        System.out.print("숫자를 입력해 주세요 : ");
+    }
+    static public void ball(int ballCount){
+        System.out.print(ballCount+"볼 ");
+    }
+    static public void strike(int strikeCount){
+        System.out.print(strikeCount+"스트라이크");
+    }
+    static public void nothing(){
+        System.out.print("낫싱");
+    }
+    static public void gameClear(){
+        System.out.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+    static public void requestRestartGameOrExit(){
+        System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+}
