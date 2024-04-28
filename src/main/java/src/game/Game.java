744b0d7d7e86be9fe66effe0172ad976d311c1fc
Profile-exe <kang67572346@gmail.com>
2024-04-28 22:59:24 +0900

    refactor(Game): 게임 상태 관리 및 라운드 처리 로직 개선
    `Game` 클래스의 상태 관리 방식을 `Boolean`에서 `GameState` 열거형으로 변경해 상태를 더 명확하게 알 수 있게 함.
    
    게임 라운드의 처리를 별도의 `playGround` 메서드로 분리해 라운드 로직만 처리하도록 구현함.
    
     동시에 `start()` 메서드는 `while`구문 내부에서 `playground()`만 호출하며 요구사항에 맞도록 indent depth를 2 이하로 줄였음

--- src/main/java/src/game/Game.java
+++ src/main/java/src/game/Game.java
@@ -6,37 +6,41 @@ import static src.io.Input.*;
 
 public class Game {
 
-    private Boolean run = true;
-    private int randomNumber = generateRandomNumber();
+    private GameState state;
+    private int randomNumber;
+
+    public Game() {
+        resetGame();
+    }
+
+    private void resetGame() {
+        state = GameState.RESTART;
+        randomNumber = generateRandomNumber();  // 새로운 난수 생성
+    }
 
     public void start() {
-        while (run) {
-            try {
-                int userInput = getGuessNumberFromUser();
-
-                boolean isCorrect = compareNumber(randomNumber, userInput);
-
-                if (isCorrect) {
-                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                    run = restartOrExit();
-                }
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-                run = false;
-            }
+        while (state.isContinueGame()) {
+            playRound();
         }
     }
 
-    public boolean restartOrExit() {
-        switch (getGameOptionFromUser()) {
-            case RESTART -> {
-                randomNumber = generateRandomNumber();  // 새로운 난수 생성
-                return true;
+    private void playRound() {
+        try {
+            int userInput = getGuessNumberFromUser();
+
+            boolean isCorrect = compareNumber(randomNumber, userInput);
+
+            if (isCorrect) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                updateGameState();
             }
-            case EXIT -> {
-                return false;
-            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            state = GameState.EXIT;
         }
-        return false;
+    }
+
+    public void updateGameState() {
+        state = getGameStateFromUser();
     }
 }
