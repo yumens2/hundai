4f909391a32556a7faa6b085b2214a3356184fd7
HSHwan <tnghks2866@gmail.com>
2024-04-29 20:39:24 +0900

    feat: 게임 종료 문구 출력 기능 추가

--- src/main/java/BaseballPrinter.java
+++ src/main/java/BaseballPrinter.java
@@ -15,4 +15,9 @@ public class BaseballPrinter {
     public static void printNothingHintMessage() {
         System.out.print("낫싱");
     }
+
+    public static void printGameOverMessage() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
 }
