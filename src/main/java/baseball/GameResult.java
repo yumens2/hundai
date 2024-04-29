0aae5a9dbe788379e506360d77e2ef23ced4852b
JaeBin <201924576@pusan.ac.kr>
2024-04-29 13:22:27 +0900

    fix(game): fix game result when ball and strike is 0
    when ball and strike is 0, that should be print '낫싱' but it doesn't so, fix to print '낫싱'

--- src/main/java/baseball/GameResult.java
+++ src/main/java/baseball/GameResult.java
@@ -23,6 +23,9 @@ public class GameResult {
         if (strikeCount != 0)
             stringBuilder.append(strikeCount).append("스트라이크");
 
+        if (ballCount == 0 && strikeCount == 0)
+            stringBuilder.append("낫싱");
+
         return stringBuilder.toString();
     }
 }
