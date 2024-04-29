95f774d4fe3c7088864054c5d9d7b3378de0ba74
HSHwan <tnghks2866@gmail.com>
2024-04-29 21:33:31 +0900

    test: Computer getAnswerNumber 단위 테스트 추가

---
+++ src/test/java/ComputerTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ComputerTest {
+
+    Computer computer;
+    
+    @BeforeEach
+    void setUp() {
+        computer = new Computer();
+    }
+    
+    @Test
+    void getAnswerNumber() {
+        String answerNumber = computer.getAnswerNumber();
+        org.assertj.core.api.Assertions.assertThat(answerNumber.length()).isEqualTo(3);
+        Assertions.assertDoesNotThrow(() -> (Integer.parseInt(answerNumber)));
+        for (int i = 0; i < answerNumber.length(); i++) {
+            char currentDigit = answerNumber.charAt(i);
+            int digitCount = Math.toIntExact(answerNumber.chars()
+                    .filter((ch) -> (ch == currentDigit))
+                    .count());
+            org.assertj.core.api.Assertions.assertThat(digitCount).isEqualTo(1);
+        }
+    }
+}
