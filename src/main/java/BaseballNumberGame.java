5d9a205fe1047e817a788ab80921b177bded49ea
canyos <4581974@naver.com>
2024-04-29 16:29:51 +0900

    사용자 입력 변수 받는 selectNumber메서드 정의

--- src/main/java/BaseballNumberGame.java
+++ src/main/java/BaseballNumberGame.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class BaseballNumberGame extends Game {
     private int answer;
@@ -25,5 +26,19 @@ public class BaseballNumberGame extends Game {
         }while (random100 == random10 || random100 == random1 || random10 == random1 || random100 == 0 || random10 ==0 || random1 ==0);
 
         return answer;
+    }
+    private int selectNumber() throws IllegalArgumentException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("숫자를 입력해 주세요 : ");
+        int select=0;
+        try{
+            select = scanner.nextInt();
+        }catch(Exception e){
+            throw new IllegalArgumentException("입력을 확인하세요.");
+        }
+        if(select >999){
+            throw new IllegalArgumentException("3자리 숫자만 입력해주세요.");
+        }
+        return select;
     }
 }
