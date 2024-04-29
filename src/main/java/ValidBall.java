1fc663d384bb574ad51c946e57ae047072909ee0
capDoYeonLee <ghtyu21@naver.com>
2024-04-29 09:35:42 +0900

    feat: Valid Interface 구현 클래스

---
+++ src/main/java/ValidBall.java
@@ -0,0 +1,28 @@
+import java.util.HashSet;
+
+public class ValidBall implements Valid {
+    public ValidBall() {
+        super();
+    }
+
+    @Override
+    public void valid(int number) {
+
+        HashSet<Integer> set = new HashSet<>();
+
+        if (number < 111 && number > 999) {
+            throw new IllegalArgumentException("잘못된 범위의 입력입니다");
+        }
+
+        while (number > 0) {
+            int digit = number % 10;
+            if (!set.add(digit)) {
+                throw new IllegalArgumentException("입력된 숫자 중 중복되는 숫자가 있습니다.");
+            }
+            number /= 10;
+        }
+
+
+
+    }
+}
