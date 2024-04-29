bb1075a214caea48aa4c1f296a21bb96b83d9cd0
canyos <4581974@naver.com>
2024-04-29 16:28:20 +0900

    game클래스 상속하는 BaseballNumberGame 클래스 생성

---
+++ src/main/java/BaseballNumberGame.java
@@ -0,0 +1,14 @@
+public class BaseballNumberGame extends Game {
+    private int answer;
+    private int random100 = 0, random10 = 0, random1 = 0;
+
+    @Override
+    public void play() {
+
+    }
+
+    @Override
+    protected void printEndGame() {
+
+    }
+}
