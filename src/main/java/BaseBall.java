42b2cb0b49b45ef965814f54ee2d86c19f02bd72
capDoYeonLee <ghtyu21@naver.com>
2024-04-29 12:03:22 +0900

    fix: 요구사항 적용

--- src/main/java/BaseBall.java
+++ src/main/java/BaseBall.java
@@ -15,15 +15,25 @@ DIP(Dependency Inversion Principle): 의존 역전 원칙
 public class BaseBall {
 
     public static void main(String[] args) {
+        baseball();
+        while (true) {
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+            Scanner scanner = new Scanner(System.in);
+            int answer = scanner.nextInt();
+            if (answer == 1) {baseball();}
+            else if (answer == 2) {scanner.close(); break;}
+        }
+    }
+
+    private static void baseball() {
 
         Scanner scanner = new Scanner(System.in);
         PlayBall play = new PlayBall();
         RandomBall random = new RandomBall();
         ValidBall validBall = new ValidBall();
 
-
-        int number = random.makeRandomBall(); // 1 ~ 9 사이 서로 다른 수로 이루어진 세 자리 숫자 생성
-
+        int number = random.makeRandomBall();
 
         while (true) {
 
@@ -36,11 +46,10 @@ public class BaseBall {
             int ballCount = result.get(0);
             int strikeCount = result.get(1);
 
-            System.out.println("결과: " + ballCount + "볼, " + strikeCount + "스트라이크");
-            if (strikeCount == 3) {System.out.println("정답입니다!"); break;}
+            if ((ballCount==0)&&(strikeCount==0)) {System.out.println("낫싱");}
+            else {System.out.println("결과: " + ballCount + "볼, " + strikeCount + "스트라이크");}
 
+            if (strikeCount == 3) {System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료"); break;}
         }
-        scanner.close();
-
     }
 }
