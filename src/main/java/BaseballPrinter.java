7d5faf3b0d4abe1c2d9763a0d32b79e1ec28e745
HSHwan <tnghks2866@gmail.com>
2024-04-29 15:56:58 +0900

    feat: 볼카운트 힌트 문구 출력 기능 추가

--- src/main/java/BaseballPrinter.java
+++ src/main/java/BaseballPrinter.java
@@ -1,7 +1,18 @@
 public class BaseballPrinter {
 
-    public static void printNumericInputPhrase() {
+    public static void printNumericInputMessage() {
         System.out.print("숫자를 입력해 주세요 : ");
     }
 
+    public static void printBallCountHintMessage(int ballCount) {
+        System.out.print(ballCount + "볼 ");
+    }
+
+    public static void printStrikeCountHintMessage(int strikeCount) {
+        System.out.print(strikeCount + "스트라이크 ");
+    }
+
+    public static void printNothingHintMessage() {
+        System.out.print("낫싱");
+    }
 }
