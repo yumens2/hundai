b932232cff91d948fa892fc29ea5a0ca17e9f494
HSHwan <tnghks2866@gmail.com>
2024-04-29 14:23:05 +0900

    feat: 입력 검사 기능에서 에러 전달을 제외한 boolean 반환 제거

--- src/main/java/InputNumberValidator.java
+++ src/main/java/InputNumberValidator.java
@@ -1,21 +1,21 @@
 public class InputNumberValidator {
 
-    public static boolean isInputNumberCorrect(String inputNumber) {
-        return isStringLengthCorrect(inputNumber) && isStringNumeric(inputNumber) && isDifferentDigitNumber(inputNumber);
+    public static void isInputNumberCorrect(String inputNumber) {
+        isStringLengthCorrect(inputNumber);
+        isStringNumeric(inputNumber);
+        isDifferentDigitNumber(inputNumber);
     }
 
-    public static boolean isStringLengthCorrect(String string) throws IllegalStateException {
+    public static void isStringLengthCorrect(String string) throws IllegalStateException {
         if (string.length() != 3) {
             throw new IllegalStateException();
         }
-        return true;
     }
 
-    public static boolean isStringNumeric(String string) {
+    public static void isStringNumeric(String string) {
         for (int i = 0; i < string.length(); i++) {
             isCharacterNumeric(string, i);
         }
-        return true;
     }
 
     public static void isCharacterNumeric(String string, int index) throws IllegalStateException {
@@ -24,13 +24,12 @@ public class InputNumberValidator {
         }
     }
 
-    public static boolean isDifferentDigitNumber(String number) {
+    public static void isDifferentDigitNumber(String number) {
         for (int i = 0; i < number.length(); i++) {
             if (getDigitCount(number, number.charAt(i)) != 1) {
                 throw new IllegalStateException();
             }
         }
-        return true;
     }
 
     public static int getDigitCount(String number, char digit) {
