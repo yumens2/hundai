634726d4402d4ab5fba424d9e15ee3974ca24693
HSHwan <tnghks2866@gmail.com>
2024-04-29 22:44:39 +0900

    refactor: 불필요 변수 선언 및 close문 삭제

--- src/main/java/InputNumberReceiver.java
+++ src/main/java/InputNumberReceiver.java
@@ -4,9 +4,6 @@ public class InputNumberReceiver {
 
     public static String getInputNumber() {
         Scanner scanner = new Scanner(System.in);
-        String inputNumber = scanner.next();
-        scanner.close();
-        return inputNumber;
+        return scanner.next();
     }
-
 }
