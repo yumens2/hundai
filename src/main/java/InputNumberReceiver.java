e5f46169e448b5601895a7a8402e674b630da386
HSHwan <tnghks2866@gmail.com>
2024-04-29 15:09:44 +0900

    refactor: scanner close하여 resource lear 방지

--- src/main/java/InputNumberReceiver.java
+++ src/main/java/InputNumberReceiver.java
@@ -5,6 +5,8 @@ public class InputNumberReceiver {
     public String getInputNumber() throws IllegalStateException {
         Scanner scanner = new Scanner(System.in);
         String inputNumber = scanner.next();
+        scanner.close();
+
         InputNumberValidator.isInputNumberCorrect(inputNumber);
         return inputNumber;
     }
