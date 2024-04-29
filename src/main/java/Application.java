431e4ed5ce65d0eb5fca7e1f0ae195f113476f64
HSHwan <tnghks2866@gmail.com>
2024-04-29 21:26:52 +0900

    feat: 숫자 야구 게임 애플리케이션 추가

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,9 @@
 public class Application {
 
     public static void main(String[] args) {
-
+        do {
+            BaseballGame baseballGame = new BaseballGame();
+            baseballGame.playBall();
+        } while (GameRestartOption.isGameRestart());
     }
 }
