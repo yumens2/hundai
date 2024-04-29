2ccb5deb8a4eb4363fb38fc2323534dd8070b055
HSHwan <tnghks2866@gmail.com>
2024-04-29 19:38:16 +0900

    test: 랜덤 한 자리 수 생성 메서드 단위 테스트 삭제 및 static 메서드 변경으로 인한 코드 변경

--- src/test/java/RandomNumberGeneratorTest.java
+++ src/test/java/RandomNumberGeneratorTest.java
@@ -1,21 +1,14 @@
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
 class RandomNumberGeneratorTest {
-    RandomNumberGenerator randomNumberGenerator;
-
-    @BeforeEach
-    void settings() {
-        randomNumberGenerator = new RandomNumberGenerator();
-    }
 
     @Test
     void getRandomNumber() {
-        String randomNumber = randomNumberGenerator.getRandomNumber();
+        String randomNumber = RandomNumberGenerator.getRandomNumber();
         org.assertj.core.api.Assertions.assertThat(randomNumber.length()).isEqualTo(3);
         Assertions.assertDoesNotThrow(() -> (Integer.parseInt(randomNumber)));
         for (int i = 0; i < randomNumber.length(); i++) {
@@ -25,12 +18,5 @@ class RandomNumberGeneratorTest {
                     .count());
             org.assertj.core.api.Assertions.assertThat(digitCount).isEqualTo(1);
         }
-    }
-
-    @Test
-    void appendRandomSingleDigit() {
-        List<String> randomNumberList = new ArrayList<>();
-        randomNumberGenerator.appendRandomSingleDigit(randomNumberList);
-        Assertions.assertTrue(Character.isDigit(randomNumberList.get(0).charAt(0)) && randomNumberList.size() == 1);
     }
 }
