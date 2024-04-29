6e6bd8355ef16e3d5c49d091c39b08fb16095d88
geon <kky8586696@naver.com>
2024-04-29 16:09:36 +0900

    feat: User 값 입력 기능 추가

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -17,5 +17,16 @@ public class User {
         }
 
     }
+    public static List<Integer> createInputNum(){
+        String inputnum;
+        List<Integer> userInput = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        inputnum = sc.nextLine();
+        isValidInputNum(inputnum);
+        for(String num: inputnum.split("")){
+            userInput.add(Integer.parseInt(num));
+        }
+        return userInput;
 
+    }
 }
