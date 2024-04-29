27a3cf942e9e60ca7200baa08878ad72600b8169
Kim Sanghae <pelikan@pusan.ac.kr>
2024-04-29 16:43:11 +0900

    feat(Application): add function that make computer's number randomly

---
+++ src/main/java/Application.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Application {
+    public static void main(String[] args) {
+
+        NumberGenerating numberGenerating = new NumberGenerating();
+
+        // TODO: 숫자 랜덤하게 생성
+        ArrayList<Integer> computerNumber = numberGenerating.generateRandomNumber();
+
+        // TODO: 사용자가 숫자 입력
+
+        // TODO: 입력받은 숫자로 야구 결과 알기
+
+
+    }
+}
