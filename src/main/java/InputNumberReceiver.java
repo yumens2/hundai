63bb14ca0af49c240a6870770fdb7f9ecddc4e56
HSHwan <tnghks2866@gmail.com>
2024-04-29 13:48:15 +0900

    modify: 숫자 입력 기능 수정

--- src/main/java/InputNumberReceiver.java
+++ src/main/java/InputNumberReceiver.java
@@ -4,8 +4,7 @@ public class InputNumberReceiver {
 
     public String getInputNumber() throws IllegalStateException {
         Scanner scanner = new Scanner(System.in);
-        String inputNumber;
-        inputNumber = scanner.nextLine();
+        String inputNumber = scanner.next();
         return inputNumber;
     }
 }
