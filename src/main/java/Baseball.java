78c9437e210984297ace0d4443d5664f7fc5ac13
MGPOCKY <kkm1447@naver.com>
2024-04-29 17:22:36 +0900

    feat: Implement guess feature

--- src/main/java/Baseball.java
+++ src/main/java/Baseball.java
@@ -16,4 +16,37 @@ public class Baseball {
     }
     return answerNums;
   }
+
+  private static void printGuessResult(Integer strike, Integer ball) {
+    if (strike == 0 && ball == 0) {
+      System.out.println("낫싱");
+    } else if (strike == 0) {
+      System.out.println(ball + " 볼");
+    } else if (ball == 0) {
+      System.out.println(strike + " 스트라이크");
+    } else {
+      System.out.println(ball + " 볼, " + strike + " 스트라이크");
+    }
+    if (strike == NUM_LEN) {
+      System.out.println(NUM_LEN + "개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+  }
+
+  public static boolean guess(Integer[] guessIntegers, Integer[] answerIntegers) {
+    Integer strike = 0;
+    Integer ball = 0;
+    for (int index = 0; index < NUM_LEN; index++) {
+      if (answerIntegers[index] == guessIntegers[index]) {
+        strike++;
+      } else if (Arrays.asList(answerIntegers).contains(guessIntegers[index])) {
+        ball++;
+      }
+    }
+    printGuessResult(strike, ball);
+
+    if (strike == NUM_LEN) {
+      return true;
+    }
+    return false;
+  }
 }
