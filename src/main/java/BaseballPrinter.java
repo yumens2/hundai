97746fb4dd44adf4a4fc6108ff47587df7c0a767
HSHwan <tnghks2866@gmail.com>
2024-04-29 21:13:09 +0900

    feat: 줄바꿈 기능 추가

--- src/main/java/BaseballPrinter.java
+++ src/main/java/BaseballPrinter.java
@@ -16,6 +16,10 @@ public class BaseballPrinter {
         System.out.print("낫싱");
     }
 
+    public static void printLineBreak() {
+        System.out.println();
+    }
+
     public static void printGameOverMessage() {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
