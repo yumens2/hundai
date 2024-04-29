1726f90a2ea1ed2a3da90e8828ad03185caace51
김수랑 <we6610@gmail.com>
2024-04-29 11:24:14 +0900

    indent 최소화, 가독성 최대화, 주석 추가

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -3,6 +3,7 @@ package baseball;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class BaseballGame {
 
@@ -20,10 +21,11 @@ public class BaseballGame {
         while (!endGame) {
 
             int[] userThreeNum = new int[3]; //유저가 입력한 수를 저장할 배열
-            Map<String, Integer> stkAndBall = new HashMap<>(); //strike와 ball count를 저장할 map
 
+            Map<String, Integer> stkAndBall = new HashMap<>(); //strike와 ball count를 저장할 map
             stkAndBall.put("스트라이크", 0);
             stkAndBall.put("볼", 0);
+
             System.out.print("숫자를 입력해 주세요 : ");
 
             try {
@@ -39,18 +41,7 @@ public class BaseballGame {
                 if (stkAndBall.get("스트라이크") == 3) { //3 스트라이크 라면
                     System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                     System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-
-                    int i = input.nextInt();
-                    input.nextLine();
-
-                    if (i < 1 || i > 2) { //유저가 올바른 수를 입력했는지 판별
-                        throw new IllegalArgumentException();
-                    }
-                    if (i == 1) { //1이면 새로 시작하기 위해 랜덤으로 다시 뽑음
-                        setRandomThreeNum(computerThreeNum);
-                    } else { // 2면 끝내기
-                        endGame = true;
-                    }
+                    endGame = isEndGame(input);
 
                 } else { // 3스트라이크가 아니라면
                     printSBmap(stkAndBall); //몇 볼, 몇 스트라이크 인지 출력
@@ -60,24 +51,71 @@ public class BaseballGame {
                 System.out.println("잘못된 수를 입력하였습니다.");
                 break;
             }
-
-
         }
-
     }
 
+    private void setRandomThreeNum(int[] threeNum) {
+        threeNum[0] = (int) (Math.random() * 9 + 1);
+        for (int index = 1; index < 3; ) {
+            int randomInt = (int) (Math.random() * 9 + 1);
+            if (!haveSameElement(threeNum, randomInt, index)) { // 랜덤 수는 서로 다른수여야 함
+                threeNum[index] = randomInt;
+                index++;
+            }
+        }
+    }
     private void inputToInt(int[] userThreeNum, String s) {
         userThreeNum[0] = Integer.parseInt(String.valueOf(s.charAt(0)));
 
         for (int index = 1; index < 3; index++) {
             int usersIthInt = Integer.parseInt(String.valueOf(s.charAt(index)));
-
             if (haveSameElement(userThreeNum, usersIthInt, index)) { //인풋은 서로 다른수여야 함
                 throw new IllegalArgumentException();
             } else {
                 userThreeNum[index] = usersIthInt;
             }
         }
+    }
+
+    private boolean isIllegalArgument(String str) {
+        try {
+            if (str.length() != 3) { //인풋의 길이는 3이여야 함
+                return true;
+            }
+
+            for (int i = 0; i < 3; i++) { //각각의 숫자는 1~9까지여야 함
+                if (str.charAt(i) < '1' || str.charAt(i) > '9') {
+                    return true;
+                }
+            }
+            return false;
+        } catch (RuntimeException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean haveSameElement(int[] threeNum, int Num, int index) {
+        for (int i = 0; i < index; i++) {
+            if (threeNum[i] == Num) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void countStkAndBall(Map<String, Integer> map, int[] computerThreeNum, int[] userThreeNum) {
+        int strike = 0;
+        int ball = 0;
+        for (int i = 0; i < 3; i++) {
+            int userIth = userThreeNum[i];
+            if (computerThreeNum[i] == userIth) {
+                strike++;
+            } else if (IntStream.of(computerThreeNum).anyMatch(x -> x == userIth)) { //스트라이크가 아니면서 매치되는 것이 하나라도 있을 때
+                ball++;
+            }
+        }
+        map.put("스트라이크", strike);
+        map.put("볼", ball);
     }
 
     private void printSBmap(Map<String, Integer> map) {
@@ -95,58 +133,19 @@ public class BaseballGame {
             }
         }
     }
+    private boolean isEndGame(Scanner input) {
+        boolean endGame = false;
+        int i = input.nextInt();
+        input.nextLine();
 
-
-    private void countStkAndBall(Map<String, Integer> map, int[] computerThreeNum, int[] userThreeNum) {
-        for (int userIdx = 0; userIdx < 3; userIdx++) {
-            for (int comIdx = 0; comIdx < 3; comIdx++) {
-                if (userThreeNum[userIdx] == computerThreeNum[comIdx]) {
-                    if (userIdx == comIdx) {
-                        map.replace("스트라이크", map.get("스트라이크") + 1);
-                    } else {
-                        map.replace("볼", map.get("볼") + 1);
-                    }
-                }
-            }
-        }
-    }
-
-
-    private boolean isIllegalArgument(String str) {
-        try {
-            if (str.length() != 3) { //인풋의 길이는 3이여야 함
-                return true;
-            }
-
-            for (int i = 0; i < 3; i++) { //각각의 숫자는 1~9까지여야 함
-                if (str.charAt(i) < '1' || str.charAt(i) > '9') {
-                    return true;
-                }
-            }
-            return false;
-        } catch (Exception e) {
+        if (i < 1 || i > 2) { //유저가 올바른 수를 입력했는지 판별
             throw new IllegalArgumentException();
         }
-    }
-
-
-    private void setRandomThreeNum(int[] threeNum) {
-        threeNum[0] = (int) (Math.random() * 9 + 1);
-        for (int index = 1; index < 3; ) {
-            int randomInt = (int) (Math.random() * 9 + 1);
-            if (!haveSameElement(threeNum, randomInt, index)) { // 랜덤 수는 서로 다른수여야 함
-                threeNum[index] = randomInt;
-                index++;
-            }
+        if (i == 1) { //1이면 새로 시작하기 위해 랜덤으로 다시 뽑음
+            setRandomThreeNum(computerThreeNum);
+        } else { // 2면 끝내기
+            endGame = true;
         }
-    }
-
-    private boolean haveSameElement(int[] threeNum, int Num, int index) {
-        for (int i = 0; i < index; i++) {
-            if (threeNum[i] == Num) {
-                return true;
-            }
-        }
-        return false;
+        return endGame;
     }
 }
