bd50b9e1f6b218b8d0f1211e97e9e1121b1472a6
momnpa333 <momnpa333@gmail.com>
2024-04-30 15:16:55 +0900

    feat(GamePackConfig.java): 싱글톤 구현및 게임팩 init 기능

---
+++ src/main/java/global/config/GamePackConfig.java
@@ -0,0 +1,40 @@
+package global.config;
+
+import game.gamePack.BaseballGame.BaseballGame;
+import game.gamePack.GamePack;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GamePackConfig {
+
+    private static final List<GamePack> gamePacks = new ArrayList<>();
+
+    /**
+     * 생성 제한
+     */
+    private GamePackConfig() {
+    }
+
+    /**
+     * 사용가능한 게임팩 init
+     */
+    private void initGamePacks() {
+        gamePacks.add(new BaseballGame());
+    }
+
+    /**
+     * 동시성 이슈 해결을 위한 synchronized
+     */
+    public static synchronized List<GamePack> getGamePacks() {
+        /**
+         * gamePacks가 비어있다면 최초로 한번만 init
+         */
+        if (gamePacks.isEmpty()) {
+            GamePackConfig gamePackConfig = new GamePackConfig();
+            gamePackConfig.initGamePacks();
+        }
+        return gamePacks;
+    }
+
+
+}
