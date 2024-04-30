da5909aa10996175404ff33c17d39a2040660cbb
ajy4304 <ajy4304@naver.com>
2024-04-30 13:16:02 +0900

    feat(User): 생성자, getUserInputNumbers() 메서드 추가
    사용자에게 숫자를 입력받고 사용자의 입력값을 반환하여 추가 검증할 수 있는 메서드 작성

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,2 +1,15 @@
+import java.util.Scanner
 public class User {
+    private Scanner scanner;
+    private String useNumbers;
+    public User(){
+        scanner = new Scanner(System.in);
+    }
+
+    public String getUserInputNumbers(){
+        System.out.println("숫자를 입력해 주세요 : ");
+        useNumbers = scanner.nextLine();
+        return useNumbers;
+    }
+
 }
