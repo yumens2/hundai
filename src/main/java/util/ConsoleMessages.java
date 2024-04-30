c15a52cc8918f7e546784185675fd9b9196f49a6
SeongHun <tjdgns5506@gmail.com>
2024-04-30 16:17:29 +0900

    fix: 개행추가 System.out.print -> ~.println

--- src/main/java/util/ConsoleMessages.java
+++ src/main/java/util/ConsoleMessages.java
@@ -17,9 +17,9 @@ public class ConsoleMessages {
         System.out.print("낫싱");
     }
     static public void gameClear(){
-        System.out.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
     static public void requestRestartGameOrExit(){
-        System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 }
