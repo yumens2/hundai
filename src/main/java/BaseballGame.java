c3a4c288d1b3bcc2ee9827ce5430b77d7bdf4413
pjhcsols <pjhcsols@naver.com>
2024-04-30 20:36:03 +0900

    Feat: App & BaseballGame class생성 & startGame 로직 생성[박한솔]

---
+++ src/main/java/BaseballGame.java
@@ -0,0 +1,13 @@
+
+
+public class BaseballGame {
+    public void startGame() {
+        boolean playAgain = true;
+        while (playAgain) {
+            //playGame();
+            //playAgain = askToPlayAgain();
+        }
+        System.out.println("게임 종료");
+    }
+
+}
