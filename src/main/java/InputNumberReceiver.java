abe071b641872bac846c582c6b492aa7bd92641c
HSHwan <tnghks2866@gmail.com>
2024-04-30 16:36:20 +0900

    style: 메서드명 추상화로 인한 수정

--- src/main/java/InputNumberReceiver.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class InputNumberReceiver {
-
-    public static String getInputNumber() {
-        Scanner scanner = new Scanner(System.in);
-        return scanner.next();
-    }
-}
