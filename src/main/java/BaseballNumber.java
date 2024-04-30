ea85f176ce9e9f82ef10bca098f59ae5f935daad
alsswl <alswl4223@naver.com>
2024-04-30 22:51:31 +0900

    feat : 난수 3개를 만들고 이를 리스트에 저장한다.

--- src/main/java/BaseballNumber.java
+++ src/main/java/BaseballNumber.java
@@ -3,4 +3,17 @@ public class BaseballNumber {
     private List<Integer> randomNum;
     private List<Integer> userNum;
 
+    BaseballNumber() {
+        randomNum = new ArrayList<>();
+        userNum = new ArrayList<>();
+    }
+
+    public void setUserNum(List<Integer> userNum) {
+        this.userNum = userNum;
+    }
+
+    public void setRandomNum(List<Integer> randomNum) {
+        this.randomNum = randomNum;
+    }
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,17 +5,31 @@ public class Main {
     private static final int YES = 1;
     private static final int NO = 0;
 
+    public static final int NUMBER_OF_CASE = 3;
+
+    private static List<Integer> pickNum() {
+        Random random = new Random();
+        List<Integer> randomNum = new ArrayList<>();
+        while (randomNum.size() != NUMBER_OF_CASE) {
+            int ranNum = random.nextInt(9 ) + 1;
+            if (!randomNum.contains(ranNum)) {
+                randomNum.add(ranNum);
+            }
+        }
+        return randomNum;
+    }
+
+
+
     public static void main(String[] args){
         int ContinueGameSet = YES;
         int ContinueUserSet = YES;
 
         BaseballNumber baseballNumber;
 
-        baseballNumber = new BaseballNumber();
-
-
-
         while (ContinueGameSet == YES) {
+            baseballNumber = new BaseballNumber();
+            baseballNumber.setRandomNum(pickNum());
 
 
             while (ContinueUserSet == YES) {
