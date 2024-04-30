573b4f55d85a428553d971224ae45583a57762c1
alsswl <alswl4223@naver.com>
2024-04-30 22:41:41 +0900

    feat : 기본적인 main함수 while문으로 틀 짜두기

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static final int YES = 1;
+    private static final int NO = 0;
+
+    public static void main(String[] args){
+        int ContinueGameSet = YES;
+        int ContinueUserSet = YES;
+        while (ContinueGameSet == YES) {
+
+
+            while (ContinueUserSet == YES) {
+
+            }
+
+        }
+    }
+}
