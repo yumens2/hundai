cefe6ec8ed97f2f793f664f3a3dfd9d07cb8af49
HSHwan <tnghks2866@gmail.com>
2024-04-30 16:41:23 +0900

    refactor: 싱글톤 패턴으로 리팩토링

---
+++ src/main/java/utility/InputStringReceiver.java
@@ -0,0 +1,30 @@
+package utility;
+
+import java.util.Scanner;
+
+public class InputStringReceiver {
+
+    private static Scanner scanner;
+
+
+    private InputStringReceiver() {
+    }
+
+    public static Scanner getInstance() {
+        if (scanner == null) {
+            scanner = new Scanner(System.in);
+        }
+        return scanner;
+    }
+
+    public static void close() {
+        if (scanner != null) {
+            scanner.close();
+            scanner = null;
+        }
+    }
+
+    public static String getInputString() {
+        return getInstance().next();
+    }
+}
