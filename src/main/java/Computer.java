9e42c1ab4deaff3ace2c0559c85d525826cd54c1
ajy4304 <ajy4304@naver.com>
2024-04-30 13:03:24 +0900

    feat(Computer): 생성자, getComputerNumbers() 추가
    생성자와 Computer의 랜덤 수를 얻을 수 있도록 getter 추가

--- src/main/java/Computer.java
+++ src/main/java/Computer.java
@@ -3,10 +3,12 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
-
 public class Computer {
     private List<Integer> numbers;
 
+    public Computer(){
+        numbers = computerRandomNumbers();
+    }
     // 1~9 범위의 중복되지 않는 수 3개를 뽑아내는 함수
     private List<Integer> computerRandomNumbers(){
         List<Integer> numList = new ArrayList<>();
@@ -19,5 +21,9 @@ public class Computer {
         Collections.shuffle(numList);
         // 인덱스 0~2까지(총 3개)를 반환
         return numList.subList(0,3);
+    }
+
+    public List<Integer> getComputerNumbers(){
+        return numbers;
     }
 }
