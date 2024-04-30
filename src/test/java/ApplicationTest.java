b23f5cb930547a8e3a019d69648c8f743f8821b6
Park <pswaao88@gmail.com>
2024-04-30 20:32:10 +0900

    <refactor>: startGame() 의 리턴자료형을 boolean 에서 void 변경
    불필요한 boolean리턴을 void로 바꿈

--- src/test/java/ApplicationTest.java
+++ src/test/java/ApplicationTest.java
@@ -1,20 +1,48 @@
+
+import Model.Answer;
+import Model.Player;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ApplicationTest {
     /*
-        1. 컴퓨터 랜덤숫자 만드는지
-        2. answer에 저장
-        3. Player예 입력된 수 저장
-        4. 입력된 수 유효성 검사(정수가 맞는지, 3자가 맞는지, 0이 있는지, 중복되진 않는지
-        5. 힌트 계산
-        6. 계산된 힌트 출력
-        7. 3스트라이크면 게임종료 아니면 반복
-        8. 재시학할지 종료할지 입력( 유효성검사, 1,2아니면 싹다 아님)
+        1.
+        2.
+        3.
+        4.
+        5.
+        6.
+        7.
+        8.
         유효성 검사(정수가 맞는지, 3자가 맞는지, 0이 있는지, 중복되진 않는지
 
 
 
      */
-    //@Test
-    //void
+    @Test
+    @DisplayName("Player클래스에 수가 저장이 잘 되는지 테스트")
+    void Player검사() {
+        Player player = new Player("123");
+        assertThat(player.getPlayerNum()).isEqualTo("123");
+    }
+
+    @Test
+    @DisplayName("Answer클래스에 수가 저장이 잘 되는지 테스트")
+    void Answer검사() {
+        Answer answer = new Answer("123");
+        assertThat(answer.getAnswerNum()).isEqualTo("123");
+    }
+
+
+    //컴퓨터 랜덤숫자 만드는지 (보류)
+    //answer에 저장(저장이 잘되는지)
+    //Player예 입력된 수 저장(저장이 잘되는지)
+    //입력된 수 유효성 검사(정수가 맞는지, 3자가 맞는지, 0이 있는지, 중복되진 않는지)
+    //힌트 계산
+    //계산된 힌트 출력문구
+    //3스트라이크면 게임종료 아니면 반복
+    //재시학할지 종료할지 입력( 유효성검사, 1,2아니면 싹다 아님)
+    //startGame이 true를 리턴하는지
 }
