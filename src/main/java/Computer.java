40b9131dd14ad23d66c4899408030e014be3ec15
ajy4304 <ajy4304@naver.com>
2024-04-30 12:07:09 +0900

    feat(Computer): computerRandomNumbers() 메서드 추가
    1~9 범위의 중복되지 않는 수 3개를 뽑아내기 위한 함수 추가

--- src/main/java/Computer.java
+++ src/main/java/Computer.java
@@ -1,3 +1,23 @@
-public class Computer {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
+
+public class Computer {
+    private List<Integer> numbers;
+
+    // 1~9 범위의 중복되지 않는 수 3개를 뽑아내는 함수
+    private List<Integer> computerRandomNumbers(){
+        List<Integer> numList = new ArrayList<>();
+
+        // 먼저 1~9를 numList 에 넣음
+        for (int i = 1; i <= 9; i++){
+            numList.add(i);
+        }
+        // 수를 섞음
+        Collections.shuffle(numList);
+        // 인덱스 0~2까지(총 3개)를 반환
+        return numList.subList(0,3);
+    }
 }
