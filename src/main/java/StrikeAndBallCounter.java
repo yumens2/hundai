c3f58d829bb5bd7c2f34451fa0da52379537f575
ajy4304 <ajy4304@naver.com>
2024-04-30 16:52:30 +0900

    feat(StrikeAndBallCounter.java): 스트라이크와 볼을 세는 카운터 클래스 추가
    인덴트 뎁스를 줄이기 위한 클래스

---
+++ src/main/java/StrikeAndBallCounter.java
@@ -0,0 +1,17 @@
+public class StrikeAndBallCounter {
+    private int strikes;
+    private int balls;
+
+    public StrikeAndBallCounter(int strikes, int balls){
+        this.strikes = strikes;
+        this.balls = balls;
+    }
+
+    public int getStrikes(){
+        return strikes;
+    }
+
+    public int getBalls() {
+        return balls;
+    }
+}
