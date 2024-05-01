ab90f6708861195141157e6474610eac5928e995
GitJIHO <jiho99322@gmail.com>
2024-05-01 22:01:04 +0900

    feat(NumberGenerator): createRandomNumbers메소드에 1~9까지의 랜덤 숫자 생성 기능 구현

--- src/main/java/NumberGenerator.java
+++ src/main/java/NumberGenerator.java
@@ -1,2 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class NumberGenerator {
+    public List<Integer> createRandomNumbers(){
+        List<Integer> numbers = new ArrayList<>();
+        for(int i=0; i<3; i++) {
+            int num = new Random().nextInt(9) +1;
+            numbers.add(num);
+        }
+        return numbers;
+    }
 }
