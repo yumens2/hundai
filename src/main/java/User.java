75e53dafb6f40281335365be392ae6690f1d2fc2
geon <kky8586696@naver.com>
2024-05-01 22:44:35 +0900

    refactor(User): 유효성 검사 분리

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -2,26 +2,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import static utils.Validation.isValidInputNum;
+
 public class User {
-    /**
-     * 입력값 유효성 검증 메서드
-     * @param input
-     * @throws IllegalArgumentException
-     */
-    public static void isValidInputNum(String input) throws IllegalArgumentException {
 
-        if((input.length() !=3) ||
-                (input.charAt(0)==input.charAt(1) || input.charAt(0)==input.charAt(2)||input.charAt(1)==input.charAt(2))
-                ||(input.contains("0"))){
-            throw new IllegalArgumentException("입력값이 유효하지 않습니다.");
-        }
-        for(String num : input.split("")){
-            if(!Character.isDigit(num.charAt(0))){
-                throw new IllegalArgumentException("입력값이 유효하지 않습니다.");
-            }
-        }
-
-    }
 
     /**
      * 사용자에게 입력값을 받고 입력값 List 반환 메서드
