aec4c8f3b20eb9665ecfce3299b78cdbd68fbb7f
GitJIHO <jiho99322@gmail.com>
2024-05-01 21:55:14 +0900

    feat(Judgement):클래스 생성

---
+++ src/main/java/Judgement.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public class Judgement {
+    public int matchNumber(List<Integer> computer, List<Integer> player){ //숫자 중 몇개가 같은지 확인하는 메소드
+        return 0;
+    }
+
+    public boolean placeCheck(List<Integer> computer, int placeNumber, int number){ //placeNumber에 number가 있는지 확인하는 메소드
+        return false;
+    }
+}
+
