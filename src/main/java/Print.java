8e2887429a956d2416544b35c3ce7f03f16ada49
GitJIHO <jiho99322@gmail.com>
2024-05-01 23:30:27 +0900

    fix(Print): gamePrint메소드에서 종료후 스캔결과를 리턴하는 기능 추가

--- src/main/java/Print.java
+++ src/main/java/Print.java
@@ -6,7 +6,7 @@ public class Print {
 
     final Referee referee = new Referee();
     NumberScanner player = new NumberScanner();
-    void gamePrint() {
+    int gamePrint() {
         String result = "";
         while (!result.equals("3스트라이크")) {
             result = referee.compare(numbers, player.playerNumber());
@@ -14,5 +14,6 @@ public class Print {
         }
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        return player.again();
     }
 }
