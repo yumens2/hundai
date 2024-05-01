e1456a1af0a5a99c8beadef5c22c4f9a775e75c1
GitJIHO <jiho99322@gmail.com>
2024-05-01 22:04:46 +0900

    feat(Application): main문에 NumberGenerator을 통한 객체를 생성하여 랜덤숫자 생성 확인기능 추가

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,3 +1,9 @@
-public class Application {
+import java.util.List;
 
+public class Application {
+    public static void main(String[] args) {
+        final NumberGenerator generator = new NumberGenerator();
+        List<Integer> numbers = generator.createRandomNumbers();
+        System.out.println("numbers = " + numbers);
+    }
 }
