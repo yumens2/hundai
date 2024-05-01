289f2b717a74ca0ca37894827b5abfb50dcb6016
GitJIHO <jiho99322@gmail.com>
2024-05-01 22:19:50 +0900

    feat(Judgement): 숫자 중 몇개가 같은지 return 하는 기능을 가진 matchNumber 메소드 추가

--- src/main/java/Judgement.java
+++ src/main/java/Judgement.java
@@ -2,7 +2,16 @@ import java.util.List;
 
 public class Judgement {
     public int matchNumber(List<Integer> computer, List<Integer> player){ //숫자 중 몇개가 같은지 확인하는 메소드
-        return 0;
+        int result = 0;
+
+        for(int i = 0; i < computer.size(); i++){
+            int playerNum = player.get(i);
+            computer.contains(playerNum);
+            if (computer.contains(playerNum)){
+                result++;
+            }
+        }
+        return result;
     }
 
     public boolean placeCheck(List<Integer> computer, int placeNumber, int number){ //placeNumber에 number가 있는지 확인하는 메소드
