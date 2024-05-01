f6c68d5c698c39da7a3eac32658dd37897e7ffa1
Park <pswaao88@gmail.com>
2024-05-01 12:01:29 +0900

    <refacor>: test클래스의 메소드 한글에서 영어로 변경
    메소드가 한글이면 오류가 발생해 전부 영어로 변경하여 오류 없앰

--- src/test/java/ApplicationTest.java
+++ src/test/java/ApplicationTest.java
@@ -18,7 +18,7 @@ public class ApplicationTest {
 
     @Test
     @DisplayName("Player클래스에 수가 저장이 잘 되는지 테스트")
-    void Player검사() {
+    void classPlayerTest() {
         Player player = new Player("123");
 
         assertThat(player.getPlayerNum()).isEqualTo("123");
@@ -26,7 +26,7 @@ public class ApplicationTest {
 
     @Test
     @DisplayName("Answer클래스에 수가 저장이 잘 되는지 테스트")
-    void Answer검사() {
+    void classAnswerTest() {
         Answer answer = new Answer("123");
 
         assertThat(answer.getAnswerNum()).isEqualTo("123");
@@ -35,14 +35,14 @@ public class ApplicationTest {
     //유효성 정수가 맞는지, 3자리가 맞는지, 0을 포함했는지, 중복이 됐는지, 1또는2인지
     @Test
     @DisplayName("숫자가 맞는지 유효성 검사 테스트")
-    void 숫자유효성검사정수변환(){
+    void checkNumberTet(){
         assertThat(CheckValidity.numberValidity("123")).isEqualTo(true);
         assertThat(CheckValidity.numberValidity("abc")).isEqualTo(false);
     }
 
     @Test
     @DisplayName("3자리의 정수이면서 양수인지 테스트")
-    void 숫자유효성검사3자리수(){
+    void overThreeTest(){
         assertThat(CheckValidity.digitValidity("123")).isEqualTo(true);
         assertThat(CheckValidity.digitValidity("1234")).isEqualTo(false);
 
@@ -50,7 +50,7 @@ public class ApplicationTest {
 
     @Test
     @DisplayName("0을 포함한 수를 올바르게 검사하는지 테스트")
-    void 숫자유효성검사0을포함(){
+    void containZeroTest(){
         assertThat(CheckValidity.zeroValidity("123")).isEqualTo(true);
         assertThat(CheckValidity.zeroValidity("204")).isEqualTo(false);
 
@@ -58,21 +58,21 @@ public class ApplicationTest {
 
     @Test
     @DisplayName("즁복된 수를 올바르게 검사하는지 테스트")
-    void 숫자유효성검사중복(){
+    void sameNUmberTest(){
         assertThat(CheckValidity.sameValidity("123")).isEqualTo(true);
         assertThat(CheckValidity.sameValidity("112")).isEqualTo(false);
     }
 
     @Test
     @DisplayName("1 또는 2가 맞는지 테스트")
-    void 숫자유효성검사1또는2(){
+    void OneOrTwoTest(){
         assertThat(CheckValidity.oneTwoValidity("1")).isEqualTo(true);
         assertThat(CheckValidity.oneTwoValidity("2")).isEqualTo(true);
     }
 
     @Test
     @DisplayName("올바르지 않은 입력이 IllegalArgumentException을 일으키는지 테스트")
-    void 올바르지않은입력에러테스트(){
+    void IllegalInputTest(){
         assertThatThrownBy(() -> CheckPlayerNumberValidity.playerNumberValidity("a1002"))
                 .isInstanceOf(RuntimeException.class);
         assertThatThrownBy(() -> CheckPlayerRestartNumberValidity.playerRestartNumberValidity("3"))
@@ -81,14 +81,14 @@ public class ApplicationTest {
 
     @Test
     @DisplayName("Computer가 올바른 수를 생성하는지 테스트")
-    void Computer검사(){
+    void classComputerTest(){
         String testNumber = Computer.makeRandomNumber();
         assertThat(CheckValidity.sameValidity(testNumber)).isEqualTo(true);
     }
 
     @Test
     @DisplayName("힌트를 제대로 계산하지는 테스트")
-    void 힌트검사(){
+    void calculateHintTest(){
         String answer = "574";
         String player = "548";
         assertThat(CalculateScore.ballCount(answer,player)).isEqualTo(1);
