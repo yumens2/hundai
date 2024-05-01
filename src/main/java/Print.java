36ec02801ebaf9a752703e44e3336a13c3aad98b
GitJIHO <jiho99322@gmail.com>
2024-05-01 23:20:06 +0900

    feat(Print): Print 클래스 내에 실제 print부분을 gamePrint메소드로 묶음

--- src/main/java/Print.java
+++ src/main/java/Print.java
@@ -6,12 +6,13 @@ public class Print {
 
     final Referee referee = new Referee();
     NumberScanner player = new NumberScanner();
-
-    String result = "";
-        while (!result.equals("3스트라이크")){
-        result = referee.compare(numbers, player.playerNumber());
-        System.out.println(result);
-    }
+    void gamePrint() {
+        String result = "";
+        while (!result.equals("3스트라이크")) {
+            result = referee.compare(numbers, player.playerNumber());
+            System.out.println(result);
+        }
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
 }
