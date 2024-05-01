362266855fc76b3a17f52c5799dcaf912371f4d0
xkrwjdals <developjmt@gmail.com>
2024-05-02 00:32:21 +0900

    feat(Game) : add variables
    - Add description for functions to be implemented
    - Add variables for Game Class

---
+++ src/main/java/Game.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Game {
+    public static void main(String[] args) {
+        String compNum ;
+        String userNum;
+        String count;
+        int choice;
+        boolean answer;
+
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
