11eebf2c88cfa3644db7aba1a90dd7098aeda5a8
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-05-01 23:33:44 +0900

    Feat : 컴퓨터의 중복 숫자 제거

--- src/main/java/Computer.java
+++ src/main/java/Computer.java
@@ -3,8 +3,12 @@ import java.util.Random;
 public class Computer {
     public void RandomThreeNumber(int[] cpunum){
         Random rd = new Random();
+        DuplicateChecking dc = new DuplicateChecking();
         for(int i = 0; i < cpunum.length; i++){
             cpunum[i] = rd.nextInt(9)+1;
+            if(dc.ExistDuplicate(cpunum,i)){
+                i = i - 1;
+            }
         }
     }
 }
---
+++ src/main/java/DuplicateChecking.java
@@ -0,0 +1,10 @@
+public class DuplicateChecking {
+    public boolean ExistDuplicate(int[] numarray, int fori){
+        for(int j = 0; j < fori; j++){
+            if(numarray[fori] == numarray[j]){
+                return true;
+            }
+        }
+        return false;
+    }
+}
