3b6bda058f60e421f25de27be1ad00fa0c8ce0f0
GitJIHO <jiho99322@gmail.com>
2024-05-01 22:48:28 +0900

    feat(Application): main문에 반복 없이 게임 수행하는 기능 구현

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -4,6 +4,18 @@ public class Application {
     public static void main(String[] args) {
         final NumberGenerator generator = new NumberGenerator();
         List<Integer> numbers = generator.createRandomNumbers();
-        System.out.println("numbers = " + numbers);
+
+        final Referee referee = new Referee();
+
+        String result = "";
+        while (!result.equals("0볼 3스트라이크")){
+            result = referee.compare(numbers, playerNumber());
+            System.out.println(result);
+        }
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+
+    public static List<Integer> playerNumber() {
+        return null;
     }
 }
