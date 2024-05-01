1410e8d9ce0e161bffca974eb59ca24e71cc4227
GitJIHO <jiho99322@gmail.com>
2024-05-01 23:05:32 +0900

    feat(NumberScanner): scan기능을 따로 분리하기 위해 NumberScanner Class 생성

--- src/main/java/NumberScanner.java
+++ src/main/java/NumberScanner.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class NumberScanner {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class NumberScanner {
+    public List<Integer> playerNumber() {
+        System.out.print("숫자를 입력해 주세요 : ");
+        final Scanner scanner = new Scanner(System.in);
+        final String scan = scanner.next();
+
+        List<Integer> numbers = new ArrayList<>();
+        for (String number : scan.split("")){
+            numbers.add(Integer.valueOf(number));
+        }
+        return numbers;
+    }
 }
