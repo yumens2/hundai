183c8b1402b6c7afd1dcda3ed6c27d5656806c72
GitJIHO <jiho99322@gmail.com>
2024-05-01 22:08:12 +0900

    fix(NumberGenerator): createRandomNumbers에서 중복으로 숫자 생성되는 문제 해결을 위한 코드 수정

--- src/main/java/NumberGenerator.java
+++ src/main/java/NumberGenerator.java
@@ -5,9 +5,13 @@ import java.util.Random;
 public class NumberGenerator {
     public List<Integer> createRandomNumbers(){
         List<Integer> numbers = new ArrayList<>();
-        for(int i=0; i<3; i++) {
-            int num = new Random().nextInt(9) +1;
-            numbers.add(num);
+        while(numbers.size()<3){
+            int num = new Random().nextInt(9)+1;
+            if (numbers.contains(num)){
+                continue;
+            } else {
+                numbers.add(num);
+            }
         }
         return numbers;
     }
