1d91175e2da5920954dbe89f6ed7c90ff47baee0
GitJIHO <jiho99322@gmail.com>
2024-05-01 23:08:33 +0900

    fix(Application): scan 기능 분리 후 수정

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -8,25 +8,14 @@ public class Application {
         List<Integer> numbers = generator.createRandomNumbers();
 
         final Referee referee = new Referee();
+        NumberScanner player = new NumberScanner();
 
         String result = "";
         while (!result.equals("3스트라이크")){
-            result = referee.compare(numbers, playerNumber());
+            result = referee.compare(numbers, player.playerNumber());
             System.out.println(result);
         }
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-    }
-
-    public static List<Integer> playerNumber() {
-        System.out.print("숫자를 입력해 주세요 : ");
-        final Scanner scanner = new Scanner(System.in);
-        final String scan = scanner.next();
-
-        List<Integer> numbers = new ArrayList<>();
-        for (String number : scan.split("")){
-            numbers.add(Integer.valueOf(number));
-        }
-        return numbers;
     }
 }
