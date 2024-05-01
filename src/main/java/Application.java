d810474ae59b756bc24d01cec368fdc6c3cf1ab5
dkswoals <a4445656@gmail.com>
2024-05-01 14:56:13 +0900

    feat: Application 게임 실행 및 진행 구현

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,22 @@
+import java.util.ArrayList;
+
 public class Application {
     public static void main(String[] args){
+        boolean start = true;
+        Input input = new Input();
+        ResultChecker rc = new ResultChecker();
+        ReStart rs = new ReStart();
 
+        while(start) {
+            ArrayList<Integer> target = RandomNumber.generateRandomNumber();
+            boolean round = true;
+            System.out.println(target);
+           while(round){
+               ArrayList<Integer> userInput = input.getUserNumber();
+               round = rc.checkRes(userInput,target);
+           }
+           System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+           start = rs.selectContinue();
+        }
     }
 }
