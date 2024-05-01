bd976e9659efbf9be60db9dd0c5f917de939d9fe
dkswoals <a4445656@gmail.com>
2024-05-01 12:55:46 +0900

    feat: 게임 종료시 재시작 여부 판단 기능 구현

---
+++ src/main/java/ReStart.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class ReStart {
+    public boolean selectContinue() {
+        Validation validation = new Validation();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("게임을 새로 시작하시려면 1, 종료하려면 2를 입력하세요.");
+        String input = sc.nextLine().trim();
+        validation.validateInt(input);
+        return input.equals("1");
+    }
+}
