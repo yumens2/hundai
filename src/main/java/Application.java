ca94af8339684bfaa7f748ab874c3881fef316e4
GitJIHO <jiho99322@gmail.com>
2024-05-02 00:16:16 +0900

    fix(Application): main문 반복 조건에 맞게 수정

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,6 +3,12 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         Print print = new Print();
-        print.gamePrint();
+        int again = print.gamePrint();
+        while(again == 1){
+            again = print.gamePrint();
+            if(again == 1) continue;
+            else if(again == 2) break;
+            else throw new IllegalArgumentException();
+        }
     }
 }
