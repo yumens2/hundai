c3122fad5efcae9e0f2f9391ba6d9c4420044c31
geist9110 <geist9110@gmail.com>
2024-05-01 20:29:54 +0900

    feat($main) : add Application class and Main function
    Add main function for start program in Application class
    Add Function to check Java Version

---
+++ src/main/java/Application.java
@@ -0,0 +1,6 @@
+public class Application {
+    public static void main(String[] args) throws Exception {
+        String version = System.getProperty("java.version");
+        System.out.println("JAVA Version : " + version);
+    }
+}
