9ed5d0a62caa5ce3b74fe190e9208e7e415403a2
GitJIHO <jiho99322@gmail.com>
2024-05-01 22:30:39 +0900

    feat(Referee): computer와 player의 숫자를 compare하여 출력하는 기능을 가진 메소드 추가

--- src/main/java/Referee.java
+++ src/main/java/Referee.java
@@ -1,2 +1,23 @@
+import java.util.List;
+
 public class Referee {
+    public String compare(List<Integer> computer, List<Integer> player){
+        Judgement judgement = new Judgement();
+        int matchNumber = judgement.matchNumber(computer, player);
+
+        int strike = 0;
+        for (int placeNumber = 0; placeNumber<player.size(); placeNumber++){
+            if (judgement.placeCheck(computer, placeNumber, player.get(placeNumber))){
+                strike++;
+            }
+        }
+
+        int ball = matchNumber - strike;
+
+        if(matchNumber == 0){
+            return "아웃";
+        }
+
+        return ball + "볼" + strike + "스트라이크 ";
+    }
 }
