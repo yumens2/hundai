9741538c617d9967449ff1eb64ea0c6b2d16bd3b
noojmogstel <goaudgo@naver.com>
2024-05-03 05:10:05 +0900

    feat(exception): add RetryException

---
+++ src/main/java/baseball/exception/RetryException.java
@@ -0,0 +1,21 @@
+package baseball.exception;
+
+public class RetryException {
+    private static final int VALID_LENGTH=1;
+
+    public void retryValidation(String input){
+        isValidLength(input);
+        isValidNumber(input);
+
+    }
+
+    public void isValidNumber(String input){
+        if(!(input.equals("1") || !input.equals("2"))) throw new IllegalArgumentException("1,2 만 입력 가능");
+
+    }
+
+    public void isValidLength(String input){
+        if(input.length()!=VALID_LENGTH) throw new IllegalArgumentException("한자리 숫자만 입력 가능");
+    }
+
+}
