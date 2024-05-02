05232cf295e0e2ed094b1321b675f2473aa494ec
momnpa333 <momnpa333@gmail.com>
2024-05-03 00:16:19 +0900

    feat(application.java): main 제작

---
+++ src/main/java/game/application.java
@@ -0,0 +1,12 @@
+package game;
+
+import global.controller.GameController;
+
+public class application {
+
+    public static void main(String[] args) {
+        GameController gameController = new GameController();
+        gameController.playGame();
+    }
+
+}
