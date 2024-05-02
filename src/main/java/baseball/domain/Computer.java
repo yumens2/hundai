3d20a96c322b845dc8f1278b33351d23bdd2e866
noojmogstel <goaudgo@naver.com>
2024-05-03 04:38:13 +0900

    feat(domain): add computer domain

---
+++ src/main/java/baseball/domain/Computer.java
@@ -0,0 +1,23 @@
+package baseball.domain;
+
+import baseball.utils.GenerateRandomNumber;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Computer {
+    private List<Integer> computerNumbers;
+    private GenerateRandomNumber generateRandomNumber = new GenerateRandomNumber();
+
+    public void setComputerNumber(){
+        computerNumbers = generateRandomNumber.generate();
+    }
+    public List<Integer> getComputerNumbers(){
+        return computerNumbers;
+    }
+
+
+
+
+}
