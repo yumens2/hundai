c597063a8694900416f4fb1ef088250f7922b6bb
GitJIHO <jiho99322@gmail.com>
2024-05-03 00:22:27 +0900

    fix(Application): 중복 오류판별 제거

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -7,8 +7,7 @@ public class Application {
             print.gamePrint();
             again = checkAgain();
             if (again == 1) continue;
-            else if (again == 2) break;
-            else throw new IllegalArgumentException("입력값이 유효하지 않습니다.");
+            else break;
         }
     }
 
