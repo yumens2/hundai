6722f076bb2e706f4e078cb04361109944ba6d0e
Jeje <jjh4450jjh4450@gmail.com>
2024-05-03 01:40:40 +0900

    feat(Application): 사용자 입력 기능 추가
    사용자 입력 기능을 추가함. 검증 코드 작성 예정

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
+
 public class Application {
 
     public static void main(String[] args) {
         System.out.println("welcome to number baseball game!");
         Logic logic = new Logic();
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("숫자를 입력해 주세요:");
+            int userNumber = sc.nextInt();
+            // todo: check if userNumber is valid
+            int strike = logic.isStrike(userNumber);
+            int ball = logic.isBall(userNumber);
+        }
     }
 }
