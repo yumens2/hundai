a78e0f9b9db39f5249b6aeb6d30149ce606daf05
EunKyung Lee <2dmsrud2002@naver.com>
2024-05-02 21:25:05 +0900

    Docs: numberValidate 메서드 주석 추가
    numberValidate 메서드는 입력 받은 값이 null이거나
    세자리 수가 아니거나 세자리 숫자가 아니거나 중복된 숫자가 아닌지
    확인하고 Boolean을 반환하는 메서드로 이에 대한 주석을 추가함

--- src/main/java/Validate.java
+++ src/main/java/Validate.java
@@ -3,15 +3,30 @@ import java.util.regex.Pattern;
 public class Validate {
     public Boolean numberValidate(String input) {
         HashSet<Character> characters = new HashSet<>();
+        /*
+            입력이 null 인 경우
+         */
         if (input == null) {
             return Boolean.FALSE;
         }
+
+        /*
+            세 자리 수가 아닌 경우
+         */
         if (input.length() != 3) {
             return Boolean.FALSE;
         }
+
+        /*
+            세 자리가 숫자가 아닌 경우
+         */
         if (!Pattern.matches("[1-9][1-9][1-9]", input)) {
             return Boolean.FALSE;
         }
+
+        /*
+            중복된 숫자가 있는 경우
+         */
         for (Character c : input.toCharArray()) {
             characters.add(c);
         }
