6d32b7d95669caf2984ade60d3f6de16837fffeb
GitJIHO <jiho99322@gmail.com>
2024-05-02 23:45:35 +0900

    feat(ApplicationTest): ApplicationTest클래스 생성

---
+++ src/test/java/ApplicationTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApplicationTest {
+    @Test
+    @DisplayName("3스트라이크 후 입력값이 1,2가 아닌 경우 예외 발생")
+    public void testInvalidInput() {
+        Application application = new Application();
+
+    }
+}
