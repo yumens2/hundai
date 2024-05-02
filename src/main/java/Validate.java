ff535f1f55ab579bf742d2e2a93f7ff9f82cc488
EunKyung Lee <2dmsrud2002@naver.com>
2024-05-02 21:21:39 +0900

    Feat: 사용자 입력 값에 중복된 값이 있는 경우

--- src/main/java/Validate.java
+++ src/main/java/Validate.java
@@ -1,5 +1,7 @@
+import java.util.HashSet;
 public class Validate {
     public Boolean numberValidate(String input) {
+        HashSet<Character> characters = new HashSet<>();
         if (input == null) {
             return Boolean.FALSE;
         }
@@ -7,6 +9,12 @@ public class Validate {
             return Boolean.FALSE;
         }
         if (!input.chars().allMatch(Character::isDigit)) {
+            return Boolean.FALSE;
+        }
+        for (Character c : input.toCharArray()) {
+            characters.add(c);
+        }
+        if (characters.size() != 3) {
             return Boolean.FALSE;
         }
         return Boolean.TRUE;
