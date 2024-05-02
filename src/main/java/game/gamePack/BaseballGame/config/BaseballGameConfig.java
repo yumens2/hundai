f17b888bda2c1a22696b94194cc541e491057f6d
momnpa333 <momnpa333@gmail.com>
2024-05-03 00:34:40 +0900

    refact(BaseballGameConfig.java): 사용하지 않는 getComputer 메서드 삭제

--- src/main/java/game/gamePack/BaseballGame/config/BaseballGameConfig.java
+++ src/main/java/game/gamePack/BaseballGame/config/BaseballGameConfig.java
@@ -4,7 +4,6 @@ import game.gamePack.BaseballGame.model.BaseballGame;
 import game.gamePack.BaseballGame.model.Computer;
 import game.gamePack.BaseballGame.model.Result;
 import game.gamePack.BaseballGame.service.BaseballGameService;
-import game.gamePack.BaseballGame.view.OutputView;
 
 public class BaseballGameConfig {
 
@@ -13,18 +12,13 @@ public class BaseballGameConfig {
     private static final Result result = Result.of();
     private static final BaseballGameService baseballGameService = BaseballGameService.of(computer,
         baseballGame, result);
-    private static final OutputView outputView = OutputView.of();
 
     /**
      * 생성자 제한
      */
     private BaseballGameConfig() {
     }
-
-    public static Computer getComputer() {
-        return computer;
-    }
-
+    
     public static BaseballGameService getBaseballGameService() {
         return baseballGameService;
     }
