851ded4d738dab9f724241f7ff072f24dfd1e2a4
momnpa333 <momnpa333@gmail.com>
2024-05-03 00:33:48 +0900

    refact(InputView.java): 사용하지 않는 변수 삭제

--- src/main/java/game/gamePack/BaseballGame/view/InputView.java
+++ src/main/java/game/gamePack/BaseballGame/view/InputView.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class InputView {
 
-    private static final String INPUT_NUMBERS_MESSAGE = "숫자를 입력해주세요 : ";
     private static final String INPUT_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     private static final String GUESS_MESSAGE = "숫자를 입력해주세요 : ";
 
@@ -23,8 +22,4 @@ public class InputView {
         return scanner.nextLine();
     }
 
-    public static String inputNumbers() {
-        System.out.println(INPUT_NUMBERS_MESSAGE);
-        return scanner.nextLine();
-    }
 }
