6eea985d2da319552b0b6edf0676970a19031942
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-05-03 00:16:56 +0900

    게임 종료 및 새시작을 위해 입력을 받고 입력값에 따라 행동하기

--- src/main/java/BaseBallBE.java
+++ src/main/java/BaseBallBE.java
@@ -1,6 +1,21 @@
 public class BaseBallBE {
     public static void main(String[] args) {
-        DetailedGame dg = new DetailedGame();
-        dg.play();
+        boolean gamestatus = true;
+        int continuenum = 0;
+        while(gamestatus){
+            DetailedGame dg = new DetailedGame();
+            ContinueGame cg = new ContinueGame();
+            dg.play();
+            try{
+                continuenum = cg.continueorexit();
+            }catch(IllegalArgumentException e){
+                System.out.println(e.toString());
+            }
+            if(continuenum == 1){
+                gamestatus = true;
+            }else if(continuenum == 2){
+                gamestatus = false;
+            }
+        }
     }
 }
--- src/main/java/DetailedGame.java
+++ src/main/java/DetailedGame.java
@@ -22,10 +22,7 @@ public class DetailedGame {
             sbc.countStrikeBall(cpunum,playernum, strikeballcount);
             sbc.outputresult(strikeballcount);
         }
-        if(strikeballcount[0] == 3){
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-        }
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
 }
