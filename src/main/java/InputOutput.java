979092e33062019bbcb852b242e444108cfd7a34
EunKyung Lee <2dmsrud2002@naver.com>
2024-05-02 21:32:19 +0900

    Feat: 입력된 정보에 볼, 스트라이크, 낫싱 정보 출력
    InputOutput 클래스의 hitterResultOutput 메서드에서는 ballCnt와
    strikeCnt를 매개변수로 받아서 각 정보에 따라 출력

--- src/main/java/InputOutput.java
+++ src/main/java/InputOutput.java
@@ -10,4 +10,16 @@ public class InputOutput {
         return scanner.nextLine();
     }
 
+    public void hitterResultOutput(int ballCnt, int strikeCnt) {
+        if (ballCnt == 0 && strikeCnt == 0) {
+            System.out.println("낫싱");
+        } else if (ballCnt == 0 && strikeCnt != 0) {
+            System.out.println(strikeCnt + "스트라이크");
+        } else if (ballCnt != 0 && strikeCnt == 0) {
+            System.out.println(ballCnt + "볼");
+        } else {
+            System.out.println(ballCnt + "볼 " + strikeCnt + "스트라이크");
+        }
+    }
+
 }
