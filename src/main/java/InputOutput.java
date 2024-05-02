52f7cc639cd00acfc0f9a39049f7110086b19d41
EunKyung Lee <2dmsrud2002@naver.com>
2024-05-02 21:15:36 +0900

    Feat: 게임 시작에 대한 출력

---
+++ src/main/java/InputOutput.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class InputOutput {
+    private Scanner scanner = new Scanner(System.in);
+    public void startOutput() {
+        System.out.print("숫자를 입력해 주세요 : ");
+    }
+
+}
