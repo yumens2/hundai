276cce5fa0f0f827eed6c01ea3926cea9ee0e692
xkrwjdals <developjmt@gmail.com>
2024-05-02 18:49:13 +0900

    feat(Game) : add two method to print result
    - printResult(String count) method return boolean type
    - if (strike == 3) return true else return false
    - printResult method print result about user input
    - if answer == true, call userChoice() method for ask continue game or not

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -14,6 +14,40 @@ public class Game {
             userNum = sc.nextLine();
 
             count = CountNum.CountSB(userNum, compNum);
+            answer = Game.printResult(count);
+            if (answer) {
+                choice = Game.userChoice();
+                compNum = CreateNum.createNumbers();
+            }
         }
+    }
+
+    public static boolean printResult(String count) {
+        int strike = count.charAt(0) - '0';
+        int ball = count.charAt(1) - '0';
+        boolean result = false;
+        if (ball == 0 && strike == 0) {
+            System.out.println("낫싱");
+        } else if (ball == 0) {
+            System.out.println(strike + "스트라이크");
+        } else if (strike == 0) {
+            System.out.println(ball + "볼");
+        } else {
+            System.out.println(ball + "볼 " + strike + "스트라이크");
+        }
+
+        if (strike == 3) {
+            result = true;
+        }
+        return result;
+    }
+
+    public static int userChoice() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+        return sc.nextInt();
     }
 }
