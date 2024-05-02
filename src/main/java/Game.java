ee96dca7c2ba32b974c18d9d8633fe4b71875e1e
xkrwjdals <developjmt@gmail.com>
2024-05-03 02:10:11 +0900

    refactor(PrintInfo) : Separate methods that output results into a new class
    - migrate printResult() method and userChoice() method to PrintInfo class
    - in Game class, minor code style modifications due to method migration

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -6,7 +6,7 @@ public class Game {
         int choice = 1;
         boolean answer;
 
-        while(choice == 1) {
+        while (choice == 1) {
             System.out.println("숫자를 입력해주세요 : ");
             try {
                 userNum = InputException.inputUserNum();
@@ -16,42 +16,11 @@ public class Game {
             }
 
             count = CountNum.CountSB(userNum, compNum);
-            answer = Game.printResult(count);
+            answer = PrintInfo.printResult(count);
             if (answer) {
-                choice = Game.userChoice();
+                choice = PrintInfo.userChoice();
                 compNum = CreateNum.createNumbers();
             }
-        }
-    }
-
-    public static boolean printResult(String count) {
-        int strike = count.charAt(0) - '0';
-        int ball = count.charAt(1) - '0';
-        boolean result = false;
-        if (ball == 0 && strike == 0) {
-            System.out.println("낫싱");
-        } else if (ball == 0) {
-            System.out.println(strike + "스트라이크");
-        } else if (strike == 0) {
-            System.out.println(ball + "볼");
-        } else {
-            System.out.println(ball + "볼 " + strike + "스트라이크");
-        }
-
-        if (strike == 3) {
-            result = true;
-        }
-        return result;
-    }
-
-    public static int userChoice() {
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-        try {
-            return InputException.inputChoice();
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            return 2;
         }
     }
 }
