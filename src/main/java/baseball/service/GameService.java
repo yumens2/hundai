12909b3751396d9f89753339ec30c2483408bb44
noojmogstel <goaudgo@naver.com>
2024-05-03 05:16:14 +0900

    feat(service): add GameService

---
+++ src/main/java/baseball/service/GameService.java
@@ -0,0 +1,21 @@
+package baseball.service;
+
+import baseball.domain.Computer;
+
+public class GameService {
+    private static final int RESTART = 1;
+    private static final int END = 2;
+    private boolean endGame;
+
+    public boolean isEndGame(){
+        return endGame;
+    }
+
+    public void setEndGame(int restartOrEnd, Computer computer){
+        if(restartOrEnd==RESTART)
+            computer.setComputerNumber();
+        if(restartOrEnd==END)
+            endGame=true;
+    }
+
+}
