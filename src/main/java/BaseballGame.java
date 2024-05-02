265dc6f8f423a2cd07cabe4b3b2b35e7d68825c6
5win <woozzoo7@gmail.com>
2024-05-02 18:31:07 +0900

    feat(BaseballGame.java): 게임 재시작/종료 기능 추가 - game(): 유저가 1을 입력하면 재시작, 2를 입력하면 종료 - Computer, User 인스턴스를 final로 변경

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -1,6 +1,8 @@
 public class BaseballGame {
-    private Computer com;
-    private User user;
+    static final int RETRY = 1;
+
+    private final Computer com;
+    private final User user;
 
     public BaseballGame() {
         com = new Computer();
@@ -8,11 +10,19 @@ public class BaseballGame {
     }
 
     public void game() {
-        while(true) {
-            GameProcess gameProcess = new GameProcess(com, user);   // 새 게임 생성
-            gameProcess.play();                                     // 새 게임 플레이
+        do {
+            new GameProcess(com, user).play();
+            com.printFinishMessage();
+            user.setRetryOrFinish();
+        } while(retryGame(user.chooseRetryOrFinish()));
+    }
 
-            // 게임을 재시작하거나 종료
-        }
+    /**
+     * 유저가 재시작을 선택했는지 아닌지 확인하는 메서드
+     * @param userChoice 유저의 선택
+     * @return 재시작을 선택했다면 true
+     */
+    private boolean retryGame(int userChoice) {
+        return userChoice == RETRY;
     }
 }
