cc9473528feec331da5a28b47db8c0edd9ac7058
EunKyung Lee <2dmsrud2002@naver.com>
2024-05-02 21:37:47 +0900

    Feat: 게임을 다시 시작하거나 종료하려는 경우 출력

--- src/main/java/InputOutput.java
+++ src/main/java/InputOutput.java
@@ -6,6 +6,10 @@ public class InputOutput {
         System.out.print("숫자를 입력해 주세요 : ");
     }
 
+    public void restartOutput() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+
     public String numberInput() {
         return scanner.nextLine();
     }
