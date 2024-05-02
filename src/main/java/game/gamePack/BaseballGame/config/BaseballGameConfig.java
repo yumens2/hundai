beb8d6bb18ad1e7a0c0c5717424db063419d8c07
momnpa333 <momnpa333@gmail.com>
2024-05-02 23:13:55 +0900

    feat(BaseballGameConfig.java): BaseballGameService 생성자 변경에 따른 의존성 주입

--- src/main/java/game/gamePack/BaseballGame/config/BaseballGameConfig.java
+++ src/main/java/game/gamePack/BaseballGame/config/BaseballGameConfig.java
@@ -1,12 +1,19 @@
 package game.gamePack.BaseballGame.config;
 
+import game.gamePack.BaseballGame.model.BaseballGame;
 import game.gamePack.BaseballGame.model.Computer;
+import game.gamePack.BaseballGame.model.Result;
 import game.gamePack.BaseballGame.service.BaseballGameService;
+import game.gamePack.BaseballGame.view.OutputView;
 
 public class BaseballGameConfig {
 
-    private static final BaseballGameService baseballGameService = BaseballGameService.of();
+    private static final BaseballGame baseballGame = BaseballGame.of();
     private static final Computer computer = Computer.of();
+    private static final Result result = Result.of();
+    private static final BaseballGameService baseballGameService = BaseballGameService.of(computer,
+        baseballGame, result);
+    private static final OutputView outputView = OutputView.of();
 
     /**
      * 생성자 제한
