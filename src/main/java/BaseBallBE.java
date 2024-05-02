6187f35ecac23c4836c5e281da9061a6da883301
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-05-02 21:10:54 +0900

    Feat : 결과에 따라 스트라이크, 볼, 낫싱 출력하기

--- src/main/java/BaseBallBE.java
+++ src/main/java/BaseBallBE.java
@@ -1,12 +1,8 @@
-
-import java.util.InputMismatchException;
-import java.util.Scanner;
-
 public class BaseBallBE {
     public static void main(String[] args) {
         int[] cpunum = new int[3];
         int[] playernum = new int[3];
-        int[] strikeball = new int[2];
+        int[] strikeballcount = new int[2];
         Computer cpu = new Computer();
         Player ply = new Player();
         StrikeBallChecking sbc = new StrikeBallChecking();
@@ -22,7 +18,7 @@ public class BaseBallBE {
         for(int i = 0; i < playernum.length; i++){
             System.out.println(playernum[i]);
         }
-        sbc.countStrikeBall(cpunum,playernum,strikeball);
-        System.out.println(strikeball[0]+"스트라이크, "+strikeball[1]+"볼");
+        sbc.countStrikeBall(cpunum,playernum, strikeballcount);
+        sbc.outputresult(strikeballcount);
     }
 }
--- src/main/java/StrikeBallChecking.java
+++ src/main/java/StrikeBallChecking.java
@@ -1,6 +1,6 @@
 public class StrikeBallChecking {
 
-    public int[] countStrikeBall(int[] cpunum, int[] playernum,int[] strikeball){
+    public int[] countStrikeBall(int[] cpunum, int[] playernum,int[] strikeballcount){
         int strike = 0;
         int ball = 0;
         for(int i = 0; i < cpunum.length; i++){
@@ -10,9 +10,9 @@ public class StrikeBallChecking {
                 ball += result[1];
             }
         }
-        strikeball[0] = strike;
-        strikeball[1] = ball;
-        return strikeball;
+        strikeballcount[0] = strike;
+        strikeballcount[1] = ball;
+        return strikeballcount;
     }
     private int[] StrikeBall(int[] cpunum, int[] playernum, int i, int j){
         int strike = 0;
@@ -24,5 +24,17 @@ public class StrikeBallChecking {
             ball++;
         }
         return new int[]{strike, ball};
+    }
+
+    public void outputresult(int[] strikeballcount){
+        if((strikeballcount[0] != 0) && (strikeballcount[1] != 0)){
+            System.out.println(strikeballcount[1]+"볼 "+ strikeballcount[0]+"스트라이크");
+        }else if((strikeballcount[0] == 0) && (strikeballcount[1] == 0)){
+            System.out.println("낫싱");
+        }else if((strikeballcount[0] == 0) && (strikeballcount[1] != 0)){
+            System.out.println(strikeballcount[1]+"볼");
+        }else{
+            System.out.println(strikeballcount[0]+"스트라이크");
+        }
     }
 }
