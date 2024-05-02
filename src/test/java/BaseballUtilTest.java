00fa0e316889d9a56cbb66af6fdcf57702438e35
EunKyung Lee <2dmsrud2002@naver.com>
2024-05-02 21:45:31 +0900

    Refactor: BaseballUnitTest 매개변수 변경

--- src/test/java/BaseballUtilTest.java
+++ src/test/java/BaseballUtilTest.java
@@ -7,50 +7,50 @@ public class BaseballUtilTest {
 
     @Test
     public void 스트라이크_같은_수() {
-        int random = 156;
-        int input = 156;
+        String random = "156";
+        String input = "156";
         assertEquals(3, baseballUtil.strike(random, input));
     }
 
     @Test
     public void 한_자리_수_스트라이크_같은_수() {
-        int random = 456;
-        int input = 496;
+        String random = "456";
+        String input = "496";
         assertEquals(2, baseballUtil.strike(random, input));
     }
 
     @Test
     public void 모두_다른_수() {
-        int random = 897;
-        int input = 123;
+        String random = "897";
+        String input = "123";
         assertEquals(0, baseballUtil.strike(random, input));
     }
 
     @Test
     public void 볼_같은_수() {
-        int random = 123;
-        int input = 123;
+        String random = "123";
+        String input = "123";
         assertEquals(0, baseballUtil.ball(random, input));
     }
 
     @Test
     public void 볼_모든_자리_다름() {
-        int random = 894;
-        int input = 123;
+        String random = "894";
+        String input = "123";
         assertEquals(0, baseballUtil.ball(random, input));
     }
 
     @Test
     public void 볼_다른_자리_세_개() {
-        int random = 145;
-        int input = 451;
+        String random = "145";
+        String input = "451";
         assertEquals(3, baseballUtil.ball(random, input));
     }
 
     @Test
     public void 볼_다른_자리_두_개() {
-        int random = 145;
-        int input = 471;
+        String random = "145";
+        String input = "471";
         assertEquals(2, baseballUtil.ball(random, input));
     }
 }
