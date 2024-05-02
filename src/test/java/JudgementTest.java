721b0e45195888eac6c9d0d138f5b5be3c1ea464
GitJIHO <jiho99322@gmail.com>
2024-05-02 22:29:33 +0900

    feat(Judgement): 컴퓨터와 플레이어의 리스트에 2개가 같을 경우 result = 2 테스트기능 추가

--- src/test/java/JudgementTest.java
+++ src/test/java/JudgementTest.java
@@ -25,6 +25,21 @@ class JudgementTest {
     }
 
     @Test
+    @DisplayName("컴퓨터와 플레이어의 숫자 리스트 비교 - 일치하는 숫자 2개인 경우")
+    void matchNumber_twoMatch() {
+        //given
+        Judgement judgement = new Judgement();
+        List<Integer> computer = Arrays.asList(1,2,3);
+        List<Integer> player = Arrays.asList(2,3,7);
+
+        //when
+        int result = judgement.matchNumber(computer, player);
+
+        //then
+        assertEquals(2,result);
+    }
+
+    @Test
     void placeCheck() {
     }
 }
