d2bbb6c6d1dcfcb203675ca58590c1f04f632301
suhyeon <nucc1186@gamil.com>
2024-05-03 08:55:54 +0900

    [update] Generator

--- src/main/java/model/BaseballNumberGenerator.java
+++ src/main/java/model/BaseballNumberGenerator.java
@@ -2,13 +2,14 @@ package model;
 
 import java.util.List;
 
-public class BaseballNumberDTO {
+public class BaseballNumberGenerator {
     private List<Integer> number;
     private static int making_number_count = 3;
 
-    public BaseballNumberDTO(){
+    public List<Integer> generate(){
         for(int i = 0; i < making_number_count; i++) {
             number.add((int) (Math.random() * 10));
         }
+        return number;
     }
 }
