e095b4a351015082a4074c5d4db3a584e4d14c04
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-05-02 14:37:57 +0900

    Fix : 예외 위치 수정

--- src/main/java/BaseBallBE.java
+++ src/main/java/BaseBallBE.java
@@ -6,7 +6,6 @@ public class BaseBallBE {
     public static void main(String[] args) {
         int[] cpunum = new int[3];
         int[] playernum = new int[3];
-        int num;
         Computer cpu = new Computer();
         Player ply = new Player();
         cpu.RandomThreeNumber(cpunum);
@@ -15,11 +14,11 @@ public class BaseBallBE {
         }
         try {
             ply.inputplayernum();
-        }catch(InputMismatchException a){
-            System.out.println("잘못 입력했습니다. 정수만 입력 가능합니다.");
-        }
-        catch(IllegalArgumentException e){
+        }catch(IllegalArgumentException e){
             System.out.println(e.toString());
+        }
+        catch(InputMismatchException a){
+            System.out.println("잘못 입력했습니다. 정수만 입력 가능합니다.");
         }
     }
 }
