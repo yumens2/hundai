ee547a55397082436b9973fa0989582b309f30d7
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-05-02 23:40:55 +0900

    Feat : 게임 반복을 위한 새로운 클래스 생성
    Feat : 게임을 계속하거나 종료하기 위한 안내문 출력

---
+++ src/main/java/DetailedGame.java
@@ -0,0 +1,31 @@
+public class DetailedGame {
+    public void play(){
+        int[] cpunum = new int[3];
+        int[] playernum = new int[3];
+        int[] strikeballcount = new int[2];
+        Computer cpu = new Computer();
+        Player ply = new Player();
+        StrikeBallChecking sbc = new StrikeBallChecking();
+        cpu.RandomThreeNumber(cpunum);
+        for(int i = 0; i < cpunum.length; i++){
+            System.out.print(cpunum[i]);
+        }
+        while(strikeballcount[0] != 3){
+            try {
+                ply.inputplayernum(playernum);
+            }catch(IllegalArgumentException e){
+                System.out.println(e.toString());
+            }
+            for(int i = 0; i < playernum.length; i++){
+                System.out.println(playernum[i]);
+            }
+            sbc.countStrikeBall(cpunum,playernum, strikeballcount);
+            sbc.outputresult(strikeballcount);
+        }
+        if(strikeballcount[0] == 3){
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        }
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
+    }
+
+}
