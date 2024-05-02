baadab8b1501143c9f75496802049e6937bb1fcf
dong_yxxn <g0326kdy@gmail.com>
2024-05-02 19:47:40 +0900

    initializing

---
+++ src/main/java/Application.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Application {
+
+    public static void main(String[] args) {
+        int flag=1;
+        Scanner sc = new Scanner(System.in);
+        while(flag==1){
+
+        }
+    }
+}
