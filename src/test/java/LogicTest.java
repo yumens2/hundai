d2b44d864b1e7ccf249bae82993ae8e5dccb9618
Jeje <jjh4450jjh4450@gmail.com>
2024-05-03 00:55:44 +0900

    test(LogicTest.java): add test logic for LogicTest.java
    스트라이크를 검증 코드를 작성함
    
    BREAKING CHANGE: 테스트의 용이성을 높이기 위해 generateValidNumbers(), safeGetDigit(), modifyNumber() 메소드를 새롭게 추가함. 해당 변경사항에 대한 검증을 위해 shouldGenerateValidNumbers(), shouldModifyNumberCorrectly() 메소드도 작성됨.
    - generateValidNumbers: 중복되지 않는 3자리 수 배열을 생성하며, shouldGenerateValidNumbers() 메소드를 통해 이 조건이 만족되는지 검증됨.
    - safeGetDigit: 세 자리 정수의 각 자릿수를 추출하여 배열로 반환함. 이 메소드는 Utils.getDigits와 독립적으로 작동하며, 테스트의 안정성을 높임. 함수의 단순함으로 인해 별도의 검증 함수는 필요하지 않음.
    - modifyNumber: 주어진 idx와 (idx + 1) % 3번째의 수를 교환함. 중복되는 숫자가 없는 환경에서 볼 카운트는 2, 스트라이크 카운트는 1이 됨. shouldModifyNumberCorrectly()를 통해 함수의 정확성을 검증함.

--- src/test/java/LogicTest.java
+++ src/test/java/LogicTest.java
@@ -7,9 +7,110 @@ import org.junit.jupiter.api.Test;
 
 class LogicTest {
 
+    private ArrayList<Integer> validNumbers;
+
+    /**
+     * Set up the valid numbers for testing
+     */
+    @BeforeEach
+    void setUp() {
+        validNumbers = generateValidNumbers();
+    }
+
+    /**
+     * Generate valid numbers for testing<br> This numbers follow below rules<br> - 3 digits<br> -
+     * All digits are different<br> - No digit is 0<br>
+     *
+     * @return list of valid numbers
+     */
+    ArrayList<Integer> generateValidNumbers() {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        for (int i = 111; i <= 999; i++) {
+            if (i % 111 == 0) { // skip numbers with all digits the same
+                continue;
+            }
+            if ((i % 100) % 11 == 0 || (i / 10) % 11 == 0
+                || i / 100 == i % 10) { // skip numbers with two digits the same
+                continue;
+            }
+            if (i % 10 != 0 && i / 10 % 10 != 0) { // skip numbers with 0
+                numbers.add(i);
+            }
+        }
+        return numbers;
+    }
+
+    @Test
+    void shouldGenerateValidNumbers() {
+        for (int number : validNumbers) {
+            assertThat(number).isBetween(111, 999);
+            int[] digits = safeGetDigit(number);
+            assertThat(digits[0]).isNotEqualTo(digits[1]);
+            assertThat(digits[1]).isNotEqualTo(digits[2]);
+            assertThat(digits[2]).isNotEqualTo(digits[0]);
+            assertThat(digits[0]).isNotEqualTo(0);
+            assertThat(digits[1]).isNotEqualTo(0);
+            assertThat(digits[2]).isNotEqualTo(0);
+        }
+    }
+
+    void shuldModifyNumberCorrectly() {
+        assertThat(modifyNumber(123, 0)).isEqualTo(213);
+        assertThat(modifyNumber(123, 1)).isEqualTo(132);
+        assertThat(modifyNumber(123, 2)).isEqualTo(123);
+    }
+
     @Test
     void shouldSetComputerNumberCorrectly() {
         Logic logic = new Logic(123);
         assertThat(logic.getComputerNumber()).isEqualTo(123);
+    }
+
+    @Test
+    void shouldCalculateStrikesCorrectly() {
+        Logic edgeCaseLogic = new Logic(111);
+        assertThat(edgeCaseLogic.isStrike(111)).isEqualTo(3);
+        for (Integer number : validNumbers) {
+            Logic logic = new Logic(number);
+            assertThat(logic.isStrike(number)).isEqualTo(3);
+            for (int j = 0; j < 3; j++) {
+                int modifiedNumber = modifyNumber(number, j);
+                logic.setComputerNumber(modifiedNumber);
+                assertThat(logic.isStrike(number)).isEqualTo(1);
+            }
+        }
+    }
+
+    /**
+     * Extracts each digit from a three-digit integer and returns them as an array. This method
+     * operates independently from {@link Utils#getDigits} and is guaranteed to function correctly
+     * with three-digit numbers.
+     *
+     * @param number number to get digits
+     * @return array of digits
+     */
+    private int[] safeGetDigit(int number) {
+        int[] ret = new int[3];
+        ret[0] = number / 100;
+        ret[1] = number / 10 % 10;
+        ret[2] = number % 10;
+        return ret;
+    }
+
+
+    /**
+     * Adjust the numbers to match the 2 balls, 1 strike condition. swap the idx-th digit with the
+     * (idx + 1) % 3-th digit.
+     *
+     * @param number number to modify
+     * @param index  index of the digit to increment by 1
+     * @return modified number
+     */
+    private int modifyNumber(int number, int index) {
+        int[] digits = safeGetDigit(number);
+        int tmp = digits[index];
+        digits[index] = digits[(index + 1) % 3];
+        digits[(index + 1) % 3] = tmp;
+        return digits[0] * 100 + digits[1] * 10 + digits[2];
     }
 }
