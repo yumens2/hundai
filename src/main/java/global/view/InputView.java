d70466638b47f5a9a339f6025b8e460bb39f5f6f
momnpa333 <momnpa333@gmail.com>
2024-05-03 00:13:46 +0900

    feat(InputView.java, OutputView.java): global View 게임 종료 관련 view 제작

--- src/main/java/global/view/InputView.java
+++ src/main/java/global/view/InputView.java
@@ -3,13 +3,16 @@ package global.view;
 
 import java.util.Scanner;
 
-public class InputViewHandler {
+public class InputView {
 
     private final static Scanner scanner = new Scanner(System.in);
     private final static String INPUT_GAME_NUMBER_MESSAGE = "게임 번호를 입력해주세요 : ";
+    private final static String END_GAME_MESSAGE = "게임기를 종료 합니다.";
 
     public static String printInputGameNumber() {
         System.out.println(INPUT_GAME_NUMBER_MESSAGE);
         return scanner.nextLine();
     }
+
+
 }
--- src/main/java/global/view/OutputView.java
+++ src/main/java/global/view/OutputView.java
@@ -4,10 +4,12 @@ import game.gamePack.GamePack;
 import global.config.GamePackConfig;
 import java.util.List;
 
-public class MainViewHandler {
+public class OutputView {
 
     private static final String GAME_OPEN_MESSAGE = "환영합니다.";
     private static final String GAME_END_SELECT = "0. 종료";
+    private static final String GAME_LIST = "게임 목록";
+    private static final String END_GAME_MESSAGE = "게임기를 종료 합니다.";
 
     public static void printGameOpenMessage() {
         System.out.println(GAME_OPEN_MESSAGE);
@@ -16,10 +18,16 @@ public class MainViewHandler {
     public static void printGameList() {
         List<GamePack> gamePackList = GamePackConfig.getGamePacks();
 
+        System.out.println(GAME_LIST);
         for (int i = 0; i < gamePackList.size(); i++) {
             System.out.println((i + 1) + ". " + gamePackList.get(i).getGameName());
         }
         System.out.println(GAME_END_SELECT);
     }
+
+    public static void printEndGameMessage() {
+        System.out.println(END_GAME_MESSAGE);
+    }
+
 
 }
