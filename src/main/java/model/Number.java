f5a9bb58dd7647e57cac8117e4c1d69b998ddaec
anacnoda77 <sm1108shin@naver.com>
2024-05-04 00:17:03 +0900

    Feat(Number.java): 메소드 추가
    배열 형태의 numbers를 int로 변환하는 메소드를 추가

--- src/main/java/model/Number.java
+++ src/main/java/model/Number.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.Arrays;
+
 public class Number {
 
     private final int[] numbers;
@@ -10,5 +12,9 @@ public class Number {
 
     public int[] getNumbers() {
         return numbers;
+    }
+
+    public int toInt() {
+        return numbers[0]*100 + numbers[1]*10 + numbers[2];
     }
 }
