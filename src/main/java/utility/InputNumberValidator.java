2e01a0cda8d47ad047504dcdfbea406035c19116
HSHwan <tnghks2866@gmail.com>
2024-05-03 14:37:20 +0900

    refactor: 구체적인 구현 메서드를 다른 클래스로 분리

--- src/main/java/utility/InputNumberValidator.java
+++ src/main/java/utility/InputNumberValidator.java
@@ -2,31 +2,25 @@ package utility;
 
 public class InputNumberValidator {
 
-    public static void isPlayerNumberCorrect(String playerNumber) throws IllegalArgumentException {
-        isStringLengthCorrect(playerNumber);
-        isStringNumeric(playerNumber);
-        isDifferentDigitNumber(playerNumber);
-    }
-
-    private static void isStringLengthCorrect(String string) {
-        if (string.length() != 3) {
+    protected static void isStringLengthCorrect(String string, int stringLength) {
+        if (string.length() != stringLength) {
             throw new IllegalArgumentException();
         }
     }
 
-    private static void isStringNumeric(String string) throws IllegalArgumentException {
+    protected static void isStringNumeric(String string) throws IllegalArgumentException {
         for (int i = 0; i < string.length(); i++) {
             isCharacterNumeric(string.charAt(i));
         }
     }
 
-    private static void isCharacterNumeric(char singleChar) {
-        if (!Character.isDigit(singleChar) || singleChar == '0') {
+    private static void isCharacterNumeric(char singleCharacter) {
+        if (!Character.isDigit(singleCharacter) || singleCharacter == '0') {
             throw new IllegalArgumentException();
         }
     }
 
-    private static void isDifferentDigitNumber(String number) {
+    protected static void isDifferentDigitNumber(String number) {
         for (int i = 0; i < number.length(); i++) {
             if (getDigitCount(number, number.charAt(i)) != 1) {
                 throw new IllegalArgumentException();
@@ -38,11 +32,5 @@ public class InputNumberValidator {
         return Math.toIntExact(number.chars()
                 .filter((ch) -> (ch == digit))
                 .count());
-    }
-
-    public static void isRestartNumberCorrect(String selectionNumber) {
-        if (!selectionNumber.equals("1") && !selectionNumber.equals("2")) {
-            throw new IllegalArgumentException();
-        }
     }
 }
