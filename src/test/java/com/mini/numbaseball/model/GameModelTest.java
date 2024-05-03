0dcbd1537b5c73bab2702570fc5123bc270ca5a2
wgrgwg <kyoeng14697@naver.com>
2024-05-04 01:39:13 +0900

    test(model): add countStrikeTest
    Add countStrikeTest to ensure that countStrike method counts strike
    correctly.

--- src/test/java/com/mini/numbaseball/model/GameModelTest.java
+++ src/test/java/com/mini/numbaseball/model/GameModelTest.java
@@ -14,18 +14,29 @@ class GameModelTest {
 
         int answer = gameModel.getAnswer();
 
-        // 값 초기화 확인
+        // 값 초기화 테스트
         assertThat(answer).isNotEqualTo(0);
 
-        // 세 자리수 확인
+        // 세 자리수 테스트
         assertThat(Integer.toString(answer)).matches("[1-9]{3}");
 
-        // 자리수 중복 확인
-        int first = answer/100;
-        int second = (answer/10)%10;
-        int third = answer%10;
+        // 자리수 중복 테스트
+        int first = answer / 100;
+        int second = (answer / 10) % 10;
+        int third = answer % 10;
         assertThat(first).isNotEqualTo(second);
         assertThat(second).isNotEqualTo(third);
         assertThat(first).isNotEqualTo(third);
+    }
+
+    @Test
+    void countStrikeTest() {
+        GameModel gameModel = new GameModel();
+
+        // 0-3 strike 개수 카운팅 테스트
+        assertThat(gameModel.countStrike(123, 456)).isEqualTo(0);
+        assertThat(gameModel.countStrike(123, 145)).isEqualTo(1);
+        assertThat(gameModel.countStrike(123, 523)).isEqualTo(2);
+        assertThat(gameModel.countStrike(987, 987)).isEqualTo(3);
     }
 }
