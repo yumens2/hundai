55ac40c8af2b932b73c00dac0c421a9a04cccb4c
NextrPlue <98487008+NextrPlue@users.noreply.github.com>
2024-05-03 20:25:25 +0900

    feat(InputManager.java): InputManager 제작

--- src/main/java/BaseballGame/InputManager.java
+++ src/main/java/BaseballGame/InputManager.java
@@ -1,4 +1,21 @@
 package BaseballGame;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class InputManager {
+    private Scanner scanner = new Scanner(System.in);
+
+    public List<String> getValidInput() {
+        System.out.print("숫자를 입력해 주세요 : ");
+        String input = scanner.nextLine();
+        if (!input.matches("\\d{3}")) {
+            throw new IllegalArgumentException("잘못된 값을 입력하였습니다.");
+        }
+        return List.of(input.split(""));
+    }
+
+    public void closeScanner() {
+        scanner.close();
+    }
 }
