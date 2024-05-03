af44af6571ba65f735dc10ded59ace277bfe67e7
humpose <yjpx416@gamil.com>
2024-05-04 08:59:02 +0900

    숫자 비교해서 스트라이크와 볼 갯수 측정 기능

--- src/main/java/ScoreCalculator.java
+++ src/main/java/ScoreCalculator.java
@@ -1,19 +1,17 @@
-package Game;
-
 import java.util.List;
 
-//사용자의 숫자와 컴퓨터의 숫자를 비교해 strike와 ball의 갯수를 알려주는 기능
 public class ScoreCalculator {
     public static int[] calculateScore(List<Integer> userNumbers, List<Integer> computerNumbers) {
-        int strikes = 0;
-        int balls = 0;
+        int strike = 0;
+        int ball = 0;
         for (int i = 0; i < 3; i++) {
             if (userNumbers.get(i).equals(computerNumbers.get(i))) {
-                strikes++;
-            } else if (computerNumbers.contains(userNumbers.get(i))) {
-                balls++;
+                strike++;
+            }
+            else if (computerNumbers.contains(userNumbers.get(i))) {
+                ball++;
             }
         }
-        return new int[]{strikes, balls};
+        return new int[]{strike, ball};
     }
 }
