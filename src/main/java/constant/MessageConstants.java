7d26ac90ecb7decf95476187f42607f29a3b51e6
Gyumin Sim <gyu2781@gmail.com>
2024-05-04 02:41:44 +0900

    feat(constant): 메시지 상수 작성

---
+++ src/main/java/constant/MessageConstants.java
@@ -0,0 +1,10 @@
+package constant;
+
+public final class MessageConstants {
+    public static final String GUESS_MESSAGE = "숫자를 입력해주세요 : ";
+    public static final String GUESS_ERROR_MESSAGE = "올바른 숫자를 입력하세요.";
+    public static final String GUESS_DUPLICATED_ERROR_MESSAGE = "중복되지 않은 숫자를 입력하세요.";
+
+    private MessageConstants() {
+    }
+}
