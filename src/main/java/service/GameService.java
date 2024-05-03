0df45b1d37a236f50d308685bfa20b23fbbcc60a
anacnoda77 <sm1108shin@naver.com>
2024-05-03 18:57:33 +0900

    Feat(GameService.java): 게임 지속 여부

--- src/main/java/service/GameService.java
+++ src/main/java/service/GameService.java
@@ -75,6 +75,12 @@ public class GameService {
     public boolean isAnswered(Hints hints) {
         return hints.getStrike() == 3 && hints.getBall() == 0;
     }
-    
+
+    public boolean isContinuing() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+        return getInput() == 2;
+    }
 
 }
