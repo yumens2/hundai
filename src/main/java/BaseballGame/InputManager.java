27cae890fd8a01c533a34dfff005129ef63a2fe7
NextrPlue <98487008+NextrPlue@users.noreply.github.com>
2024-05-03 22:11:14 +0900

    feat(InputManager.java): 입력값 규칙 더욱 엄격하게 수정

--- src/main/java/BaseballGame/InputManager.java
+++ src/main/java/BaseballGame/InputManager.java
@@ -1,7 +1,6 @@
 package BaseballGame;
 
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class InputManager {
     private Scanner scanner = new Scanner(System.in);
@@ -9,10 +8,29 @@ public class InputManager {
     public List<String> getValidInput() {
         System.out.print("숫자를 입력해 주세요 : ");
         String input = scanner.nextLine();
-        if (!input.matches("\\d{3}")) {
+        if (!isValidInput(input)) {
             throw new IllegalArgumentException("잘못된 값을 입력하였습니다.");
         }
         return List.of(input.split(""));
+    }
+
+    public int getUserChoice() {
+        String userChoice = scanner.nextLine();
+        if (Objects.equals(userChoice, "1") || Objects.equals(userChoice, "2")) {
+            throw new IllegalArgumentException("잘못된 값을 입력하였습니다.");
+        }
+        return Integer.parseInt(userChoice);
+    }
+
+    private boolean isValidInput(String input) {
+        if (!input.matches("[1-9]{3}")) {
+            return false;
+        }
+        Set<Character> uniqueDigits = new HashSet<>();
+        for (char c : input.toCharArray()) {
+            uniqueDigits.add(c);
+        }
+        return uniqueDigits.size() == 3;
     }
 
     public void closeScanner() {
