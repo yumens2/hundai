c805b3de877296088c7b19db3015e38a145cfefa
coco3x <inferior3x@gmail.com>
2024-05-04 02:28:41 +0900

    add: 예외 메세지(종료 상태 아닐 때) 추가

--- src/main/java/precourse/game/Messages.java
+++ src/main/java/precourse/game/Messages.java
@@ -7,6 +7,7 @@ public enum Messages {
     ALL_STRIKE_FORMAT("%d개의 숫자를 모두 맞추셨습니다! 게임종료"),
 
     NOT_STARTED("게임이 시작되지 않았습니다."),
+    NOT_ENDED("게임이 끝난 상태가 아닙니다."),
     ALREADY_STARTED("게임이 시작된 상태입니다."),
     INVALID_NUMBER_LENGTH("숫자 길이는 1 ~ 9여야 합니다."),
     INVALID_COMMAND("올바르지 않은 명령어입니다."),
