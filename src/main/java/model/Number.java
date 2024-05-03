575b30d5c1fb7b6715f13f069bfef36cde3be2f6
anacnoda77 <sm1108shin@naver.com>
2024-05-03 18:54:27 +0900

    Feat(Number.java): 숫자 모델 생성

---
+++ src/main/java/model/Number.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Number {
+
+    private final int[] numbers;
+
+    public Number(int[] numbers) {
+        this.numbers = numbers;
+    }
+
+    public int[] getNumbers() {
+        return numbers;
+    }
+}
