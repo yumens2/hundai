a4213e30a188343266843882018c0d24e00d1e02
Gyumin Sim <gyu2781@gmail.com>
2024-05-04 03:10:09 +0900

    feat(Application.java): 전체 게임로직 작성
    - 구현한 기능들을 바탕으로 전체 게임로직 Application에 정의

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,4 +1,26 @@
+import domain.Computer;
+import domain.Result;
+
+import java.util.Scanner;
+
 public class Application {
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final Computer computer = new Computer();
+    private static final InputHandler inputHandler = new InputHandler(scanner);
+    private static final Result result = new Result();
+
     public static void main(String[] args) {
+        runGame();
+        scanner.close();
+    }
+
+    private static void runGame(){
+        computer.generateRandomNumbers();
+
+        while(!result.isCorrect()){
+            String input = inputHandler.getUserGuess();
+            result.calculate(input, computer.getNumber());
+            System.out.println(result);
+        }
     }
 }
