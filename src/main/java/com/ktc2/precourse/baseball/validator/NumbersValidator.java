cff90b193ade0fa63bcb060f9a27ee82dec22e80
lja3723 <lja3723@gmail.com>
2024-05-04 02:08:08 +0900

    style: change code position

--- src/main/java/com/ktc2/precourse/baseball/validator/NumbersValidator.java
+++ src/main/java/com/ktc2/precourse/baseball/validator/NumbersValidator.java
@@ -11,22 +11,6 @@ public class NumbersValidator implements Validator<String> {
         return instance;
     }
 
-    @Override
-    public boolean isValid(String str) {
-        if (str == null) {
-            return false;
-        }
-        if (str.length() != Numbers.LENGTH) {
-            return false;
-        }
-        if (isNotDigit(str)) {
-            return false;
-        }
-
-        //숫자 문자열에 중복된 숫자가 있으면 유효하지 않음.
-        return hasDuplicatedDigit(str);
-    }
-
     //모든 문자가 1~9의 숫자로 이뤄지지 않은 경우
     private boolean isNotDigit(String str) {
         return !str.chars().allMatch(s -> Character.isDigit(s) && s != '0');
@@ -43,5 +27,21 @@ public class NumbersValidator implements Validator<String> {
         }
 
         return true;
+    }
+
+    @Override
+    public boolean isValid(String str) {
+        if (str == null) {
+            return false;
+        }
+        if (str.length() != Numbers.LENGTH) {
+            return false;
+        }
+        if (isNotDigit(str)) {
+            return false;
+        }
+
+        //숫자 문자열에 중복된 숫자가 있으면 유효하지 않음.
+        return hasDuplicatedDigit(str);
     }
 }
