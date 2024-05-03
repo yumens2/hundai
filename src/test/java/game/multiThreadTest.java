788572fab986a1b06a0ad4c72c4024f322d7e079
momnpa333 <momnpa333@gmail.com>
2024-05-03 22:23:24 +0900

    test(multiThreadTest): 스레드 종료 로직 추가

--- src/test/java/game/multiThreadTest.java
+++ src/test/java/game/multiThreadTest.java
@@ -7,6 +7,7 @@ import game.gamePack.BaseballGame.service.BaseballGameService;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -16,10 +17,10 @@ public class multiThreadTest {
 
     @Test
     @DisplayName("멀티스레드환경에서 BaseballService 개수 1개인지 확인")
-    void multiThreadBaseballServiceTest() {
+    void multiThreadBaseballServiceTest() throws InterruptedException {
         // given
         Map<String, BaseballGameService> baseballGameServiceMap = new ConcurrentHashMap<>();
-        var executorService = Executors.newFixedThreadPool(1000);
+        var executorService = Executors.newFixedThreadPool(10);
 
         // When
         IntStream.range(0, 10000).forEach((index) -> executorService.submit(() -> {
@@ -28,12 +29,16 @@ public class multiThreadTest {
         }));
 
         // Then
+
+        executorService.shutdown();
+        executorService.awaitTermination(10, TimeUnit.SECONDS);
+
         Assertions.assertEquals(1, baseballGameServiceMap.size());
     }
 
     @Test
     @DisplayName("멀티스레드환경에서 Computer 개수 1개인지 확인")
-    void multiThreadComputerTest() {
+    void multiThreadComputerTest() throws InterruptedException {
         // given
         Map<String, Computer> computerMap = new ConcurrentHashMap<>();
         var executorService = Executors.newFixedThreadPool(1000);
@@ -44,12 +49,14 @@ public class multiThreadTest {
         }));
 
         // Then
+        executorService.shutdown();
+        executorService.awaitTermination(10, TimeUnit.SECONDS);
         Assertions.assertEquals(1, computerMap.size());
     }
 
     @Test
     @DisplayName("멀티스레드환경에서 Result 개수 1개인지 확인")
-    void multiThreadResultTest() {
+    void multiThreadResultTest() throws InterruptedException {
         // given
         Map<String, Result> baseballGameResultMap = new ConcurrentHashMap<>();
         var executorService = Executors.newFixedThreadPool(1000);
@@ -61,6 +68,8 @@ public class multiThreadTest {
         }));
 
         // Then
+        executorService.shutdown();
+        executorService.awaitTermination(10, TimeUnit.SECONDS);
         Assertions.assertEquals(1, baseballGameResultMap.size());
     }
 
