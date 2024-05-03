29a43a74785073ea96250383839ab7480aa243a6
min1018 <102647148+min1018@users.noreply.github.com>
2024-05-03 15:34:06 +0900

    Feat: 메인 추가

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -3,14 +3,14 @@ package baseball.view;
 import java.util.Scanner;
 
 public class InputView {
-    private static Scanner scanner = new Scanner(System.in);
+    private static final Scanner scanner = new Scanner(System.in);
     public static String printGameStart() {
         System.out.print("숫자를 입력해 주세요 : ");
         return scanner.nextLine();
     }
 
-    public static Integer printRestartRequest() {
+    public static String printRestartRequest() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-        return scanner.nextInt();
+        return scanner.nextLine();
     }
 }
