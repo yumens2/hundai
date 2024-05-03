7d30e72f951cd780948bea8e036b3e3f99a1eb05
suhyeon <nucc1186@gamil.com>
2024-05-03 11:29:01 +0900

    test(TestController) : chage
    Generator Test 수정

--- src/test/java/TestController.java
+++ src/test/java/TestController.java
@@ -1,18 +1,20 @@
 import model.BaseballNumberGenerator;
-import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class TestController {
+
     @DisplayName("Generator Test")
     @Test
-    void BaseballNumberSizeTest(){
+    void BaseballNumberRangeTest(){
         //given
         final BaseballNumberGenerator baseballNumberGenerator = new BaseballNumberGenerator();
         //when
-        final List<Integer> baseballNumber = baseballNumberGenerator.generate();
+        final ArrayList<Integer> baseballNumber = baseballNumberGenerator.generate();
         //then
         assertThat(baseballNumber.stream().allMatch(v ->v>=1 && v <=9)).isTrue();
     }
