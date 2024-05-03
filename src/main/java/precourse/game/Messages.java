2bc438e158a662d20d412ac0fd0a1f0851fcff65
coco3x <inferior3x@gmail.com>
2024-05-04 01:43:55 +0900

    add: 예외 메세지

--- src/main/java/precourse/game/Messages.java
+++ src/main/java/precourse/game/Messages.java
@@ -7,7 +7,14 @@ public enum Messages {
     ALL_STRIKE_FORMAT("%d개의 숫자를 모두 맞추셨습니다! 게임종료"),
 
     NOT_STARTED("게임이 시작되지 않았습니다."),
-    ALREADY_STARTED("게임이 시작된 상태입니다.");
+    ALREADY_STARTED("게임이 시작된 상태입니다."),
+    INVALID_NUMBER_LENGTH("숫자 길이는 1 ~ 9여야 합니다."),
+    INVALID_COMMAND("올바르지 않은 명령어입니다."),
+    NOT_MATCHED_LENGTH("길이가 맞지 않습니다"),
+    INVALID_NUMBER("올바른 숫자가 아닙니다"),
+    NOT_UNIQUE_DIGITS("중복된 숫자가 있습니다"),
+
+    ;
 
     private final String message;
 
