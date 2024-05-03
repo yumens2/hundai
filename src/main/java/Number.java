0631a50020c0a7f05255d843d71fafce5c2f0721
hyyyh0x <hyyyh0x@gmail.com>
2024-05-04 00:46:59 +0900

    feat : README no.3 클래스에 랜덤으로 숫자 생성하는 함수 생성(생성자 사용)

--- src/main/java/Number.java
+++ src/main/java/Number.java
@@ -8,6 +8,19 @@ public class Number{
     int MY_NUM;
     int BALL;
     int STRIKE;
+    public Number() {
+        FLAG=true;
+        BALL=0; STRIKE=0;
+        RANDOM_NUM[0]=(int)(10*Math.random());
+        while(true){
+            RANDOM_NUM[1]=(int)(10*Math.random());
+            if(RANDOM_NUM[0]!=RANDOM_NUM[1]) break;
+        }
+        while(true){
+            RANDOM_NUM[2]=(int)(10*Math.random());
+            if(RANDOM_NUM[0]!=RANDOM_NUM[2]&&RANDOM_NUM[1]!=RANDOM_NUM[2]) break;
+        }
+    }
     public void chkEquality() throws Exception {
         boolean FLAGNUM1=true;
         while(FLAGNUM1){
