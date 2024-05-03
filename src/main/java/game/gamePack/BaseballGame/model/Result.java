7f49019c335ed215c2e5493af6e5c8240cb6e72a
momnpa333 <momnpa333@gmail.com>
2024-05-03 15:26:52 +0900

    refact(Result.java): 멀티쓰레드 관련 이슈 해결

--- src/main/java/game/gamePack/BaseballGame/model/Result.java
+++ src/main/java/game/gamePack/BaseballGame/model/Result.java
@@ -6,16 +6,29 @@ import java.util.stream.IntStream;
 
 public class Result {
 
-    private static Result result;
+    //private static Result result;
 
     private Result() {
     }
 
+//    public static Result of() {
+//        if (result == null) {
+//            result = new Result();
+//        }
+//        return result;
+//    }
+
+    /**
+     * static inner class를 이용한 싱글톤 패턴 클래스가 로딩될 때 생성되기 때문에 thread-safe 함 lazy loading이 가능하며, of()메소드가
+     * 호출되기 전까지는 객체가 생성되지 않음
+     */
+    private static class ResultHolder {
+
+        private static final Result result = new Result();
+    }
+
     public static Result of() {
-        if (result == null) {
-            result = new Result();
-        }
-        return result;
+        return ResultHolder.result;
     }
 
     public boolean getOutCome(List<Integer> computerNumbers, List<Integer> guessNumber) {
