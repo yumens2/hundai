2c6a3b54f2f2046790c0c82e7c65c87b14b472a2
coco3x <inferior3x@gmail.com>
2024-05-04 01:25:28 +0900

    add: 상태 예외 메세지 추가

--- src/main/java/precourse/game/Messages.java
+++ src/main/java/precourse/game/Messages.java
@@ -4,7 +4,10 @@ public enum Messages {
     REQUIRE_NUMBERS("숫자를 입력해주세요."),
     REQUIRE_END_COMMAND("게임을 새로 시작하려면 "+ EndCommand.CONTINUE.getCommand()
             + ", 종료하려면 " + EndCommand.STOP.getCommand() + "를 입력하세요."),
-    ALL_STRIKE_FORMAT("%d개의 숫자를 모두 맞추셨습니다! 게임종료");
+    ALL_STRIKE_FORMAT("%d개의 숫자를 모두 맞추셨습니다! 게임종료"),
+
+    NOT_STARTED("게임이 시작되지 않았습니다."),
+    ALREADY_STARTED("게임이 시작된 상태입니다.");
 
     private final String message;
 
