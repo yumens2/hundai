774bd0d63b71b72f273638cf86f9bed35c82dcd5
Gyumin Sim <gyu2781@gmail.com>
2024-05-04 03:27:24 +0900

    feat(domain): 게임 재시작 여부 수집
    - 입력 값 검증 실패 시 예외 발생

--- src/main/java/InputHandler.java
+++ src/main/java/InputHandler.java
@@ -20,6 +20,15 @@ public class InputHandler {
         return input;
     }
 
+    public boolean isReplay() {
+        System.out.println(REPLAY_MESSAGE);
+        String input = scanner.nextLine();
+
+        validateReplay(input);
+
+        return input.equals("1");
+    }
+
     private void validateGuess(String input) {
         if (!input.matches("[1-9]{3}")) {
             throw new IllegalArgumentException(GUESS_ERROR_MESSAGE);
@@ -28,6 +37,12 @@ public class InputHandler {
             Set<Character> inputSet = Set.of(input.charAt(0), input.charAt(1), input.charAt(2));
         } catch (IllegalArgumentException e) {
             throw new IllegalArgumentException(GUESS_DUPLICATED_ERROR_MESSAGE);
+        }
+    }
+
+    private void validateReplay(String input) {
+        if (!input.matches("[1-2]")) {
+            throw new IllegalArgumentException(REPLAY_ERROR_MESSAGE);
         }
     }
 }
--- src/main/java/constant/MessageConstants.java
+++ src/main/java/constant/MessageConstants.java
@@ -10,6 +10,9 @@ public final class MessageConstants {
     public static final String RESULT_NOTHING = "낫싱";
     public static final String RESULT_CORRECT_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
 
+    public static final String REPLAY_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static final String REPLAY_ERROR_MESSAGE = "1 또는 2를 입력하세요.";
+
     private MessageConstants() {
     }
 }
