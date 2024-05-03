bebd37d29257f8c643aa3236cc74d1077e4deee6
NextrPlue <98487008+NextrPlue@users.noreply.github.com>
2024-05-03 21:18:30 +0900

    feat(ScoreEvaluator.java): ScoreEvaluator 제작

--- src/main/java/BaseballGame/ScoreEvaluator.java
+++ src/main/java/BaseballGame/ScoreEvaluator.java
@@ -1,4 +1,37 @@
 package BaseballGame;
 
+import java.util.List;
+
 public class ScoreEvaluator {
+    public String getScore(List<String> guess, String answer) {
+        int strike = countStrikes(guess, answer);
+        int ball = countBall(guess, answer);
+
+        if (strike == 0 && ball == 0) {
+            return "낫싱";
+        }
+        else {
+            return "%s볼 %s스트라이크".formatted(strike, ball);
+        }
+    }
+
+    private int countStrikes(List<String> guess, String answer) {
+        int strike = 0;
+        for (int i = 0; i < 3; i++) {
+            if (answer.indexOf(guess.get(i)) == i) {
+                strike++;
+            }
+        }
+        return strike;
+    }
+
+    private int countBall(List<String> guess, String answer) {
+        int ball = 0;
+        for (int i = 0; i < 3; i++) {
+            if (answer.indexOf(guess.get(i)) != i && answer.contains(guess.get(i))) {
+                ball++;
+            }
+        }
+        return ball;
+    }
 }
