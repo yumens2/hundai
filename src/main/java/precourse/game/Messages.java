f85fbd01f3eb5ee2185b1139e63bf078d0299eeb
coco3x <inferior3x@gmail.com>
2024-05-03 23:57:45 +0900

    add: 게임 종료 후 나오는 메세지

--- src/main/java/precourse/game/Messages.java
+++ src/main/java/precourse/game/Messages.java
@@ -2,9 +2,9 @@ package precourse.game;
 
 public enum Messages {
     REQUIRE_NUMBERS("숫자를 입력해주세요."),
+    REQUIRE_END_COMMAND("게임을 새로 시작하려면 "+ EndCommand.CONTINUE.getCommand()
+            + ", 종료하려면 " + EndCommand.STOP.getCommand() + "를 입력하세요."),
     ALL_STRIKE_FORMAT("%d개의 숫자를 모두 맞추셨습니다! 게임종료");
-
-    //TODO: EndCommand 구현하고 메세지 추가
 
     private final String message;
 
