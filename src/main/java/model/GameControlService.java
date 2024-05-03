47a398d3ec5c38693d259c0ed2bb0103a34bf1c4
suhyeon <nucc1186@gamil.com>
2024-05-03 09:35:11 +0900

    feature(GameControlServiceClass) : add PrintQuestion

--- src/main/java/model/GameControlService.java
+++ src/main/java/model/GameControlService.java
@@ -1,2 +1,9 @@
-package model;public class GameControlService {
+package model;
+
+public class GameControlService {
+    public void start(){
+    }
+    public void printQuestion(){
+        System.out.print("숫자를 입력해 주세요 : ");
+    }
 }
