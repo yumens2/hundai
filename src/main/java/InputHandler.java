1f3633a38e0d94f1da96003acb6593d3793ac827
Gyumin Sim <gyu2781@gmail.com>
2024-05-04 02:41:04 +0900

    feat(InputHandler.java): 플레이어로부터 3개의 숫자 수집
    - 플레이어의 입력을 처리하는 InputHandler 클래스에 구현
    - 입력 값 조건에 맞게 검증

---
+++ src/main/java/InputHandler.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.util.Set;
+
+import static constant.MessageConstants.*;
+
+public class InputHandler {
+    private final Scanner scanner;
+
+    public InputHandler(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public String getUserGuess() {
+        System.out.print(GUESS_MESSAGE);
+
+        String input = scanner.nextLine();
+
+        validateGuess(input);
+
+        return input;
+    }
+
+    private void validateGuess(String input) {
+        if (!input.matches("[1-9]{3}")) {
+            throw new IllegalArgumentException(GUESS_ERROR_MESSAGE);
+        }
+        try {
+            Set<Character> inputSet = Set.of(input.charAt(0), input.charAt(1), input.charAt(2));
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException(GUESS_DUPLICATED_ERROR_MESSAGE);
+        }
+    }
+}
