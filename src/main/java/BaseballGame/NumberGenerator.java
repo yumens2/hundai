17049eb7dc0557973f03d198275ef4e7dc715f5b
NextrPlue <98487008+NextrPlue@users.noreply.github.com>
2024-05-03 20:24:05 +0900

    feat(NumberGenerator.java): NumberGenerator 제작

--- src/main/java/BaseballGame/NumberGenerator.java
+++ src/main/java/BaseballGame/NumberGenerator.java
@@ -1,4 +1,17 @@
 package BaseballGame;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class NumberGenerator {
+    public String generateNumber() {
+        List<Integer> numbers = new ArrayList<Integer>();
+        for (int i = 0; i <= 9; i++) {
+            numbers.add(i);
+        }
+        Collections.shuffle(numbers);
+
+        return String.valueOf(numbers.get(0) * 100 + numbers.get(1) * 10 + numbers.get(2));
+    }
 }
