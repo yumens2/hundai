c07a5aea4be72dcabf4129845ae5b38f61df5940
lja3723 <lja3723@gmail.com>
2024-05-04 09:37:47 +0900

    feat: implement MatchResultCreator

--- src/main/java/com/ktc2/precourse/baseball/logic/MatchResultCreator.java
+++ src/main/java/com/ktc2/precourse/baseball/logic/MatchResultCreator.java
@@ -1,4 +1,54 @@
 package com.ktc2.precourse.baseball.logic;
 
+import com.ktc2.precourse.baseball.object.MatchResult;
+import com.ktc2.precourse.baseball.object.Numbers;
+
+/**
+ * 두 Numbers를 비교한 후 MatchResult를 생성하는 클래스
+ */
 public class MatchResultCreator {
+    private static final int[] ret = new int[2];
+    /**
+     * 국소적 비교 결과를 계산한다.
+     * @param number 상위 숫자
+     * @param idx 상위 숫자의 인덱스
+     * @param n 비교 대상 Numbers
+     * @return 국지적 비교 결과
+     */
+    private static int[] getLocalResult (int number, int idx, Numbers n) {
+        ret[0] = 0;
+        ret[1] = 0;
+
+        for (int i = 0; i < Numbers.LENGTH; i++) {
+            if (number != n.get(i)) {
+                continue;
+            }
+            if (idx == i) {
+                ret[0]++;
+            } else {
+                ret[1]++;
+            }
+        }
+
+        return ret;
+    }
+
+    /**
+     * 두 Numbers를 비교하여 숫자야구 비교 결과를 반환한다.
+     * @param n1 비교할 Numbers 1
+     * @param n2 비교할 Numbers 2
+     * @return 비교 결과
+     */
+    public static MatchResult getCompareResult(Numbers n1, Numbers n2) {
+        int strike = 0;
+        int ball = 0;
+
+        for (int i = 0; i < Numbers.LENGTH; i++) {
+            int[] local = getLocalResult(n1.get(i), i, n2);
+            strike += local[0];
+            ball += local[1];
+        }
+
+        return new MatchResult(strike, ball);
+    }
 }
--- src/main/java/com/ktc2/precourse/baseball/object/MatchResult.java
+++ src/main/java/com/ktc2/precourse/baseball/object/MatchResult.java
@@ -1,4 +1,70 @@
 package com.ktc2.precourse.baseball.object;
 
+/**
+ * 매칭 결과를 표현하는 클래스
+ */
 public class MatchResult {
+    private final int strike;
+    private final int ball;
+
+    public MatchResult(int strike, int ball) {
+        this.strike = strike;
+        this.ball = ball;
+    }
+
+    @Override
+    public String toString() {
+        String ret = "";
+        if (strike > 0) {
+            ret += strike + " 스트라이크 ";
+        }
+        if (ball > 0) {
+            ret += ball + " 볼 ";
+        }
+        if (isNothing()) {
+            ret = "낫싱";
+        }
+        return ret;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof MatchResult)) {
+            return false;
+        }
+
+        MatchResult r = (MatchResult) obj;
+        if (this.ball != r.ball) {
+            return false;
+        }
+        if (this.strike != r.strike) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return 10 * strike + ball;
+    }
+
+    /**
+     * 모두 스트라이크인지 확인
+     * @return 모두 스트라이크이면 true
+     */
+    public boolean isAllStrike() {
+        return strike == Numbers.LENGTH;
+    }
+
+    /**
+     * 낫싱인지 확인
+     * @return 낫싱이면 true
+     */
+    public boolean isNothing() {
+        return strike == 0 && ball == 0;
+    }
 }
