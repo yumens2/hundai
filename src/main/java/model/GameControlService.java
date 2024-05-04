94a5bab8b206fdbc3d7cdd025121c35266c9f48a
suhyeon <nucc1186@gamil.com>
2024-05-04 14:11:39 +0900

    feature(GameControlService.java) scanRestart
    add scanRestart feature

--- src/main/java/model/GameControlService.java
+++ src/main/java/model/GameControlService.java
@@ -38,8 +38,20 @@ public class GameControlService {
     }
 
     public void printRestart(){
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 
-
+    public GameControlService scanRestart(){
+        int checkRestart = Integer.parseInt(scanner.nextLine());
+        if(checkRestart == 1){
+            GameControlService gameControlService = new GameControlService();
+            return gameControlService;
+        }
+        else if(checkRestart == 2) {
+            return null;
+        }
+        else
+            throw new IllegalArgumentException("Invalid Input : " + checkRestart);
+    }
 }
