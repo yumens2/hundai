90775bf4c18a885c4fa5a3d84aaefca013c6207b
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-05-04 22:04:13 +0900

    Feat : DuplicateChecking클래스에 대한 테스트 코드 작성(JUnit5)

---
+++ src/test/java/DuplicateCheckingTest.java
@@ -0,0 +1,22 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class DuplicateCheckingTest {
+
+    @Test
+    public void testExistDuplicate() {
+        DuplicateChecking dc = new DuplicateChecking();
+        int[] numArray = {1, 2, 3, 4, 2};
+
+        //주어진 조건이 false로 평가되는지 확인한다.
+        //조건이 false이면 테스트가 통과된다.
+        //numArray에서 인덱스 3에서 중복 항목을 확인할 때 false를 반환해야 한다.
+        assertFalse(dc.ExistDuplicate(numArray, 3));
+
+        //주어진 조건이 true로 평가되는지 확인한다.
+        //조건이 true이면 테스트가 통과된다.
+        //numArray에서 인덱스 4에서 중복 항목을 확인할 때 true를 반환해야 한다.
+        assertTrue(dc.ExistDuplicate(numArray, 4));
+    }
+}
