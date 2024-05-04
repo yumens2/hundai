a806f56f3d5145b89054861018d1035c27f90a3a
박민규 <pmq0101@naver.com>
2024-05-05 03:53:19 +0900

    feat(Computer): 랜덤숫자 생성 기능 추가

--- src/main/java/Computer.java
+++ src/main/java/Computer.java
@@ -1,5 +1,22 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Computer {
     private static final int GAME_NUM = 3;
     private int[] comNum = new int[GAME_NUM + 1];
+    public void createGameNumber(){
+        List<Integer> tmp = new ArrayList<>();
+        for (int i = 1; i <= 9; i++) {
+            tmp.add(i);
+        }
+        Collections.shuffle(tmp);
+        for (int i = 1; i < 4; i++) {
+            this.comNum[i] = tmp.get(i - 1);
+        }
+    }
 
+    public int[] getComNum() {
+        return comNum;
+    }
 }
