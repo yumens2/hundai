39b18bb9c9a5616f9d91bf15740e22957ac1f96e
sunandrabbit <sunandrabbit@gamil.com>
2024-05-04 15:25:02 +0900

    feat : 각종 출력들을 담당하는 기능을 구현

---
+++ src/main/java/OutputView.java
@@ -0,0 +1,35 @@
+public class OutputView {
+
+	private static final String INPUT_NUMBER_REQUEST = "숫자를 입력하세요.";
+	private static final String BALL = "볼";
+	private static final String STRIKE = "스트라이크";
+	private static final String NOTHING = "낫싱(0 볼, 0 스트라이크) 입니다.";
+	private static final String CORRECT = "정답~! 참 잘 했어요!";
+	private static final String RESTART_OR_QUIT_REQUEST = "재시작은 1, 종료는 2를 입력하세요.";
+
+	public static void printNumberInput() {
+		System.out.println(INPUT_NUMBER_REQUEST);
+	}
+
+	public static void printCorrect() {
+		System.out.println(CORRECT);
+	}
+
+	public static void printRestartOrQuit() {
+		System.out.println(RESTART_OR_QUIT_REQUEST);
+	}
+
+	public static void printAnswer(Answer answer) {
+		StringBuilder sb = new StringBuilder();
+
+		int ball = answer.getBall();
+		int strike = answer.getStrike();
+
+		if(ball > 0)  sb.append(ball).append(BALL);
+		if(strike > 0)  sb.append(strike).append(STRIKE);
+		if(ball == 0 && strike == 0) sb.append(NOTHING);
+
+		System.out.println(sb);
+	}
+
+}
