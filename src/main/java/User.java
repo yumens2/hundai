9d9f0ee76259677b8fe3f7735bbeca217145318c
박민규 <pmq0101@naver.com>
2024-05-05 03:06:36 +0900

    feat(User.java): User 생성

---
+++ src/main/java/User.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class User {
+    private static final int GAME_NUM = 3;
+
+    private final Scanner scanner;
+
+    public User(Scanner scanner) {
+        this.scanner = scanner;
+    }
+}
