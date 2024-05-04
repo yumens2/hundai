961bdabd6712be8305e66a4e18cb7884b698200f
KimSongMok <mok0618@naver.com>
2024-05-04 20:56:03 +0900

    NumberBaseBallGame

--- src/main/java/BaseBall/Application.java
+++ src/main/java/BaseBall/Application.java
@@ -1,2 +1,11 @@
-package BaseBall;public class Application {
+package BaseBall;
+public class Application {
+    public static void main(String[] args) {
+        try {
+            new GameController().startGame();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
+
--- src/main/java/Model/InputValidator.java
+++ src/main/java/Model/InputValidator.java
@@ -1,2 +1,10 @@
-package Model;public class InputValidator {
+package Model;
+public class InputValidator {
+    public static boolean isValid(String input) {
+        if (input.length() != 3) return false;
+        for (char ch : input.toCharArray()) {
+            if (!Character.isDigit(ch) || ch == '0') return false;
+        }
+        return input.charAt(0) != input.charAt(1) && input.charAt(0) != input.charAt(2) && input.charAt(1) != input.charAt(2);
+    }
 }
