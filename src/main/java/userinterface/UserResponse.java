aa4d998a97e26e8a2c16b4df78e4b6a9c496a072
Gyumin Sim <gyu2781@gmail.com>
2024-05-04 12:50:11 +0900

    refactor(userinterface): userinterface 분리
    - 기존의 InputHandler.java를 InputValidator, UserRequest, UserResponse로 분리
    - User의 input과 output을 처리하는 클래스 UserRequest, UserResponse 생성
    - User와 상호작용하는 클래스 userinterface로 패키징

---
+++ src/main/java/userinterface/UserResponse.java
@@ -0,0 +1,19 @@
+package userinterface;
+
+import domain.Result;
+
+import static userinterface.constant.UserMessageConstants.*;
+public class UserResponse {
+    public void printGuessMessage() {
+        System.out.print(GUESS_MESSAGE);
+    }
+
+
+    public void printResult(Result result) {
+        System.out.println(result);
+    }
+
+    public void printReplayMessage() {
+        System.out.println(REPLAY_MESSAGE);
+    }
+}
