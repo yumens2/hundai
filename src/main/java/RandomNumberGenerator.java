05d5fcc673e9a6688dc94cca098801986294cd49
student-p <paulmoon00@naver.com>
2024-05-04 20:13:23 +0900

    feat (RandomNumberGenerator.java): 랜덤한 숫자를 생성하는 클래스 작성

---
+++ src/main/java/RandomNumberGenerator.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class RandomNumberGenerator {
+
+    public List<Integer> generate(){
+
+        List<Integer> numbers = new ArrayList<>();
+        for(int i = 0; i <= 9; i++){
+            numbers.add(i);
+        }
+
+        Collections.shuffle(numbers);
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < 3; i++){
+            result.add(numbers.get(i));
+        }
+        return result;
+    }
+}
