fde5a67632fffe4b1b2e751590ed6a842b82c410
seoyoung-dev <seoyoung.dev@gmail.com>
2024-05-05 03:57:54 +0900

    feat(BaseballGame): 3자리 수 생성

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,7 @@
 public class Application {
 
     public static void main(String[] args) {
-
+    BaseballGame baseballGame = new BaseballGame();
+    baseballGame.startGame();
     }
 }
--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -1,5 +1,29 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class BaseballGame {
     public void startGame() {
+        System.out.println("Game started");
+        playGame();
+    }
 
+    public void playGame() {
+        int[] targetNumbers = generateRandomNumbers();
+        System.out.println(Arrays.toString(targetNumbers));
+    }
+
+    public static int[] generateRandomNumbers() {
+        ArrayList<Integer> digits = new ArrayList<>();
+        for (int i = 1; i <= 9; i++) {
+            digits.add(i);
+        }
+        Collections.shuffle(digits);
+
+        int[] result = new int[3];
+        for (int i = 0; i < 3; i++) {
+            result[i] = digits.get(i);
+        }
+        return result;
     }
 }
