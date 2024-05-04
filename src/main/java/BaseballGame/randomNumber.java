a00356b381b96bb2b8cbe89d7c53086fb1c20a46
pykido <animoto1@naver.com>
2024-05-04 21:50:39 +0900

    feat(randomNumber.java) : 난수 생성하는 generateRandomNumbers 메서드 구현

---
+++ src/main/java/BaseballGame/randomNumber.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class randomNumber {
+    public ArrayList<Integer> generateRandomNumbers() {
+        ArrayList<Integer> randomNumbers = new ArrayList<>();
+
+        while (randomNumbers.size() < 3) {
+            int number = (int) (Math.random() * 9) + 1;
+            if (!randomNumbers.contains(number)) {
+                randomNumbers.add(number);
+            }
+        }
+
+        return randomNumbers;
+    }
+}
