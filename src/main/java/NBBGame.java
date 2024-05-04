758f4d323cb419a2a59aad9a0fe71904b2daa6f0
Yongjin <yongjin5352@gmail.com>
2024-05-04 14:23:48 +0900

    feat(NBBGame.getUserGuess()) : complete the feature, "숫자 입력 받기"

--- src/main/java/NBBGame.java
+++ src/main/java/NBBGame.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 public class NBBGame {
     private final GameNumber answer;
 
     NBBGame() {
         this.answer = new GameNumber();
+    }
+
+    private static GameNumber getUserGuess() {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        return new GameNumber(input);
     }
 }
