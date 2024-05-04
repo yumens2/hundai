3c9ec4522fb55cbb4eaa261ce64f9cbb08d79f4f
Yongjin <yongjin5352@gmail.com>
2024-05-04 12:10:02 +0900

    feat (GameNumber) : complete the feature, "랜덤 숫자 생성"
    create a constructor for the 'GameNumber' class that doesn't take any arguments and
    initializes the 'gameNumber' of the instance with a valid random Number

--- src/main/java/GameNumber.java
+++ src/main/java/GameNumber.java
@@ -21,6 +21,28 @@ public class GameNumber {
         return digitKindCnt == 3;
     }
 
+    public static String randomGameNumber() {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        for (int i = 0; i < 3; i++) {
+            int chosen = (int) ((arr.length - i) * Math.random()) + i;
+
+            int temp = arr[chosen];
+            arr[chosen] = arr[i];
+            arr[i] = temp;
+        }
+
+        String result = "";
+        for (int i = 0; i < 3; i++) {
+            result += arr[i];
+        }
+        return result;
+    }
+
+    GameNumber() {
+        this.gameNumber = randomGameNumber();
+    }
+
     GameNumber(String gameNumber) {
         if (!isValid(gameNumber))
             throw new IllegalArgumentException("유효하지 않은 게임 숫자입니다.\n유효한 수 : 중복되지 않는 세 자리 숫자");
