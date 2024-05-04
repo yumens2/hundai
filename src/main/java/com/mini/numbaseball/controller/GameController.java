5ae0041938fadbbd5a5cee1b9420e9ef47ecfbe8
wgrgwg <kyoeng14697@naver.com>
2024-05-04 23:45:18 +0900

    feat(controller): add validateInput() method
    Add validateInput() method to GameController for user input(guess) validation.

---
+++ src/main/java/com/mini/numbaseball/controller/GameController.java
@@ -0,0 +1,33 @@
+package com.mini.numbaseball.controller;
+
+public class GameController {
+
+    public int validateInput(String userInput) {
+        int input;
+
+        if (userInput.length() != 3) {
+            throw new IllegalArgumentException("입력값이 세 자리가 아닙니다.");
+        }
+
+        try {
+            input = Integer.parseInt(userInput);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("입력값이 정수가 아닙니다.");
+        }
+
+        int tempInput = input;
+        int first = tempInput / 100;
+        int second = (tempInput / 10) % 10;
+        int third = tempInput % 10;
+
+        if (first == second || second == third || first == third) {
+            throw new IllegalArgumentException("입력값에 중복된 숫자가 있습니다.");
+        }
+
+        if (first==0 || second ==0 || third == 0) {
+            throw new IllegalArgumentException("입력값에 1부터 9까지의 숫자가 아닌 값이 포함되어 있습니다.");
+        }
+
+        return input;
+    }
+}
