1b6bd478c270d53b2eab4568455da0f809491459
박민규 <pmq0101@naver.com>
2024-05-05 05:04:05 +0900

    feat(Validator): 사용자 입력 숫자 확인 기능 추가

--- src/main/java/Validator.java
+++ src/main/java/Validator.java
@@ -2,4 +2,13 @@ public class Validator {
     private boolean isUserInputLengthGameNum(String userInput){
         return (userInput != null && userInput.length() == 3);
     }
+
+    private boolean isUserInputNotInt(String userInput){
+        for (char c : userInput.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
