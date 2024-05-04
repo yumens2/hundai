4cc4c1daf7b53d5502556e5a67ea4a9ad29bdd9b
Gyumin Sim <gyu2781@gmail.com>
2024-05-04 12:50:36 +0900

    style(Result.java): import 수정

--- src/main/java/domain/Result.java
+++ src/main/java/domain/Result.java
@@ -2,7 +2,7 @@ package domain;
 
 import java.util.List;
 
-import static constant.MessageConstants.*;
+import static domain.constant.ResultConstants.*;
 
 public class Result {
     private int strike;
