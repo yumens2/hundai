35057a42a5fc0a456137c666f7262260c81359fc
wgrgwg <kyoeng14697@naver.com>
2024-05-05 00:00:04 +0900

    test(controller): add validateIsReplayTest
    Add validateIsReplayTest to ensrue that validateIsReplay method
    validates input from user and throws exception with proper message.

--- src/test/java/com/mini/numbaseball/controller/GameControllerTest.java
+++ src/test/java/com/mini/numbaseball/controller/GameControllerTest.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.Test;
 class GameControllerTest {
 
     @Test
-    @DisplayName("사용자 입력값 유효성 검사")
+    @DisplayName("사용자 답 입력값 유효성 검사")
     void validateInputTest() {
         GameController gameController = new GameController();
 
@@ -31,5 +31,23 @@ class GameControllerTest {
         assertThatThrownBy(() -> gameController.validateInput("012"))
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessage("입력값에 1부터 9까지의 숫자가 아닌 값이 포함되어 있습니다.");
+    }
+
+    @Test
+    @DisplayName("재시작 여부 입력값 유효성 검사")
+    void validateIsReplayTest(){
+        GameController gameController = new GameController();
+
+        assertThat(true).isEqualTo(gameController.validateIsReplay("1"));
+
+        assertThat(false).isEqualTo(gameController.validateIsReplay("2"));
+
+        assertThatThrownBy(() -> gameController.validateIsReplay("abc"))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("입력값이 정수가 아닙니다.");
+
+        assertThatThrownBy(() -> gameController.validateIsReplay("3"))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("입력값이 1 또는 2가 아닙니다.");
     }
 }
