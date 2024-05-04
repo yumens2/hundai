e107ff9b5444d4ca9b1ee40b8c8822fb05971813
sunandrabbit <sunandrabbit@gamil.com>
2024-05-04 14:58:24 +0900

    feat : ball과 strike의 정보를 담는 객체 생성

---
+++ src/main/java/Answer.java
@@ -0,0 +1,18 @@
+public class Answer {
+
+	private int ball;
+	private int strike;
+	public Answer(int ball, int strike) {
+		this.ball = ball;
+		this.strike = strike;
+	}
+
+	public int getBall() {
+		return ball;
+	}
+
+	public int getStrike() {
+		return strike;
+	}
+
+}
