abc11c15aced526b1c740a77a29b053ff8b1acd9
sunandrabbit <sunandrabbit@gamil.com>
2024-05-04 17:34:54 +0900

    fix : 재시작 종료 기능 추가에 따른 테스트 코드도 추가

--- src/test/java/InputViewTest.java
+++ src/test/java/InputViewTest.java
@@ -12,6 +12,7 @@ import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class InputViewTest {
@@ -36,39 +37,83 @@ class InputViewTest {
 		System.setIn(new ByteArrayInputStream(input.getBytes()));
 		inputView.br = new BufferedReader(new InputStreamReader(System.in));
 	}
-	@Test
-	@DisplayName("입력한 값이 정수리스트로 제대로 반환되는지 확인")
-	void isInputChangeToIntegerList() throws IOException {
-		//given
-		makeInput("123");
 
-		List<Integer> list = inputView.readNumber();
+	@Nested
+	@DisplayName("read Number에 관해")
+	class ReadNumberCheck {
+		@Test
+		@DisplayName("입력한 값이 정수리스트로 제대로 반환되는지 확인")
+		void isInputChangeToIntegerList() throws IOException {
+			//given
+			makeInput("123");
 
-		softAssertions.assertThat(list).isEqualTo(Arrays.asList(1,2,3));
+			List<Integer> list = inputView.readNumber();
+
+			softAssertions.assertThat(list).isEqualTo(Arrays.asList(1,2,3));
+		}
+
+		@Test
+		@DisplayName("3이외의 길이 입력시 throw 발생")
+		void lengthIsNotThree_Throw_IllegalException() {
+			makeInput("12");
+
+			assertThrows(IllegalArgumentException.class, InputView::readNumber);
+		}
+
+		@Test
+		@DisplayName("숫자를 제외한 값 입력시 throw 발생")
+		void inputIsNotNumber_Throw_IllegalException() {
+			makeInput("abc");
+
+			assertThrows(IllegalArgumentException.class, InputView::readNumber);
+		}
+
+		@Test
+		@DisplayName("1~9범위 밖 숫자 입력시 throw 발생")
+		void numberIsNotBetweenOneAndNine_Throw_IllegalException() {
+			makeInput("012");
+
+			assertThrows(IllegalArgumentException.class, InputView::readNumber);
+		}
 	}
 
-	@Test
-	@DisplayName("3이외의 길이 입력시 throw 발생")
-	void lengthIsNotThree_Throw_IllegalException() {
-		makeInput("12");
+	@Nested
+	@DisplayName("read State에 관해")
+	class ReadStateCheck {
+		@Test
+		@DisplayName("입력한 값이 정수로 제대로 반환되는지 확인")
+		void isInputChangeToIntegerList() throws IOException {
+			//given
+			makeInput("1");
 
-		assertThrows(IllegalArgumentException.class, InputView::readNumber);
-	}
+			int number = inputView.readState();
 
-	@Test
-	@DisplayName("숫자를 제외한 값 입력시 throw 발생")
-	void inputIsNotNumber_Throw_IllegalException() {
-		makeInput("abc");
+			softAssertions.assertThat(number).isEqualTo(1);
+		}
 
-		assertThrows(IllegalArgumentException.class, InputView::readNumber);
-	}
+		@Test
+		@DisplayName("1이외의 길이 입력시 throw 발생")
+		void lengthIsNotThree_Throw_IllegalException() {
+			makeInput("12");
 
-	@Test
-	@DisplayName("1~9범위 밖 숫자 입력시 throw 발생")
-	void numberIsNotBetweenOneAndNine_Throw_IllegalException() {
-		makeInput("012");
+			assertThrows(IllegalArgumentException.class, InputView::readState);
+		}
 
-		assertThrows(IllegalArgumentException.class, InputView::readNumber);
+		@Test
+		@DisplayName("숫자를 제외한 값 입력시 throw 발생")
+		void inputIsNotNumber_Throw_IllegalException() {
+			makeInput("a");
+
+			assertThrows(IllegalArgumentException.class, InputView::readState);
+		}
+
+		@Test
+		@DisplayName("1~9범위 밖 숫자 입력시 throw 발생")
+		void numberIsNotBetweenOneAndNine_Throw_IllegalException() {
+			makeInput("3");
+
+			assertThrows(IllegalArgumentException.class, InputView::readState);
+		}
 	}
 }
 
