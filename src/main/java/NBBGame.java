3ccfe7b5c2b2352a68d295deaadc0648dd1fb631
Yongjin <yongjin5352@gmail.com>
2024-05-04 14:35:05 +0900

    feat(NBBGame > playGame()) : complete the feature, "게임 시작하기"

--- src/main/java/NBBGame.java
+++ src/main/java/NBBGame.java
@@ -14,4 +14,19 @@ public class NBBGame {
 
         return new GameNumber(input);
     }
+
+    public void playGame() {
+        while (true) {
+            System.out.print("숫자를 입력해 주세요 : ");
+            GameNumber userGuess = getUserGuess();
+
+            SBCount result = userGuess.compareTo(answer);
+            System.out.println(result);
+
+            if (result.isThreeStrike()) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                break;
+            }
+        }
+    }
 }
