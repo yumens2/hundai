0ab17437cb4da79e9a9ac4d3914e173eebb2b399
Hyewon Seok <hws2008@naver.com>
2024-05-05 03:39:37 +0900

    test: add DisplayName annotation

--- src/test/java/baseball/validator/AnswerValidatorTest.java
+++ src/test/java/baseball/validator/AnswerValidatorTest.java
@@ -2,6 +2,7 @@ package baseball.validator;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -11,6 +12,7 @@ class AnswerValidatorTest {
     @ParameterizedTest
     @NullAndEmptySource
     @ValueSource(strings = {"abcd", "1234", "565", "908"})
+    @DisplayName("예외 발생 테스트")
     void 예외_발생_테스트(String input) {
         assertThrows(IllegalArgumentException.class, () -> {
             AnswerValidator.validate(input);
@@ -19,6 +21,7 @@ class AnswerValidatorTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"123", "456", "789", "421"})
+    @DisplayName("정상 입력 테스트")
     void 정상_입력_테스트(String input) {
         assertDoesNotThrow(() -> AnswerValidator.validate(input));
     }
--- src/test/java/baseball/validator/BitValidatorTest.java
+++ src/test/java/baseball/validator/BitValidatorTest.java
@@ -2,6 +2,7 @@ package baseball.validator;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -9,6 +10,7 @@ class BitValidatorTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"0", "3", "a", "z"})
+    @DisplayName("예외 발생 테스트")
     void 예외_발생_테스트(String input) {
         assertThrows(IllegalArgumentException.class, () -> {
             BitValidator.validate(input);
@@ -17,6 +19,7 @@ class BitValidatorTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"1", "2"})
+    @DisplayName("정상 입력 테스트")
     void 정상_입력_테스트(String input) {
         assertDoesNotThrow(() -> BitValidator.validate(input));
     }
