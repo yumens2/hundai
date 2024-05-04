b699766ff86171147f8c3fc751b0ce5108405acf
humpose <yjpx416@gamil.com>
2024-05-04 09:12:39 +0900

    테스트 케이스 구현

--- src/test/java/OutputTest.java
+++ src/test/java/OutputTest.java
@@ -1,8 +1,5 @@
-package View;
-
-
+import View.Output;
 import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class OutputTest {
--- src/test/java/RandomNumberGeneratorTest.java
+++ src/test/java/RandomNumberGeneratorTest.java
@@ -1,4 +1,16 @@
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
 class RandomNumberGeneratorTest {
-  
+
+    @Test
+    void generateNumbers() {
+
+        List<Integer> numbers = RandomNumberGenerator.generateNumbers();
+        assertEquals(3, numbers.size());
+        assertTrue(numbers.stream().allMatch(num -> num > 0 && num < 10));
+        assertEquals(3, numbers.stream().distinct().count());
+    }
 }
