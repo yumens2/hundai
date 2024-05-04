478787de986824af4e2d59f896ba6d1e1b857cd3
wgrgwg <kyoeng14697@naver.com>
2024-05-04 23:59:04 +0900

    feat(controller): add validateIsReplay() method
    Add validateIsReplay() method to GameController for user input(isReplay)
    validation.

--- src/main/java/com/mini/numbaseball/controller/GameController.java
+++ src/main/java/com/mini/numbaseball/controller/GameController.java
@@ -2,6 +2,8 @@ package com.mini.numbaseball.controller;
 
 public class GameController {
 
+    private boolean isReplay = false;
+
     public int validateInput(String userInput) {
         int input;
 
@@ -24,10 +26,32 @@ public class GameController {
             throw new IllegalArgumentException("입력값에 중복된 숫자가 있습니다.");
         }
 
-        if (first==0 || second ==0 || third == 0) {
+        if (first == 0 || second == 0 || third == 0) {
             throw new IllegalArgumentException("입력값에 1부터 9까지의 숫자가 아닌 값이 포함되어 있습니다.");
         }
 
         return input;
+    }
+
+    public boolean validateIsReplay(String userInput) {
+        int input;
+
+        try {
+            input = Integer.parseInt(userInput);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("입력값이 정수가 아닙니다.");
+        }
+
+        if (input == 1) {
+            return true;
+        } else if (input == 2) {
+            return false;
+        } else {
+            throw new IllegalArgumentException("입력값이 1 또는 2가 아닙니다.");
+        }
+    }
+
+    public boolean getIsReplay() {
+        return isReplay;
     }
 }
