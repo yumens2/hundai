1c98a49f740e04b913ca6c1277cdcc891b53f65f
Hyewon Seok <hws2008@naver.com>
2024-05-05 03:40:43 +0900

    test(BitValidator): add null and empty test values

--- src/test/java/baseball/validator/BitValidatorTest.java
+++ src/test/java/baseball/validator/BitValidatorTest.java
@@ -4,11 +4,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class BitValidatorTest {
 
     @ParameterizedTest
+    @NullAndEmptySource
     @ValueSource(strings = {"0", "3", "a", "z"})
     @DisplayName("예외 발생 테스트")
     void 예외_발생_테스트(String input) {
