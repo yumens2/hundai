7abd4effa8f34d8e32edaae5f26f8d72c326058b
suhyeon <nucc1186@gamil.com>
2024-05-04 15:57:15 +0900

    feature(JudgmentService.java):new feature compare()
    compare() 기능 추가

--- src/main/java/model/JudgmentService.java
+++ src/main/java/model/JudgmentService.java
@@ -1,5 +1,17 @@
 package model;
 
-public class JudgmentService {
+import java.util.ArrayList;
 
+public class JudgmentService {
+    public Score compare(ArrayList<Integer> targetNumbers, ArrayList<Integer> inputNumbers){
+        return new Score(countStrike(targetNumbers,inputNumbers), countBall(targetNumbers,inputNumbers));
+    }
+
+    public int countStrike(ArrayList<Integer> targetNumbers, ArrayList<Integer> inputNumbers){
+
+    }
+
+    public int countBall(ArrayList<Integer> targetNumbers, ArrayList<Integer> inputNumbers){
+
+    }
 }
