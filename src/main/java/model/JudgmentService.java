a5e12c0a2a488bf1d97b0be37d95b7eec7081e1a
suhyeon <nucc1186@gamil.com>
2024-05-04 16:07:39 +0900

    feature(JudgmentService.java) : change compare()
    countStrike()와 countBall의 생성에 따른 compare 수정

--- src/main/java/model/JudgmentService.java
+++ src/main/java/model/JudgmentService.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class JudgmentService {
     public Score compare(ArrayList<Integer> targetNumbers, ArrayList<Integer> inputNumbers){
-        return new Score(countStrike(targetNumbers,inputNumbers), countBall(targetNumbers,inputNumbers));
+        return new Score(countStrike(targetNumbers,inputNumbers), countBall(targetNumbers,inputNumbers) - countStrike(targetNumbers,inputNumbers));
     }
 
     public int countStrike(ArrayList<Integer> targetNumbers, ArrayList<Integer> inputNumbers){
