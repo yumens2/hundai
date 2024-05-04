fb9f54bfe64b757edcf1e520a497078da951bcbc
Gyumin Sim <gyu2781@gmail.com>
2024-05-04 12:45:17 +0900

    refactor(constant): 상수 클래스 변경
    - 기존의 MessageConstants.java를 Error, UserMessage, Result로 분리
    - 각 사용처에 맞게 패키지 분리

---
+++ src/main/java/userinterface/constant/ErrorConstants.java
@@ -0,0 +1,10 @@
+package userinterface.constant;
+
+public class ErrorConstants {
+    public static final String GUESS_ERROR_MESSAGE = "올바른 숫자를 입력하세요.";
+    public static final String GUESS_DUPLICATED_ERROR_MESSAGE = "중복되지 않은 숫자를 입력하세요.";
+    public static final String REPLAY_ERROR_MESSAGE = "1 또는 2를 입력하세요.";
+
+    private ErrorConstants() {
+    }
+}
