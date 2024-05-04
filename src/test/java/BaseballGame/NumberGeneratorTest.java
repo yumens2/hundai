cc1f7f787209b9c3e354d99c464aee97e14cf66e
NextrPlue <98487008+NextrPlue@users.noreply.github.com>
2024-05-04 11:55:26 +0900

    test(NumberGeneratorTest.java): NumberGenerator 관련 테스트

---
+++ src/test/java/BaseballGame/NumberGeneratorTest.java
@@ -0,0 +1,48 @@
+package BaseballGame;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class NumberGeneratorTest {
+
+    @Test
+    @DisplayName("생성된 번호가 Null이 아닌지 확인")
+    public void checkNotNullNumber() {
+        NumberGenerator generator = new NumberGenerator();
+        String generatedNumber = generator.generateNumber();
+
+        assertNotNull(generatedNumber);
+    }
+
+    @Test
+    @DisplayName("생성된 번호가 세 자리인지 확인")
+    public void checkLengthNumber() {
+        NumberGenerator generator = new NumberGenerator();
+        String generatedNumber = generator.generateNumber();
+
+        assertEquals(3, generatedNumber.length());
+    }
+
+    @Test
+    @DisplayName("생성된 번호가 숫자인지 확인")
+    public void testGenerateNumber() {
+        NumberGenerator generator = new NumberGenerator();
+        String generatedNumber = generator.generateNumber();
+
+        assertTrue(generatedNumber.matches("[0-9]+"));
+    }
+
+    @Test
+    @DisplayName("생성된 번호가 중복된 숫자를 포함하지 않는지 확인")
+    public void checkDuplicateNumber() {
+        NumberGenerator generator = new NumberGenerator();
+        String generatedNumber = generator.generateNumber();
+
+        for (int i = 0; i < generatedNumber.length(); i++) {
+            for (int j = i + 1; j < generatedNumber.length(); j++) {
+                assertNotEquals(generatedNumber.charAt(i), generatedNumber.charAt(j));
+            }
+        }
+    }
+}
