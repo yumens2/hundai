d622ac1c07a993355e8c9ce9263ef28803de8105
Yongjin <yongjin5352@gmail.com>
2024-05-04 14:59:32 +0900

    style (GameNumber(String gameNumber)) : add { } to comply with "Google Java Style Guide"

--- src/main/java/GameNumber.java
+++ src/main/java/GameNumber.java
@@ -70,8 +70,9 @@ public class GameNumber {
     }
 
     GameNumber(String gameNumber) {
-        if (!isValid(gameNumber))
+        if (!isValid(gameNumber)) {
             throw new IllegalArgumentException("유효하지 않은 게임 숫자입니다.\n유효한 수 : 중복되지 않는 세 자리 숫자");
+        }
 
         this.gameNumber = gameNumber;
     }
