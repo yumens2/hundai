229cfab11b7ba17f12b36ec189c716fa7d37d1c7
Yongjin <yongjin5352@gmail.com>
2024-05-04 14:58:31 +0900

    feat (Application > wantRestort) : complete the feature, "게임 재시작 여부 확인하기"

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,4 +1,19 @@
+import java.util.Scanner;
+
 public class Application {
+    private boolean wantRestart(Scanner scanner) {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. ");
+
+        String input = scanner.nextLine();
+        if (input.equals("1")) {
+            return true;
+        } else if (input.equals("2")) {
+            return false;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
     public static void main(String[] args) {
 
     }
