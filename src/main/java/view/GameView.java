ce0aa4e65cc165d41850489703fece694756e66f
us4c0d3 <jwseok312@gmail.com>
2024-05-06 03:26:20 +0900

    feat($GameView): implement GameView.checkRestartGame to pass test
    게임 종료 후 재시작 여부를 물어보고 그에 대한 입력을 받아 boolean 타입으로 반환하는 메서드
    - 1 입력시 true 반환
    - 2 입력시 false 반환
    - 그 외의 값 입력 시 `IllegalArgumentException` 발생

--- src/main/java/view/GameView.java
+++ src/main/java/view/GameView.java
@@ -25,5 +25,16 @@ public class GameView {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
-    public boolean checkRestartGame() {}
+    public boolean checkRestartGame() throws IOException {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String option = reader.readLine();
+
+        if(option.equals("1")) {
+            return true;
+        } else if(option.equals("2")) {
+            return false;
+        } else {
+            throw new IllegalArgumentException("Invalid option input.");
+        }
+    }
 }
