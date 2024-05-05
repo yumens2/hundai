6be3b795e520ebe88e3fc81b764fad22f49af2be
Dalsungmin <cnww123@naver.com>
2024-05-05 14:56:07 +0900

    결과 출력 및 게임 재시작
    judge메소드로 나온 값에 의해
    print메서드로 결과를 출력
    game메서드로 1 또는 2를 입력 받아
    게임을 재시작할지 종료할지 결정

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -7,49 +7,53 @@ public class Application {
     static int[] digits;
     static int strike = 0;
     static int ball = 0;
-
+    static int a,b = 0;
     public static void main(String[] args) {
+        a = 1;
+        while(a==1){ // a가 0이면 전체 게임 종료
 
-        makeNumber();
+            makeNumber();
 
-        Scanner scan = new Scanner(System.in);
-        System.out.print("숫자를 입력해주세요:");
-        int input = scan.nextInt();
-        try {
-            setNum(input);
+            
+
+
+            b=1;
+            while(b==1) { //b가 0이면 새로운 숫자 생성
+                Scanner scan = new Scanner(System.in);
+                System.out.print("숫자를 입력해주세요:");
+                int input = scan.nextInt();
+                try {
+                    setNum(input);
+                }
+                catch (IllegalArgumentException e) {
+                    System.out.println("유효하지 않은 숫자입니다.");
+                    return;
+                }
+                System.out.println(input);
+                digits = Stream.of(String.valueOf(input).split(""))
+                    .mapToInt(Integer::parseInt)
+                    .toArray(); //입력한 숫자를 분리해서 digits 배열에 넣기
+
+                judge();
+
+                print();
+
+            }
+            game();
         }
-        catch (IllegalArgumentException e) {
-            System.out.println("유효하지 않은 숫자입니다.");
-            return;
-        }
-        System.out.println(input);
-        digits = Stream.of(String.valueOf(input).split(""))
-            .mapToInt(Integer::parseInt)
-            .toArray();
-
-        judge();//입력한 숫자를 분리해서 digits 배열에 넣기
     }
-
-    static void makeNumber() {
+    static void makeNumber(){
         for (int i = 0; i < arr.length; i++) {
-            arr[i] = (int) ((Math.random() * 10000)
-                % 10); // 0~1사이에 난수가 생성되므로 10000을 곱한후 10으로 나눈 나머지로 만든다
-            if ((i != 0 && arr[i] == arr[i - 1]) || arr[i] == 0) { // 만든 숫자가 중복되거나 0이면 다시 만든다
+            arr[i] = (int) ((Math.random() * 10000) % 10); // 0~1사이에 난수가 생성되므로 10000을 곱한후 10으로 나눈 나머지로 만든다
+            if((i != 0 && arr[i] == arr[i-1]) || arr[i] == 0){ // 만든 숫자가 중복되거나 0이면 다시 만든다
                 i--;
             }
-            if (i == 2 && arr[2] == arr[0]) {
+            if (i == 2 && arr[2] == arr[0]){
                 i--;
             }
         }
     }
-    static void setNum(int num) throws IllegalArgumentException {
-        int n100 = num / 100; num -= n100 * 100;
-        int n10 = num / 10; num -= n10 * 10;
-        int n1 = num;
-        if ( num > 999 || n100 == 0 || n10 == 0 || n1 == 0) {
-            throw new IllegalArgumentException();
-        }
-    }
+
     static void judge(){
         for (int l = 0; l < arr.length; l++) {
             if (arr[l] == digits[l]) {
@@ -67,5 +71,47 @@ public class Application {
             }
         }
     }
+
+    static void print(){
+        Scanner scan = new Scanner(System.in);
+        if (strike == 0 && ball != 0) { //0 스트라이크 N 볼일 때
+            System.out.println(ball + "볼");
+        } else if (strike != 0 && ball == 0) { //N 스트라이크 0 볼일 때
+            System.out.println(strike + "스트라이크");
+        } else if (strike == 0 && ball == 0) { //0 스트라이크 0 볼일 때
+            System.out.println("낫싱");
+        } else // N 스트라이크 N 볼일 때
+            System.out.println(strike + "스트라이크" + ball + "볼");
+        if (strike == 3) { // 3 스트라이크로 정답일 때
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            b = 0;
+        }
+        strike = 0;
+        ball = 0;
+    }
+    static void setNum(int num) throws IllegalArgumentException {
+        int n100 = num / 100; num -= n100 * 100;
+        int n10 = num / 10; num -= n10 * 10;
+        int n1 = num;
+        if ( num > 999 || n100 == 0 || n10 == 0 || n1 == 0) {
+            throw new IllegalArgumentException();
+        }
+    }
+    static void game(){
+        Scanner scan = new Scanner(System.in);
+        int sc = scan.nextInt();
+        if (sc == 2) {
+            a = 0;
+            b = 0;
+
+        } else if (sc == 1) {
+            b = 0;
+
+        }
+    }
+
 }
+
+
 
