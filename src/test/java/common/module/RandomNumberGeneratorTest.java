8ce630375c654ca33a655e429bcdff04307b0abd
stopmin <geemin0@naver.com>
2024-05-05 23:08:22 +0900

    chore: 함수 네이밍 수정

--- src/test/java/common/module/RandomNumberGeneratorTest.java
+++ src/test/java/common/module/RandomNumberGeneratorTest.java
@@ -11,7 +11,7 @@ class RandomNumberGeneratorTest {
 
     @Test
     @DisplayName("주어진 범위 내에서 랜덤한 숫자를 생성한다.")
-    public void computerGenerateNumber() {
+    public void generateRandomNumberInRange() {
         RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
         int number = randomNumberGenerator.generateRandomNumberInRange(MIN_NUMBER, MAX_NUMBER);
         assertTrue(number >= MIN_NUMBER && number <= MAX_NUMBER);
