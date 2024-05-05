10d76d0656fa6693be65601d3363249b9ec68bb7
stopmin <geemin0@naver.com>
2024-05-05 23:11:24 +0900

    test: 컴퓨터 숫자 생성 테스트

---
+++ src/test/java/domain/ComputerPlayerTest.java
@@ -0,0 +1,38 @@
+package domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ComputerPlayerTest {
+
+    private final ComputerPlayer computerPlayer = new ComputerPlayer();
+
+    @Test
+    @DisplayName("컴퓨터는 3개의 숫자를 생성한다.")
+    public void computerGenerateThreeNumber() {
+        //given
+        computerPlayer.generateNumbers();
+
+        //when
+        int size = computerPlayer.getNumbers().size();
+
+        //then
+        assertEquals(3, size);
+    }
+
+    @Test
+    @DisplayName("컴퓨터가 생성한 숫자는 1에서 9까지의 숫자다.")
+    public void validateComputerNumber() {
+        //given
+        computerPlayer.generateNumbers();
+
+        //when
+        boolean isInvalid = computerPlayer.getNumbers().stream()
+            .anyMatch(number -> number < BaseballGame.MIN_NUMBER || number > BaseballGame.MAX_NUMBER);
+
+        //then
+        assertFalse(isInvalid);
+    }
+}
