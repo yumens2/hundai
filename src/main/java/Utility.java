98e18478d732167cf7260396bf3373f6bcc631e7
kangwlgns <luckyrkd@naver.com>
2024-05-05 23:40:53 +0900

    feat(Utility): add method of getting strike and ball

--- src/main/java/Utility.java
+++ src/main/java/Utility.java
@@ -99,4 +99,16 @@ public class Utility {
 
         return ball;
     }
+
+    public static String getStrikeBall(int strike, int ball) {
+        if (strike == 0 && ball == 0) {
+            return "낫싱";
+        } else if (ball == 0) {
+            return strike + "스트라이크";
+        } else if (strike == 0) {
+            return ball + "볼";
+        } else {
+            return ball + "볼 " + strike + "스트라이크";
+        }
+    }
 }
