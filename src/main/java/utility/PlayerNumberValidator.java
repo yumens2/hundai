e5d7c55c88bcd9ff804a70304b9b63da86a8f33b
HSHwan <tnghks2866@gmail.com>
2024-05-05 13:25:08 +0900

    style: 상위 클래스 변경 내용 반영 및 메서드명에서 동사 변경

--- src/main/java/utility/PlayerNumberValidator.java
+++ src/main/java/utility/PlayerNumberValidator.java
@@ -4,9 +4,9 @@ public class PlayerNumberValidator extends InputNumberValidator {
 
     private static final int PLAYER_NUMBER_LENGTH = 3;
 
-    public static void isPlayerNumberCorrect(String playerNumber) throws IllegalArgumentException {
-        isStringLengthCorrect(playerNumber, PLAYER_NUMBER_LENGTH);
-        isStringNumeric(playerNumber);
-        isDifferentDigitNumber(playerNumber);
+    public static void checkPlayerNumberCorrect(String playerNumber) throws IllegalArgumentException {
+        checkStringLengthCorrect(playerNumber, PLAYER_NUMBER_LENGTH);
+        checkStringNumeric(playerNumber);
+        checkDifferentDigitNumber(playerNumber);
     }
 }
