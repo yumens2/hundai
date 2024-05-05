86ec0a26975c9207ec2dc926b8d2e00a8b169f50
SeoYeongU <westzeroright@gmail.com>
2024-05-05 22:37:48 +0900

    fix : 테스트코드 수정

--- src/test/java/ComputerTest.java
+++ src/test/java/ComputerTest.java
@@ -1,15 +1,28 @@
 import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ComputerTest {
+
     @Test
-    public void computerTest(){
-        // given
-        int rand = Computer.makeRandom();
+    public void testMakeRandom() {
+        // Given
+        Computer computer = new Computer();
 
-        // when
+        // When
+        int randomNumber = computer.makeRandom();
 
-        // then
-        assertThat(Computer.makeRandom()).isEqualTo(rand);
+        // Then
+        // 각 자릿수가 서로 다른지 확인
+        int[] digits = new int[3];
+        digits[0] = randomNumber / 100;
+        digits[1] = (randomNumber / 10) % 10;
+        digits[2] = randomNumber % 10;
+
+        assertTrue(digits[0] != digits[1]);
+        assertTrue(digits[0] != digits[2]);
+        assertTrue(digits[1] != digits[2]);
+
+        // 숫자가 1부터 9까지의 범위 내에 있는지 확인
+        assertTrue(randomNumber >= 123 && randomNumber <= 987);
     }
 }
