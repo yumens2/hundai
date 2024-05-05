71a1319d3888b01ee2f55c6baa8c766c9a683729
amm0124 <amm0124@naver.com>
2024-05-06 05:21:51 +0900

    Feat : main 작성

---
+++ src/main/java/Application.java
@@ -0,0 +1,22 @@
+public class Application {
+    public static void main(String[] args) {
+
+        boolean endFlag=false;
+
+        while(!endFlag){
+
+            GenerateTargetNumber generateTargetNumber = new GenerateTargetNumber();
+            int strike = 0;
+
+            while(strike!=3){
+                PlayerInput playerInput = new PlayerInput(); // 사용자 입력
+                Compare compare=new Compare(); // 비교 객체
+                compare.compare(generateTargetNumber.getTargetNumbers(), playerInput.getPlayerNumber()); //출력을 함
+                strike = compare.strikeCount();
+            }
+
+            PlayerInputValidator playerInputValidator = new PlayerInputValidator();
+            if(!playerInputValidator.validateInput()) endFlag = true; // 다시 한다
+        }
+    }
+}
