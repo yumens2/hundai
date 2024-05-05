7cc56b205809bf7a5141a804915cc07f817e7339
ana-raka <junglkls1234@gmail.com>
2024-05-05 14:36:13 +0900

    make ans

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,19 @@
 import java.util.Scanner;
 import java.util.Random;
 public class Main {
-    static int first_randomNumber;
-    static int second_randomNumber;
-    static int third_randomNumber;
+    static int[] ansList = new int[10];
     public static Scanner scan = new Scanner(System.in);
     static void makeAttack(){
         System.out.print("숫자를 입력해주세요: ");
-        int predict = scan.nextInt();
-        System.out.println(first_randomNumber);
-        System.out.println(second_randomNumber);
-        System.out.println(third_randomNumber);
+        int predict_Num = scan.nextInt();
 
     }
     static void makeAns(){
         Random random = new Random();
-        first_randomNumber = random.nextInt(10);
-        second_randomNumber = random.nextInt(10);
-        third_randomNumber = random.nextInt(10);
-
+        for (int order = 0 ; order < 3 ; order++){
+            int randomNumber = random.nextInt(10);
+            ansList[randomNumber] = order;
+        }
     }
     public static void main(String[] args) {
         makeAns();
