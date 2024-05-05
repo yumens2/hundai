e4c73be2a6f0ee379c8f4a28a2e96e55aee98ac2
황인제 <hwang-inje@hwang-inje-ui-MacBookAir.local>
2024-05-05 19:11:27 +0900

    feat(GameLogic): 스트라이크 및 볼 계산

---
+++ src/main/java/baseball/GameLogic.java
@@ -0,0 +1,27 @@
+package baseball;
+
+import java.util.List;
+
+public class GameLogic {
+    public int countStrikes(String input, List<Integer> computerNumbers) {
+        int strikes = 0;
+        for (int i = 0; i < 3; i++) {
+            int digit = Character.getNumericValue(input.charAt(i));
+            if (digit == computerNumbers.get(i)) {
+                strikes++;
+            }
+        }
+        return strikes;
+    }
+
+    public int countBalls(String input, List<Integer> computerNumbers, int strikes) {
+        int balls = 0;
+        for (int i = 0; i < 3; i++) {
+            int digit = Character.getNumericValue(input.charAt(i));
+            if (computerNumbers.contains(digit) && digit != computerNumbers.get(i)) {
+                balls++;
+            }
+        }
+        return balls;
+    }
+}
