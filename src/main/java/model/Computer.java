a99feddf5752ee2f0dba69aee9e771e29a2bfb8b
us4c0d3 <jwseok312@gmail.com>
2024-05-05 20:45:27 +0900

    test($Computer): create test for Computer class
    `Computer`클래스의 기능을 테스트하기 위한 실패 코드 작성
    - 클래스 생성자에서 조건에 맞는 랜덤 숫자 배열 생성
    - getter를 통해 숫자 배열 반환

---
+++ src/main/java/model/Computer.java
@@ -0,0 +1,15 @@
+package model;
+
+import util.NumberUtil;
+
+public class Computer {
+    private int[] numbers;
+
+    public Computer() {
+        //TODO
+    }
+
+    public int[] getNumber() {
+        return numbers;
+    }
+}
---
+++ src/test/java/model/ComputerTest.java
@@ -0,0 +1,29 @@
+package model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ComputerTest {
+    @Test
+    @DisplayName("Computer constructor method must generate random number suitable for conditions")
+    void computerConstructorTest() throws Exception {
+        //given
+        Computer computer = new Computer();
+        int[] numbers = computer.getNumber();
+
+        //when & then(배열 길이 테스트)
+        assertThat(numbers).hasSize(3);
+
+        //when(숫자 범위 테스트)
+        for(int number : numbers) {
+            //then
+            assertThat(number).isBetween(1, 9);
+        }
+
+        //when & then(중복 검사 테스트)
+        assertThat(numbers).doesNotHaveDuplicates();
+    }
+}
