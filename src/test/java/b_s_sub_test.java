573295832bc34edc34b7a15962e247ce12d9002d
rbm0524 <rbm0524@naver.com>
2024-05-06 08:02:09 +0900

    b_s_sub에 대한 test 코드 추가

---
+++ src/test/java/b_s_sub_test.java
@@ -0,0 +1,36 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class b_s_sub_test {
+    @Test
+    public void testBSSUBTest() {
+        int[] judgement = {0, 0};
+        String com = "123";
+        String num = "321";
+        BaseballGame.b_s_sub(0, com, num, judgement);
+        BaseballGame.b_s_sub(1, com, num, judgement);
+        BaseballGame.b_s_sub(2, com, num, judgement);
+
+        //최종 결과
+        assertEquals(2, judgement[0]); // 예상 결과: 2볼
+        assertEquals(1, judgement[1]); // 예상 결과: 1스트라이크
+
+
+    }
+    @Test
+    public void testBSSUBTest2() {
+        int[] judgement = {0, 0};
+        String com = "946";
+        String num = "321";
+        BaseballGame.b_s_sub(0, com, num, judgement);
+        BaseballGame.b_s_sub(1, com, num, judgement);
+        BaseballGame.b_s_sub(2, com, num, judgement);
+
+        //최종 결과
+        assertEquals(0, judgement[0]); // 예상 결과: 0볼
+        assertEquals(0, judgement[1]); // 예상 결과: 0스트라이크
+
+
+    }
+}
