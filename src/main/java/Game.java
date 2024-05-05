a6f78a159249a836fb88e57cb8249de65681a8e2
Kangboom <jkl6147@naver.com>
2024-05-05 23:19:54 +0900

    refactor(Game)

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,18 +1,19 @@
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Game {
     private Player player =  new Player();
     private CreateRandom createRandom = new CreateRandom();
     private Judgement judgement = new Judgement();
+    private ArrayList<Integer> user;
+    private ArrayList<Integer> computer;
     public void run(){
 
-        ArrayList<Integer> computer = createRandom.createList();
+        computer = createRandom.createList();
 
         boolean start = true;
         while(start) {
 
-            ArrayList<Integer> user = player.input();
+            user = player.input();
 
             int[] cnt = judgement.judge(computer, user);
             judgement.result(cnt);
