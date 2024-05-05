ff2c4dfc3c643f9a6492bfc1ac0dbcdfa4321b61
Lucerna00 <pjs3859811@naver.com>
2024-05-05 22:16:21 +0900

    feat(Validation): add user input validation

---
+++ src/main/java/Validation.java
@@ -0,0 +1,24 @@
+import java.util.HashSet;
+import java.util.stream.Collectors;
+
+public class Validation {
+
+    public static boolean isValidNum(String input) {
+        try {
+            HashSet<Character> set = input.chars()
+                .mapToObj(e -> (char) e)
+                .collect(Collectors.toCollection(HashSet::new));
+
+            if (set.size() != 3 || set.contains('0')) {
+                throw new IllegalArgumentException();
+            }
+            Integer.parseInt(input);
+        } catch (IllegalArgumentException e) {
+            System.out.println("잘못된 값을 입력하였습니다.");
+
+            return false;
+        }
+
+        return true;
+    }
+}
