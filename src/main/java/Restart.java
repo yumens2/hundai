c9789aa84cd0d36bc47f49651171602193376054
hyunseo cho <gustj12300@naver.com>
2024-05-06 01:40:46 +0900

    재시작 여부 결정

--- src/main/java/Restart.java
+++ src/main/java/Restart.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
 
 public class Restart {
-    private final Scanner scanner;
+    private final Scanner inputs;
 
     public Restart() {
-        scanner = new Scanner(System.in);
+        inputs = new Scanner(System.in);
     }
 
     public boolean askForRestart() {
         System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-        String input = scanner.nextLine();
+        String input = inputs.nextLine();
         return "1".equals(input);
     }
 }
