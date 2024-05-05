0d777c3745808cc20797a6a40e4b462454d41d2c
Kangboom <jkl6147@naver.com>
2024-05-05 23:19:07 +0900

    refactor(CreateRandom)

--- src/main/java/CreateRandom.java
+++ src/main/java/CreateRandom.java
@@ -3,19 +3,17 @@ import java.util.Random;
 
 // 랜덤 숫자 생성
 public class CreateRandom {
+    private ArrayList<Integer> randomList = new ArrayList<>();
+    private Random random = new Random();
     public int createNum(){
-        Random random = new Random();
         return random.nextInt(9)+1;
     }
     public ArrayList<Integer> createList(){
-        ArrayList<Integer> randomList = new ArrayList<Integer>();
+        randomList.clear();
 
         while(randomList.size() < 3){
             int rand = createNum();
-            if (randomList.contains(rand)){
-                continue;
-            }
-            else{
+            if (!(randomList.contains(rand))){
                 randomList.add(rand);
             }
         }
