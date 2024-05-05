3f5a4b5c98fa581896a2d2a7283594f0f21d8d70
hyyyh0x <hyyyh0x@gmail.com>
2024-05-05 13:02:09 +0900

    feat : Readme No.2
    Added Number class and constructor
    In constructor making 3 random numbers
    Added method that makes game start(chkEquality)

---
+++ src/main/java/Number.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Number{
+    boolean FLAG;
+    int[] RANDOM_NUM=new int[3];
+    int MY_NUM;
+    int BALL;
+    int STRIKE;
+    public Number() {
+        FLAG=true;
+        BALL=0; STRIKE=0;
+        RANDOM_NUM[0]=(int)(10*Math.random());
+        while(true){
+            RANDOM_NUM[1]=(int)(10*Math.random());
+            if(RANDOM_NUM[0]!=RANDOM_NUM[1]) break;
+        }
+        while(true){
+            RANDOM_NUM[2]=(int)(10*Math.random());
+            if(RANDOM_NUM[0]!=RANDOM_NUM[2]&&RANDOM_NUM[1]!=RANDOM_NUM[2]) break;
+        }
+    }
+    public void chkEquality() throws Exception {
+        boolean FLAG_NUM1=true;
+        while(FLAG_NUM1){
+            getNum();
+            compareNum();
+            if(STRIKE==3) {
+                finishedGame();
+                return;
+            }
+            FLAG_NUM1=FLAG;
+        }
+    }
+}
