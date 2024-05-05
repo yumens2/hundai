ea4b0a5f4b351bbb3d788d9a9686f08c9d72aca5
ddglackrp <ddglackrp@naver.com>
2024-05-05 17:33:56 +0900

    Feat "스트라이크 개수 확인 로직 추가"
    - checkStrike : 사용자의 입력을 이용해 스트라이크 수 체크하는 함수 추가

--- src/main/java/game/BaseBallGame.java
+++ src/main/java/game/BaseBallGame.java
@@ -1,4 +1,17 @@
 package game;
 
 public class BaseBallGame {
+
+    public int checkStrike(String computerNumber, String userNumber){
+        int strike = 0;
+
+        for(int i=0; i<3; i++){
+            if(computerNumber.charAt(i) == userNumber.charAt(i)){
+                strike++;
+            }
+        }
+
+        return strike;
+    }
+
 }
