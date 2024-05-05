cd7076314162f3b748d02d504ccec2765515881e
stopmin <geemin0@naver.com>
2024-05-05 22:48:14 +0900

    feat: Player 인터페이스 정의 및 컴퓨터/인간 플레이어 구현체 생성

---
+++ src/main/java/domain/ComputerPlayer.java
@@ -0,0 +1,9 @@
+package domain;
+
+public class ComputerPlayer implements Player {
+
+    @Override
+    public void generateNumbers() {
+        System.out.println("Computer player generates numbers.");
+    }
+}
---
+++ src/main/java/domain/HumanPlayer.java
@@ -0,0 +1,9 @@
+package domain;
+
+public class HumanPlayer implements Player {
+
+    @Override
+    public void generateNumbers() {
+        System.out.println("Human player generates numbers.");
+    }
+}
