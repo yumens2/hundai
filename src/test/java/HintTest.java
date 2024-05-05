7e880be0522c4c018f63b7b82f5fa70f513325f7
SeoYeongU <westzeroright@gmail.com>
2024-05-05 22:37:38 +0900

    fix : 테스트코드 수정

--- src/test/java/HintTest.java
+++ src/test/java/HintTest.java
@@ -1,17 +1,25 @@
 import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class HintTest {
+
     @Test
     public void testHint() {
-        // given
-        int rand = Computer.makeRandom();
-        int user = UserInput.userInput();
+        // Given
+        int computerNumber = 123;
 
-        // when
-        String result = Hint.hint(rand, user);
+        // When
+        String hint1 = Hint.hint(computerNumber, 123); // 3 스트라이크
+        String hint2 = Hint.hint(computerNumber, 456); // 낫싱
+        String hint3 = Hint.hint(computerNumber, 312); // 3볼
+        String hint4 = Hint.hint(computerNumber, 231); // 3볼
+        String hint5 = Hint.hint(computerNumber, 135); // 1 스트라이크 1 볼
 
-        // then
-        assertThat(result).isEqualTo(Hint.hint(rand, user));
+        // Then
+        assertEquals("3스트라이크", hint1);
+        assertEquals("낫싱", hint2);
+        assertEquals("3볼", hint3);
+        assertEquals("3볼", hint4);
+        assertEquals("1스트라이크1볼", hint5);
     }
 }
