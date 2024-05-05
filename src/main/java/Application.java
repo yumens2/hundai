4ff24c4fa0327878d6f0d54f725f5a0db625aefa
Kangboom <jkl6147@naver.com>
2024-05-05 23:17:34 +0900

    feat(Application): Add exception handling

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,6 +3,10 @@ import java.sql.SQLOutput;
 public class Application {
     public static void main(String[] args) {
         Game game = new Game();
-        game.run();
+        try {
+            game.run();
+        }catch(IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
