bd8d64ecf275c07f98a28a98f1ff10da82600f4a
JinSeoHyun <15roa1121@gmail.com>
2024-05-05 17:35:14 +0900

    수정

--- src/main/java/problem/ProvidedMessage.java
+++
@@ -1,5 +0,0 @@
-package src.main.java.problem;
-
-public enum ProvidedMessage {
-    STRIKE,BALL,NOTHING
-}
--- src/main/java/problem/TestTaker.java
+++
@@ -1,6 +0,0 @@
-package src.main.java.problem;
-
-public interface TestTaker {
-  //기능구현
-  public String providingNumString();
-}
