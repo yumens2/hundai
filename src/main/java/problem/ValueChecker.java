e6f77f931ad08ed12a98a97112f23b74b61b8198
JinSeoHyun <15roa1121@gmail.com>
2024-05-05 16:20:58 +0900

    feat(ValueChecker): 입력값 검수를 위한 스트라이크, 볼, 낫싱 기능 구현

--- src/main/java/problem/ValueChecker.java
+++ src/main/java/problem/ValueChecker.java
@@ -2,13 +2,12 @@ package src.main.java.problem;
 
 public abstract class ValueChecker {
     private final String problemValue;
+
     private ProvidedMessage providedMessage;
 
     public ValueChecker(String problemValue) {
         this.problemValue = problemValue;
     }
 
-    public String determineTheValue(String inputValue) {
-        return null;
-    }
+    public abstract String determineTheValue(String inputValue);
 }
