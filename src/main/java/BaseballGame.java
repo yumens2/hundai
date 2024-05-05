bd96e2dce2d3381b50f0e77706dbceebf9e0c0f1
정진택 <87135698+jjt4515@users.noreply.github.com>
2024-05-06 04:07:18 +0900

    게임 시작, 숫자 랜덤 생성, 사용자 입력받기 구현

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -1,2 +1,55 @@
+import java.util.Scanner;
+
 public class BaseballGame {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        boolean isContinue = true;
+
+        while (isContinue){
+            playGame();
+        }
+    scanner.close();
+    }
+   public static void playGame(){
+        int[] gameNumber = generateGameNumber();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("숫자 입력: ");
+        while (true) {
+            int[] guess = getGuess(scanner);
+            int[] result = checkResult(gameNumber, guess);
+        }
+        scanner.close();
+   }
+   public static int[] generateGameNumber(){
+        int[] num = new int[3];
+        for (int i = 0; i < 3; i++) {
+            num[i] = (int)(Math.random()*9) + 1;
+            if (isDuplication(num, i)) {
+                i--;
+            }
+        }
+        return num;
+   }
+   public static boolean isDuplication(int[] num, int i) {
+        for(int j=0; j<i; j++){
+            if (num[i] == num[j]){
+                return true;
+            }
+        }
+        return false;
+   }
+   public static int[] getGuess(Scanner scanner){
+        int[] guess = new int[3];
+        int inp = scanner.nextInt();
+
+        guess[0] = inp / 100;
+        guess[1] = (inp % 100) / 10;
+        guess[2] = inp % 10;
+
+        return guess;
+   }
+   public static int[] checkResult(int[] gameNumber, int[] guess){
+
+   }
 }
