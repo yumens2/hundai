d26fafa6f853930615099112350b4da8afffe5d0
Dalsungmin <cnww123@naver.com>
2024-05-05 15:01:03 +0900

    만들어진 수가 1 ~ 9 범위인지 테스트

--- src/test/java/BaseballTest.java
+++ src/test/java/BaseballTest.java
@@ -16,4 +16,19 @@ public class BaseballTest {
         assertNotEquals(arr[1], arr[2]);
         assertNotEquals(arr[0], arr[2]);
     }
+
+    @Test
+    public void testMakeNumber_WithinRange() { // 만들어진 수가 1 ~ 9 범위인지 테스트
+        // Arrange
+        int[] arr;
+
+        // Act
+        Application.makeNumber();
+        arr = Application.arr;
+
+        // Assert
+        assertTrue(arr[0] >= 1 && arr[0] <= 9);
+        assertTrue(arr[1] >= 1 && arr[1] <= 9);
+        assertTrue(arr[2] >= 1 && arr[2] <= 9);
+    }
 }
