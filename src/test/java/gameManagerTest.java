cb44458a1c8f0f81b35d451e74f5e10a7be6d174
J-1ac <tkdwns26@naver.com>
2024-05-06 03:30:28 +0900

    feat(gameManagerTest): implement 유효입력테스트 to test for valid input

--- src/test/java/gameManagerTest.java
+++ src/test/java/gameManagerTest.java
@@ -1,6 +1,8 @@
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.ByteArrayInputStream;
 import org.junit.jupiter.api.Test;
 
 public class gameManagerTest {
@@ -21,6 +23,19 @@ public class gameManagerTest {
         for (int num : generatedRandNum) {
             assertTrue(num >= 1 && num <= 9);
         }
+    }
+
+    @Test
+    public void 유효입력테스트() {
+        gameManager newGameManager = new gameManager();
+
+        String input = "123\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        int[] expected = {1, 2, 3};
+        int[] actual = newGameManager.getInput();
+
+        assertArrayEquals(expected, actual);
     }
 
 
