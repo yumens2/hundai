7e959e860629a93e49516d8f98eb5db306ac2e0a
kangwlgns <luckyrkd@naver.com>
2024-05-05 16:05:21 +0900

    feat(Utility): add method of making random numbers

--- src/main/java/Utility.java
+++ src/main/java/Utility.java
@@ -1,3 +1,24 @@
 public class Utility {
-    private Utility() {}
+
+    private Utility() {
+    }
+
+    public static int[] makeRandomNumbers() {
+        boolean[] flags = new boolean[10];
+        int[] randomNumbers = new int[3];
+        int count = 0;
+
+        while (count < 3) {
+            int temp = (int) (Math.random() * 10);
+            if (flags[temp]) {
+                continue;
+            } else {
+                randomNumbers[count] = temp;
+                flags[temp] = true;
+                count += 1;
+            }
+        }
+
+        return randomNumbers;
+    }
 }
