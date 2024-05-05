b940c61202a80be4944b71fdd50904a69da68a4a
Happeeee <cmg118@naver.com>
2024-05-05 13:29:58 +0900

    feat: 게임 시작 기능 구현

---
+++ src/main/java/Application.java
@@ -0,0 +1,8 @@
+import game.BaseBallGame;
+
+public class Application {
+    public static void main(String[] args) {
+        BaseBallGame baseBallGame = new BaseBallGame();
+        baseBallGame.gameStart();
+    }
+}
