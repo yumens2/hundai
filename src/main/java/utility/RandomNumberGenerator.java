aa0d40bee98c0f04de69789999bfb47ab2c445ba
HSHwan <tnghks2866@gmail.com>
2024-05-05 12:58:54 +0900

    refactor: 랜덤 숫자 생성 기능에서 사용되는 상수 이름 설정

--- src/main/java/utility/RandomNumberGenerator.java
+++ src/main/java/utility/RandomNumberGenerator.java
@@ -6,9 +6,13 @@ import java.util.Random;
 
 public class RandomNumberGenerator {
 
+    private static final int RANDOM_NUMBER_LENGTH = 3;
+    private static final int MAX_DIGIT_NUMBER = 9;
+    private static final int DIGIT_RANGE_OFFSET = 1;
+
     public static String getRandomNumber() {
         List<String> randomNumberList = new ArrayList<>();
-        while (randomNumberList.size() < 3) {
+        while (randomNumberList.size() < RANDOM_NUMBER_LENGTH) {
             appendRandomSingleDigit(randomNumberList);
         }
         return String.join("", randomNumberList);
@@ -16,7 +20,7 @@ public class RandomNumberGenerator {
 
     private static void appendRandomSingleDigit(List<String> numberList) {
         Random randomGenerator = new Random();
-        String randomSingleDigit = Integer.toString(randomGenerator.nextInt(9) + 1);
+        String randomSingleDigit = Integer.toString(randomGenerator.nextInt(MAX_DIGIT_NUMBER) + DIGIT_RANGE_OFFSET);
         if (!numberList.contains(randomSingleDigit)){
             numberList.add(randomSingleDigit);
         }
