c6de763b8c67d9407fed3167d1da38ed04f4dd68
kangwlgns <luckyrkd@naver.com>
2024-05-06 01:12:32 +0900

    feat(Utility): add method of print end message

--- src/main/java/Utility.java
+++ src/main/java/Utility.java
@@ -109,4 +109,11 @@ public class Utility {
             return ball + "볼 " + strike + "스트라이크";
         }
     }
+
+    public static void printEndMessage(int strike) {
+        if (strike == 3) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        }
+    }
 }
