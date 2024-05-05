19e41debdee11fd2d32ae406e01d2d8c7c9513b6
suhyeon <nucc1186@gamil.com>
2024-05-05 23:30:00 +0900

    feature(BaseballNumberGenerator.java) : change generate()
    generate() 함수 중복 체크 기능 추가

--- src/main/java/model/BaseballNumberGenerator.java
+++ src/main/java/model/BaseballNumberGenerator.java
@@ -6,10 +6,12 @@ public class BaseballNumberGenerator {
     public static final int making_number_count = 3;
 
     public ArrayList<Integer> generate(){
-        ArrayList<Integer> number = new ArrayList<Integer>();
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
         for(int i = 0; i < making_number_count; i++) {
-            number.add((int) (Math.random() * 9)+1);
+            int number = (int) (Math.random() * 9)+1;
+            if(numbers.contains(number))
+                numbers.add(number);
         }
-        return number;
+        return numbers;
     }
 }
