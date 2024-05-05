08c2564a1a398a17ca2e26bda49be5afd3cd0a9e
J-1ac <tkdwns26@naver.com>
2024-05-06 02:30:46 +0900

    feat(gameManager): implement getInput method to get and validate user input

--- src/main/java/gameManager.java
+++ src/main/java/gameManager.java
@@ -1,6 +1,9 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class gameManager {
+
+    private static final Scanner scanner = new Scanner(System.in);
 
     public void startGame() {
         int[] randNum = generateRand();
@@ -18,6 +21,31 @@ public class gameManager {
         } catch (IllegalArgumentException e) {
             System.out.println(e);
         }
+    }
+
+    private int[] getInput() {
+        System.out.println("숫자를 입력해 주세요 : ");
+        String userInput = scanner.nextLine();
+
+        if (userInput.length() != 3) {
+            throw new IllegalArgumentException("입력이 3자리 수가 아닙니다.");
+        }
+
+        int[] userDigits = new int[]{
+            Character.getNumericValue(userInput.charAt(0)),
+            Character.getNumericValue(userInput.charAt(1)),
+            Character.getNumericValue(userInput.charAt(2))
+        };
+
+        if (!isUniqueDigits(userDigits)) {
+            throw new IllegalArgumentException("각 자리 수는 서로 다른 수로 이루어져야 합니다.");
+        }
+
+        if (!isValidDigits(userDigits)) {
+            throw new IllegalArgumentException("각 숫자는 1부터 9까지 정수이어야 합니다.");
+        }
+
+        return userDigits;
     }
 
     private int[] generateRand() {
