3b11747d59622719684b7f2a610e2c671e215007
stopmin <geemin0@naver.com>
2024-05-05 23:42:31 +0900

    feat: 컴퓨터 숫자 생성 코드

--- src/main/java/domain/ComputerPlayer.java
+++ src/main/java/domain/ComputerPlayer.java
@@ -1,9 +1,29 @@
 package domain;
 
+import static domain.BaseballGame.MAX_NUMBER;
+import static domain.BaseballGame.MIN_NUMBER;
+
+import common.module.RandomNumberGenerator;
+import java.util.ArrayList;
+
 public class ComputerPlayer implements Player {
+
+    private final RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+    private ArrayList<Integer> numbers = new ArrayList<>();
 
     @Override
     public void generateNumbers() {
-        System.out.println("Computer player generates numbers.");
+
+        for (int i = 0; i < BaseballGame.NUMBER_SIZE; i++) {
+            int number = randomNumberGenerator.generateRandomNumberInRange(MIN_NUMBER, MAX_NUMBER);
+            while (numbers.contains(number)) {
+                number = randomNumberGenerator.generateRandomNumberInRange(MIN_NUMBER, MAX_NUMBER);
+            }
+            numbers.add(number);
+        }
+    }
+
+    public ArrayList<Integer> getNumbers() {
+        return numbers;
     }
 }
