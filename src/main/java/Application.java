955dcf48069002f5d5c74461914d3607c88c551f
JJY <ddglackrp@gmail.com>
2024-05-06 03:13:44 +0900

    Refactor "Application 클래스 구조 변경"
    1. static 함수 Application 멤버 함수로 변경
    2. 멤버 변수 추가
    3. 기존 메인 함수 Application start 함수로 변경
    4. 메인 함수 변경

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -6,51 +6,38 @@ import java.util.Scanner;
 
 public class Application {
 
-    public static void main(String[] args) {
-        Computer computer = new Computer();
-        BaseBallGameService gameService = new BaseBallGameService();
-        GameCondition gameCondition = new GameCondition();
+    private final Computer computer;
+    private final BaseBallGameService gameService;
+    private final GameCondition gameCondition;
 
-        Scanner scanner = new Scanner(System.in);
-
-        String number = computer.getNumber();
-        System.out.println(number);
-
-        while(gameCondition.canPlayGame()){
-            System.out.print("숫자를 입력해 주세요 : ");
-            String clientNumber = scanner.nextLine();
-
-            if (inputValidCheck(clientNumber, gameService)) {
-                scanner.close();
-                break;
-            }
-
-            int strike = gameService.checkStrike(computer.getNumber(), clientNumber);
-            int ball = gameService.checkBall(computer.getNumber(), clientNumber);
-
-            printGameInfo(strike, ball, gameCondition, gameService);
-
-            if(gameCondition.canCollectAnswer()){
-                checkRestartGame(scanner, computer, gameCondition);
-            }
-        }
-
-        scanner.close();
+    public Application(Computer computer, BaseBallGameService gameService, GameCondition gameCondition) {
+        this.computer = computer;
+        this.gameService = gameService;
+        this.gameCondition = gameCondition;
     }
 
-    public static boolean inputValidCheck(String clientNumber, BaseBallGameService gameService) {
+    public Computer getComputer() {
+        return computer;
+    }
+
+    private boolean inputValidCheck(String clientNumber) {
+        gameService.checkNumberValid(clientNumber);
+
         try {
-            if (!gameService.checkNumberValid(clientNumber)) {
-                throw new IllegalArgumentException("잘못된 입력입니다. 애플리케이션을 종료합니다.");
-            }
+            return true;
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
-            return true;
+            return false;
         }
-        return false;
     }
 
-    private static void printGameInfo(int strike, int ball, GameCondition gameCondition, BaseBallGameService gameService) {
+    private void continueInputValidCheck(String continueGame) {
+        if(!("1".equals(continueGame) || "2".equals(continueGame))){
+            throw new IllegalArgumentException("입력이 1과 2가 아닙니다. 애플리케이션을 종료합니다.");
+        }
+    }
+
+    private void printGameInfo(int strike, int ball) {
         if(strike == 3){
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
             gameCondition.changeCollectAnswer(true);
@@ -60,10 +47,9 @@ public class Application {
         }
     }
 
-    public static void checkRestartGame(Scanner scanner, Computer computer, GameCondition gameCondition){
+    private void checkRestartGame(String continueGame){
+        continueInputValidCheck(continueGame);
         try {
-            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-            String continueGame = scanner.nextLine();
             if("1".equals(continueGame)){
                 computer.reGenerateNumber();
                 gameCondition.changeCollectAnswer(false);
@@ -71,15 +57,45 @@ public class Application {
             else if("2".equals(continueGame)){
                 gameCondition.changePlayGame(false);
             }
-            else{
-                throw new IllegalArgumentException("잘못된 입력입니다. 애플리케이션을 종료합니다.");
-            }
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
             gameCondition.changePlayGame(false);
         }
+    }
+    public void start(){
+        Scanner scanner = new Scanner(System.in);
 
+        while(gameCondition.canPlayGame()){
+            System.out.print("숫자를 입력해 주세요 : ");
+            String clientNumber = scanner.nextLine();
+
+            if (!inputValidCheck(clientNumber)) {
+                scanner.close();
+                break;
+            }
+
+            int strike = gameService.checkStrike(computer.getNumber(), clientNumber);
+            int ball = gameService.checkBall(computer.getNumber(), clientNumber);
+
+            printGameInfo(strike, ball);
+
+            if(gameCondition.canCollectAnswer()){
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                String continueGame = scanner.nextLine();
+                checkRestartGame(continueGame);
+            }
+        }
+        scanner.close();
+        System.out.println("안녕히 가세요");
     }
 
+    public static void main(String[] args) {
+        Computer computer = new Computer();
+        BaseBallGameService gameService = new BaseBallGameService();
+        GameCondition gameCondition = new GameCondition();
+
+        Application app = new Application(computer, gameService, gameCondition);
+        app.start();
+    }
 
 }
