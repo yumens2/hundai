d7b26c86dfd54ec0928f3628b9ccfdd4d197219e
HSHwan <tnghks2866@gmail.com>
2024-05-05 18:03:02 +0900

    refactor: Player, Computer 클래스와의 결합도 약화

--- src/main/java/domain/Umpire.java
+++ src/main/java/domain/Umpire.java
@@ -11,9 +11,9 @@ public class Umpire {
         this.baseballCount = new BaseballCount();
     }
 
-    public void setBaseballCountWithCalculator(Player player, Computer computer) {
-        int ballCount = BaseballCountCalculator.countBall(player.getPlayerNumber(), computer.getAnswerNumber());
-        int strikeCount = BaseballCountCalculator.countStrike(player.getPlayerNumber(), computer.getAnswerNumber());
+    public void setBaseballCountWithCalculator(String playerNumber, String answerNumber) {
+        int ballCount = BaseballCountCalculator.countBall(playerNumber, answerNumber);
+        int strikeCount = BaseballCountCalculator.countStrike(playerNumber, answerNumber);
         ballCount = BaseballCountCalculator.countBallExceptForStrike(ballCount, strikeCount);
         baseballCount.setBaseballCount(ballCount, strikeCount);
     }
