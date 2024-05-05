bef99022f89be7f65c83bb36d98ea4d84f18cd58
Dalsungmin <cnww123@naver.com>
2024-05-05 14:45:55 +0900

    3자리 숫자 검증
    setNum메서드를 이용해 유효하지 않은 숫자면 IllegalArgumentException을 던진다.

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -13,11 +13,17 @@ public class Application {
         Scanner scan = new Scanner(System.in);
         System.out.print("숫자를 입력해주세요:");
         int input = scan.nextInt();
+        try {
+            setNum(input);
+        }
+        catch (IllegalArgumentException e) {
+            System.out.println("유효하지 않은 숫자입니다.");
+            return;
+        }
         System.out.println(input);
         digits = Stream.of(String.valueOf(input).split(""))
             .mapToInt(Integer::parseInt)
             .toArray(); //입력한 숫자를 분리해서 digits 배열에 넣기
-
     }
 
     static void makeNumber() {
@@ -30,6 +36,14 @@ public class Application {
             if (i == 2 && arr[2] == arr[0]) {
                 i--;
             }
+        }
+    }
+    static void setNum(int num) throws IllegalArgumentException {
+        int n100 = num / 100; num -= n100 * 100;
+        int n10 = num / 10; num -= n10 * 10;
+        int n1 = num;
+        if ( num > 999 || n100 == 0 || n10 == 0 || n1 == 0) {
+            throw new IllegalArgumentException();
         }
     }
 }
