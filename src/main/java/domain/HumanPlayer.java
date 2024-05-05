17f390da999775556e3046f0b5662be0ee40740c
stopmin <geemin0@naver.com>
2024-05-05 23:42:46 +0900

    feat: HumanPlayer 입력 Validation

--- src/main/java/domain/HumanPlayer.java
+++ src/main/java/domain/HumanPlayer.java
@@ -1,9 +1,49 @@
 package domain;
 
+import infra.IOHandler;
+
 public class HumanPlayer implements Player {
+
+    private final IOHandler ioHandler = new IOHandler();
 
     @Override
     public void generateNumbers() {
-        System.out.println("Human player generates numbers.");
+        String input = getInput();
+
+        validateInput(input);
+
+        while (!isValidInput(input)) {
+            ioHandler.print(Message.INVALID_INPUT.getMessage());
+            input = ioHandler.getStringInput();
+        }
+    }
+
+    public String getInput() {
+        ioHandler.print(Message.INPUT_NUMBER.getMessage());
+        return ioHandler.getStringInput();
+    }
+
+    private void validateInput(String input) {
+        if (input.length() != BaseballGame.NUMBER_SIZE) {
+            throw new IllegalArgumentException(Message.INVALID_LENGTH.getMessage());
+        }
+        if (!isNumber(input)) {
+            throw new IllegalArgumentException(Message.INVALID_NUMBER.getMessage());
+        }
+        if (hasDuplicatedNumber(input)) {
+            throw new IllegalArgumentException(Message.DUPLICATED_NUMBER.getMessage());
+        }
+    }
+
+    private boolean isValidInput(String input) {
+        return input.length() == BaseballGame.NUMBER_SIZE && isNumber(input) && !hasDuplicatedNumber(input);
+    }
+
+    private boolean isNumber(String input) {
+        return input.chars().allMatch(Character::isDigit);
+    }
+
+    private boolean hasDuplicatedNumber(String input) {
+        return input.chars().distinct().count() != BaseballGame.NUMBER_SIZE;
     }
 }
