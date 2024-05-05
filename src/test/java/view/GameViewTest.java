a376d5160ca4eebce2696680f7b1a2c79f7f13f8
us4c0d3 <jwseok312@gmail.com>
2024-05-06 01:59:09 +0900

    test($GameView): create displayResult test code

--- src/test/java/view/GameViewTest.java
+++ src/test/java/view/GameViewTest.java
@@ -3,7 +3,9 @@ package view;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.io.PrintStream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -22,5 +24,28 @@ class GameViewTest {
 
         //then
         assertEquals(expectedInput, actualInput);
+    }
+
+    @Test
+    @DisplayName("displayResult test")
+    void displayResultTest() throws Exception {
+        //given
+        String input1 = "1볼 1스트라이크";
+        String input2 = "낫싱";
+        String input3 = "3스트라이크";
+
+        ByteArrayOutputStream outputContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputContent));
+        GameView gameView = new GameView(System.in);
+
+        //when
+        gameView.displayResult(input1);
+        gameView.displayResult(input2);
+        gameView.displayResult(input3);
+
+        //then
+        String expectedOutput = input1 + "\n" + input2 + "\n" + input3 + "\n";
+        assertEquals(expectedOutput, outputContent.toString());
+
     }
 }
