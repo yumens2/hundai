13faced6b1475eb70047af8592e5c7853da1bd86
Dalsungmin <cnww123@naver.com>
2024-05-05 14:50:52 +0900

    입력받은 숫자와 생성된 숫자 비교
    숫자와 자리가 일치하면 스트라이크 +1
    숫자만 일치하고 자리만 일치하면 볼 +1

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -5,6 +5,8 @@ public class Application {
 
     static int[] arr = {0, 0, 0};
     static int[] digits;
+    static int strike = 0;
+    static int ball = 0;
 
     public static void main(String[] args) {
 
@@ -23,7 +25,9 @@ public class Application {
         System.out.println(input);
         digits = Stream.of(String.valueOf(input).split(""))
             .mapToInt(Integer::parseInt)
-            .toArray(); //입력한 숫자를 분리해서 digits 배열에 넣기
+            .toArray();
+
+        judge();//입력한 숫자를 분리해서 digits 배열에 넣기
     }
 
     static void makeNumber() {
@@ -44,6 +48,23 @@ public class Application {
         int n1 = num;
         if ( num > 999 || n100 == 0 || n10 == 0 || n1 == 0) {
             throw new IllegalArgumentException();
+        }
+    }
+    static void judge(){
+        for (int l = 0; l < arr.length; l++) {
+            if (arr[l] == digits[l]) {
+                strike += 1; //숫자와 자리가 일치하면 스트라이크 +1
+
+            }
+            if (arr[0] == digits[l] && arr[0] != digits[0]) { // 숫자만 같고 자리만 다를경우 볼 +1
+                ball += 1;
+            }
+            if (arr[1] == digits[l] && arr[1] != digits[1]) {
+                ball += 1;
+            }
+            if (arr[2] == digits[l] && arr[2] != digits[2]) {
+                ball += 1;
+            }
         }
     }
 }
