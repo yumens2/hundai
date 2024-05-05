fa2b9c19e64157536844c2f719144742ff8ec38c
donghyun kim <gimdonghyun5434@gmail.com>
2024-05-06 06:00:24 +0900

    [style] 불필요한 공백제거

--- src/test/java/JudgeTest.java
+++ src/test/java/JudgeTest.java
@@ -1,12 +1,7 @@
-
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import java.util.Arrays;
-
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class JudgeTest {
     Compare compare;
