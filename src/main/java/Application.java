b32d80e5401480703705037b01268d8b1603146b
안원모 <tommy45231@naver.com>
2024-05-05 12:25:06 +0900

    무작위 숫자 생성 기능 추가

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -16,6 +16,15 @@ public class Application{
         }
         return numbers;
     }
+    public List<Integer> generateRandomNumbers() {
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 1; i <= 9; i++) {
+            numbers.add(i);
+        }
+        Collections.shuffle(numbers);
+        return numbers.subList(0, 3);
+    }
+
 
 
 
