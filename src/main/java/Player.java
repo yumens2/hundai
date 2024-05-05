742f966443a39b553cafa2d384330e80200e8934
unknown <dn06023@naver.com>
2024-05-05 13:58:33 +0900

    feat: Player 클래스 추가
    System.in에서 숫자를 입력받는 클래스 생성.

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Player {
+import java.util.Scanner;
+import java.util.Vector;
+
+public class Player {
+    private Vector<Integer> num;
+
+    public Vector<Integer> getNum() {
+        return num;
+    }
+
+    public void pickNumber(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("숫자를 입력해 주세요 : ");
+        int temp = scanner.nextInt();
+        if(temp < 111 || temp > 999)
+            throw new IllegalArgumentException("Invalid argument: " + temp);
+
+        num = new Vector<Integer>();
+        num.add(temp / 100);
+        num.add((temp / 10) % 10);
+        num.add(temp % 10);
+    }
 }
