ac196a3af2397163c674f0c171bb02a8e050daa6
HSHwan <tnghks2866@gmail.com>
2024-05-05 13:30:42 +0900

    refactor: 유효성 검사 메서드명 동사 변경 및 예외 발생 부분을 메서드로 대체

--- src/main/java/utility/RestartNumberValidator.java
+++ src/main/java/utility/RestartNumberValidator.java
@@ -3,16 +3,16 @@ package utility;
 import java.util.Arrays;
 import java.util.List;
 
-public class RestartNumberValidator {
+public class RestartNumberValidator extends InputNumberValidator {
 
     private static final List<String> restartNumberList = Arrays.asList("1", "2");
 
-    public static void isRestartNumberCorrect(String inputNumber) {
+    public static void checkRestartNumberCorrect(String inputNumber) {
         for (String restartNumber : restartNumberList) {
             if (inputNumber.equals(restartNumber)) {
                 return;
             }
         }
-        throw new IllegalArgumentException();
+        raiseIllegalArgumentException();
     }
 }
