bc2aeddeeeaccb9e78d3a2a395e1d8e0163f0948
seojiu <seojiu5598@gmail.com>
2024-05-05 22:05:51 +0900

    feat(Randomint): 3자리수 입력받기(사용자)

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -4,5 +4,8 @@ public class Application {
         Randomint rd= new Randomint();
         int[] arr1= new int[3];
         rd.randint(arr1);
+
+        int[] arr2= new int[3];
+        rd.myint(arr2);
     }
 }
--- src/main/java/Randomint.java
+++ src/main/java/Randomint.java
@@ -1,4 +1,6 @@
 import java.util.Random;
+import java.util.Scanner;
+
 public class Randomint {
 
     int[] randint(int[] arr1) {
@@ -23,5 +25,37 @@ public class Randomint {
 
 
         return arr1;
+    }
+
+
+
+    int[] myint(int[] arr2) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("숫자를 입력해 주세요 : ");
+        int n = scanner.nextInt();
+
+        //숫자 입력 오류시 완전종료
+        if (n >999 || n<100) {
+            throw new IllegalArgumentException("3자리수가 아닙니다.");
+        }
+
+        int c2= n%10;
+        arr2[2]= c2;
+        n/=10;
+        int b2= n%10;
+        arr2[1]= b2;
+        n/=10;
+        int a2= n%10;
+        arr2[0]= a2;
+
+        if (a2==b2 || b2==c2 || a2==c2) {
+            throw new IllegalArgumentException("중복된 숫자입니다.");
+        }
+        if (a2==0 || b2==0 || c2==0) {
+            throw new IllegalArgumentException("1~9 사이의 숫자가 아닙니다.");
+        }
+
+
+        return arr2;
     }
 }
