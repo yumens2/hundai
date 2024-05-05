737623612a88010668b5e60a70f1bdc1f91b896a
박민규 <pmq0101@naver.com>
2024-05-05 13:17:12 +0900

    feat(CheckNum): ball 체크 기능 추가

--- src/main/java/CheckNum.java
+++ src/main/java/CheckNum.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.stream.IntStream;
 
 public class CheckNum {
@@ -6,5 +7,15 @@ public class CheckNum {
             .filter(i -> com[i] == user[i])
             .map(i -> 1)
             .sum();
+    }
+    
+    public int ballCheck(int[]com, int[]user){
+        return totalCheck(com, user) - strikeCheck(com, user);
+    }
+
+    private int totalCheck(int[] com, int[] user) {
+        return (int) Arrays.stream(com)
+            .filter(num -> Arrays.stream(user).anyMatch(x -> x == num))
+            .count();
     }
 }
