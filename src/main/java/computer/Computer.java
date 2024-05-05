f1e78654d3c47465f35601838472d601b0a865da
ddglackrp <ddglackrp@naver.com>
2024-05-05 17:25:33 +0900

    Feat "Computer 클래스 멤버 변수 초기화 기능 추가"
    1, createBaseballNumber()
    - 1~9 까지 수 중 겹치지 않는 3자리 랜덤 수 생성
    2. 생성자
    - 클래스 생성시 createBaseballNumber() 함수를 이용한 3자리 수 초기화

--- src/main/java/computer/Computer.java
+++ src/main/java/computer/Computer.java
@@ -1,4 +1,31 @@
 package computer;
 
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
 public class Computer {
+
+    private String number;
+
+    public Computer() {
+        number = createBaseballNumber();
+    }
+
+    private String createBaseballNumber(){
+        Random random = new Random();
+        Set<Integer> numbers = new HashSet<>();
+
+        while(numbers.size() < 3){
+            int randomNumber = random.nextInt(9) + 1;
+            numbers.add(randomNumber);
+        }
+
+        String createNumber = "";
+        for (Integer num : numbers) {
+            createNumber += num;
+        }
+
+        return createNumber;
+    }
 }
