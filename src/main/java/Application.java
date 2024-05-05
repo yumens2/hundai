2d5f50c5f034908b0dba7b25523cedeb82312566
Kim Sanghae <pelikan@pusan.ac.kr>
2024-05-06 04:53:54 +0900

    feat: Add ballcount status

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,24 +1,20 @@
-import Entity.Computer;
-import Entity.User;
+import model.Computer;
+import model.User;
+import controller.GameController;
+import view.UserInterface;
 
 public class Application {
 
-    private final static String threeStrike = "3스트라이크";
-
     public static void main(String[] args) {
-        // NOTE 사용자 또는 컴퓨터의 숫자를 생성하는 NumberGenerating 클래스
         User user = new User();
         Computer computer = new Computer();
-        NumberBaseballGame numberBaseballGame = new NumberBaseballGame(user, computer);
-        System.out.println(numberBaseballGame.printStartingGame());
-        numberBaseballGame.setComputerNumbers();
-        String result = "";
-        do {
-            numberBaseballGame.setUserNumbers();
-            result = numberBaseballGame.calculateScore();
-            System.out.println(result);
-        } while (!result.equals(threeStrike));
-        numberBaseballGame.closeScanner();
-        System.out.println(numberBaseballGame.printEndingGmae());
+
+        UserInterface userInterface = new UserInterface();
+        GameController gameController = new GameController(user, computer);
+
+        UserInterface.printStartingGame();
+        gameController.startGame();
+        UserInterface.printEndingGame();
+        userInterface.closeScanner();
     }
 }
