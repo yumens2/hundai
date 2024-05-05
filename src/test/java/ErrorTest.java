7a6a9941eb666e78b0eedea0c35ac6ea9b79eeb7
rbgusdn12@gmail.com <rbgus4056@>
2024-05-05 18:21:52 +0900

    테스트 코드 작성 및 테스트 완료

--- src/test/java/ErrorTest.java
+++ src/test/java/ErrorTest.java
@@ -1,4 +1,36 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ErrorTest {
-  
+
+    @Test
+    //check this is 3-digit number
+    void detectErrorTrue() {
+        Error error = new Error();
+        assertEquals(error.detectError("123"), 1);
+
+    }
+
+    @Test
+    //check 3-digit number
+    void detectErrorFalse() {
+        Error error = new Error();
+        assertEquals(error.detectError("1234"), 2);
+
+    }
+
+    @Test
+    //check redundant number
+    void isValidDataRedundant() {
+        Error error = new Error();
+        assertEquals(error.isValidData("112"), true);
+    }
+
+    @Test
+    //check not number like String
+    void isValidDataString() {
+        Error error = new Error();
+        assertEquals(error.isValidData("1h3"), true);
+    }
 }
