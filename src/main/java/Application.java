242d92d160b1e8a23210ba2746064da411cd8c79
JJY <ddglackrp@gmail.com>
2024-05-06 01:33:11 +0900

    Refactor "Application 리팩토링 진행"
    1. 유효하지 않은 입력에 대해 try-catch 문으로 exception handling
    2. static method 추출

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,82 +1,84 @@
 import computer.Computer;
-import game.BaseBallGame;
+import game.BaseBallGameService;
+import game.GameCondition;
 
 import java.util.Scanner;
 
 public class Application {
-    private static boolean playGame = true;
-    private static boolean collectAnswer = false;
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         Computer computer = new Computer();
-        BaseBallGame baseBallGame = new BaseBallGame();
+        BaseBallGameService gameService = new BaseBallGameService();
+        GameCondition gameCondition = new GameCondition();
+
         Scanner scanner = new Scanner(System.in);
 
         String number = computer.getNumber();
-
         System.out.println(number);
 
-        while(playGame){
+        while(gameCondition.canPlayGame()){
             System.out.print("숫자를 입력해 주세요 : ");
             String clientNumber = scanner.nextLine();
 
-            if(!checkNumberValid(clientNumber)){
-                throw new IllegalArgumentException("잘못된 입력입니다. 애플리케이션을 종료합니다.");
+            if (inputValidCheck(clientNumber, gameService)) {
+                scanner.close();
+                break;
             }
 
-            int strike = baseBallGame.checkStrike(computer.getNumber(), clientNumber);
-            int ball = baseBallGame.checkBall(computer.getNumber(), clientNumber);
+            int strike = gameService.checkStrike(computer.getNumber(), clientNumber);
+            int ball = gameService.checkBall(computer.getNumber(), clientNumber);
 
-            if(strike == 3){
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                collectAnswer = true;
-            }else{
-                baseBallGame.printGameInfo(strike, ball);
-            }
+            printGameInfo(strike, ball, gameCondition, gameService);
 
-            if(collectAnswer){
-                checkRestartGame(scanner, computer);
+            if(gameCondition.canCollectAnswer()){
+                checkRestartGame(scanner, computer, gameCondition);
             }
         }
 
         scanner.close();
     }
 
-    public static Boolean checkNumberValid(String number){
-        if (number.length() != 3) {
-            return false;
-        }
-
-        for (int i = 0; i < number.length(); i++) {
-            char c = number.charAt(i);
-
-            if (!Character.isDigit(c) || c == '0') {
-                return false;
+    public static boolean inputValidCheck(String clientNumber, BaseBallGameService gameService) {
+        try {
+            if (!gameService.checkNumberValid(clientNumber)) {
+                throw new IllegalArgumentException("잘못된 입력입니다. 애플리케이션을 종료합니다.");
             }
-
-            long count = number.chars().filter(n -> n == c).count();
-            if (count > 1){
-                return false;
-            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return true;
         }
-
-        return true;
+        return false;
     }
 
-    public static void checkRestartGame(Scanner scanner, Computer computer){
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-        String continueGame = scanner.nextLine();
+    private static void printGameInfo(int strike, int ball, GameCondition gameCondition, BaseBallGameService gameService) {
+        if(strike == 3){
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            gameCondition.changeCollectAnswer(true);
+        }else{
+            String info = gameService.printGameInfo(strike, ball);
+            System.out.println(info);
+        }
+    }
 
-        if("1".equals(continueGame)){
-            computer.reGenerateNumber();
-            collectAnswer = false;
+    public static void checkRestartGame(Scanner scanner, Computer computer, GameCondition gameCondition){
+        try {
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            String continueGame = scanner.nextLine();
+            if("1".equals(continueGame)){
+                computer.reGenerateNumber();
+                gameCondition.changeCollectAnswer(false);
+            }
+            else if("2".equals(continueGame)){
+                gameCondition.changePlayGame(false);
+            }
+            else{
+                throw new IllegalArgumentException("잘못된 입력입니다. 애플리케이션을 종료합니다.");
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            gameCondition.changePlayGame(false);
         }
-        else if("2".equals(continueGame)){
-            playGame = false;
-        }
-        else{
-            throw new IllegalArgumentException("잘못된 입력입니다. 애플리케이션을 종료합니다.");
-        }
+
     }
 
 
