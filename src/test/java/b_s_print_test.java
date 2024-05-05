29cd7e9efb2b5d2dbf1ab03381f93f60046aad01
rbm0524 <rbm0524@naver.com>
2024-05-06 08:01:35 +0900

    출력값 낫싱에 대한 test 케이스 추가

--- src/test/java/b_s_print_test.java
+++ src/test/java/b_s_print_test.java
@@ -42,6 +42,50 @@ public class b_s_print_test {
         String expectedOutput2 = "3스트라이크"; // 예상되는 출력 메시지
         String capturedOutput2 = outContent.toString().trim(); // 문자열 앞뒤의 공백 및 줄 바꿈 문자 제거
         assertEquals(expectedOutput2, capturedOutput2);
+
+
     }
+    @Test
+    public void testCheckExit2() throws IOException {
+        //2차 테스트
+        // 표준 출력을 임시로 캡처하기 위한 ByteArrayOutputStream 생성
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        // 임의 판정 결과 생성
+        int[] judgement = {0, 3};
+
+        // 메서드 호출
+        BaseballGame.b_s_print(judgement);
+
+        // 표준 출력에서 캡처한 내용을 문자열로 변환하여 확인
+        String expectedOutput = "3스트라이크"; // 예상되는 출력 메시지
+        String capturedOutput = outContent.toString().trim(); // 문자열 앞뒤의 공백 및 줄 바꿈 문자 제거
+        assertEquals(expectedOutput, capturedOutput);
+
+
+    }
+
+    @Test
+    public void testCheckExit3() throws IOException {
+        //2차 테스트
+        // 표준 출력을 임시로 캡처하기 위한 ByteArrayOutputStream 생성
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        // 임의 판정 결과 생성
+        int[] judgement = {0, 0};
+
+        // 메서드 호출
+        BaseballGame.b_s_print(judgement);
+
+        // 표준 출력에서 캡처한 내용을 문자열로 변환하여 확인
+        String expectedOutput = "낫싱"; // 예상되는 출력 메시지
+        String capturedOutput = outContent.toString().trim(); // 문자열 앞뒤의 공백 및 줄 바꿈 문자 제거
+        assertEquals(expectedOutput, capturedOutput);
+
+
+    }
+
 
 }
