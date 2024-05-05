741123aaae1ce9e420d127232fc2d10562b2d147
ddglackrp <ddglackrp@naver.com>
2024-05-05 17:37:45 +0900

    Feat "스트라이크, 볼 정보 출력 로직 추가"
    - printGameInfo : 스트라이브, 볼, 낫싱 정보 출력하는 함수

--- src/main/java/game/BaseBallGame.java
+++ src/main/java/game/BaseBallGame.java
@@ -26,4 +26,17 @@ public class BaseBallGame {
         return ball;
     }
 
+    public void printGameInfo(int strike, int ball){
+        if(strike == 0 && ball == 0){
+            System.out.println("낫싱");
+        } else if(strike > 0 && ball == 0) {
+            System.out.println(strike+"스트라이크");
+        } else if(strike == 0 && ball > 0){
+            System.out.println(ball+"볼");
+        }
+        else{
+            System.out.println(ball+"볼 "+strike+"스트라이크");
+        }
+    }
+
 }
