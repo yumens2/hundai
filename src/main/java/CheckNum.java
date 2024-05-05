926cbb55eb89015255df923e2c7847130ca52757
박민규 <pmq0101@naver.com>
2024-05-05 13:16:49 +0900

    feat(CheckNum): strike 체크 기능 추가

--- src/main/java/CheckNum.java
+++ src/main/java/CheckNum.java
@@ -1,3 +1,10 @@
-public class CheckNum {
+import java.util.stream.IntStream;
 
+public class CheckNum {
+    public int strikeCheck(int[]com, int[]user){
+        return IntStream.range(1, 1 + GameNum.GAME_NUM.getGameNum())
+            .filter(i -> com[i] == user[i])
+            .map(i -> 1)
+            .sum();
+    }
 }
