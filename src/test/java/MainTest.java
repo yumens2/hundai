e99dedd4c644de4f989052922394d2321db215e1
ana-raka <junglkls1234@gmail.com>
2024-05-05 16:32:51 +0900

    test

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void initList() {
+    }
+
+    @Test
+    void makeAttack() {
+    }
+
+    @Test
+    void makeAns() {
+    }
+
+    @Test
+    void compareAnswer() {
+    }
+
+    @Test
+    void makePredict() {
+    }
+
+    @Test
+    void printCompareResult() {
+    }
+
+    @Test
+    void main() {
+    }
+}
