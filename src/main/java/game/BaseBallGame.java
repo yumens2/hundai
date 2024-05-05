249be99cfbc8932ef0c5c4dee1f5c3ccdacd22d0
ddglackrp <ddglackrp@naver.com>
2024-05-05 17:34:14 +0900

    Feat "볼 개수 확인 로직 추가"
    - checkBall : 사용자의 입력을 이용해 볼 수 체크하는 함수 추가

--- src/main/java/game/BaseBallGame.java
+++ src/main/java/game/BaseBallGame.java
@@ -14,4 +14,16 @@ public class BaseBallGame {
         return strike;
     }
 
+    public int checkBall(String computerNumber, String userNumber){
+        int ball = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (i != j && computerNumber.charAt(i) == userNumber.charAt(j)) {
+                    ball++;
+                }
+            }
+        }
+        return ball;
+    }
+
 }
