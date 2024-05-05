7fbe888bc1dac7f7b7fbb01b11350388f1016aab
안원모 <tommy45231@naver.com>
2024-05-05 12:21:19 +0900

    입력값 파싱 기능 구현

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -5,10 +5,18 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Application{
-   
+
     public boolean containsDuplicate(List<Integer> digits) {
         return digits.stream().distinct().count() != digits.size();
     }
+    public List<Integer> parseInput(String input) {
+        List<Integer> numbers = new ArrayList<>();
+        for (char c : input.toCharArray()) {
+            numbers.add(Character.getNumericValue(c));
+        }
+        return numbers;
+    }
+
 
 
 }
