41bbff766ef0adb6c664edd6136cdb02df73fe55
us4c0d3 <jwseok312@gmail.com>
2024-05-05 20:46:47 +0900

    feat($Computer): implement Computer class to pass test
    `Computer`클래스 테스트 코드를 통과하도록 기능 구현
    - 클래스 생성자에서 조건에 맞는 랜덤 숫자 배열 생성
    - getter를 통해 숫자 배열 반환

--- src/main/java/model/Computer.java
+++ src/main/java/model/Computer.java
@@ -6,7 +6,7 @@ public class Computer {
     private int[] numbers;
 
     public Computer() {
-        //TODO
+        numbers = NumberUtil.generateRandomNumber();
     }
 
     public int[] getNumber() {
