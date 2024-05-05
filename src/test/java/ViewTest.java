0830b4b12275273f0fcaf001dde117ae7d545dc5
Yeonwoo JI <95765163+speciling@users.noreply.github.com>
2024-05-05 22:22:45 +0900

    test: View 테스트 코드 추가

---
+++ src/test/java/ViewTest.java
@@ -0,0 +1,75 @@
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ViewTest {
+
+    OutputStream out;
+
+    @BeforeEach
+    void beforeEach() {
+        out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+    }
+
+    @Test
+    void guessView() {
+        InputStream in = new ByteArrayInputStream("123".getBytes());
+        System.setIn(in);
+
+        int num1 = View.guessView();
+
+        Assertions.assertThat(num1).isEqualTo(123);
+    }
+
+    @Test
+    void hintViewNothing() {
+        ResultDto resultDto = new ResultDto(0, 0);
+
+        View.hintView(resultDto);
+
+        Assertions.assertThat(out.toString().trim()).isEqualTo("낫싱");
+    }
+
+    @Test
+    void hintViewStrike() {
+        ResultDto resultDto = new ResultDto(2, 0);
+
+        View.hintView(resultDto);
+
+        Assertions.assertThat(out.toString().trim()).isEqualTo("2스트라이크");
+    }
+
+    @Test
+    void hintViewBall() {
+        ResultDto resultDto = new ResultDto(0, 3);
+
+        View.hintView(resultDto);
+
+        Assertions.assertThat(out.toString().trim()).isEqualTo("3볼");
+    }
+
+    @Test
+    void hintViewBoth() {
+        ResultDto resultDto = new ResultDto(1, 1);
+
+        View.hintView(resultDto);
+
+        Assertions.assertThat(out.toString().trim()).isEqualTo("1볼 1스트라이크");
+    }
+
+    @Test
+    void successView() {
+        InputStream in = new ByteArrayInputStream("1".getBytes());
+        System.setIn(in);
+
+        int num1 = View.guessView();
+
+        Assertions.assertThat(num1).isEqualTo(1);
+    }
+}
