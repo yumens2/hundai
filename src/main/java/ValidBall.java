29134cdbe0629c5868c7f26d3e8a0b63ab67204b
capDoYeonLee <ghtyu21@naver.com>
2024-05-06 00:53:36 +0900

    feat: 사용자 입력받은 공 유효성 검사 기능

--- src/main/java/ValidBall.java
+++ src/main/java/ValidBall.java
@@ -1,22 +1,35 @@
 import java.util.HashSet;
 
-public class ValidBall implements Valid {
+public class ValidBall {
 
-    @Override
-    public void valid(int number) {
+    public void validate(int answer) {
+        validateInputType(answer);
+        validateRange(answer);
+        validateDuplication(answer);
+    }
 
-        HashSet<Integer> set = new HashSet<>();
-
-        if ((number < 111) || (number > 999)) {
-            throw new IllegalArgumentException("잘못된 범위의 입력입니다");
+    // 아래 validateInputTpye 메서드가 의미가 있을까? MajorLeague에서 결국 인풋 받을 때 Integer.parseInt로 파싱하는데 문자열이 입력되면
+    //결국 거기서 에러가 발생할텐데, 그럼 이건 테스트에 넣자
+    private <T> void validateInputType(T answer) {
+        if (!(answer instanceof Integer)) {
+            throw new IllegalArgumentException("잘못된 입력입니다. 다시 시도하세요.");
         }
+    }
 
-        while (number > 0) {
-            int digit = number % 10;
+    private void validateDuplication(int answer) {
+        HashSet<Integer> set = new HashSet<>();
+        while (answer > 0) {
+            int digit = answer % 10;
             if (!set.add(digit)) {
                 throw new IllegalArgumentException("입력된 숫자 중 중복되는 숫자가 있습니다.");
             }
-            number /= 10;
+            answer /= 10;
+        }
+    }
+
+    private void validateRange(int number) {
+        if ((number < 111) || (number > 999)) {
+            throw new IllegalArgumentException("잘못된 범위의 입력입니다");
         }
     }
 }
