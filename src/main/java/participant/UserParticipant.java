d6e25a7dbe58fdc4e409c4dc10ab259316edfd75
JinSeoHyun <15roa1121@gmail.com>
2024-05-05 17:42:39 +0900

    feat(UserParticipant): 입력 구현체 작성: 사용자에게 입력받음

---
+++ src/main/java/participant/UserParticipant.java
@@ -0,0 +1,17 @@
+package src.main.java.participant;
+
+import java.util.Scanner;
+
+public class UserParticipant implements Participant {
+
+    @Override
+    public String inputValue() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("1부터 9까지의 숫자 3개를 입력하세요: ");
+        String number = scanner.nextLine();
+        scanner.close();
+
+        return number;
+    }
+}
