9f84baae8b4a6a0fafbcb26945834664644f5dcd
Kiwoong Kim <gw150311@naver.com>
2024-05-05 23:06:07 +0900

    정답 (서로 다른 숫자 3개로 이루어진 3자리수) 생성

---
+++ src/main/java/game/RandomAnswerGenerator.java
@@ -0,0 +1,27 @@
+package game;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class RandomAnswerGenerator {
+    private static int nDigit = 3;
+    private List<Integer> digits = null;
+
+    public RandomAnswerGenerator() {
+        digits = new ArrayList<>(9);
+        for (int i = 1; i < 10; i++) {
+            digits.add(i);
+        }
+    }
+
+    public String getAnswerAsString() {
+        String answer = "";
+        for (int i = 0; i < nDigit; i ++) {
+            Collections.shuffle(digits);
+            int d = digits.remove(digits.size() - 1);
+            answer += String.valueOf(d);
+        }
+        return answer;
+    }
+}
