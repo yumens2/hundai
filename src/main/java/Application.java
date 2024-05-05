d448b0fd721a60af7a5b73ab03ede01e625b3059
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-05-06 00:11:29 +0900

    feat: 메인 실행 구현

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,10 @@
+import controller.GameManager;
+import util.Constants;
+
 public class Application {
     public static void main(String[] args) {
-        
+        GameManager gameManager = new GameManager();
+        System.out.println(Constants.GAME_START_MESSAGE);
+        gameManager.gameStart();
     }
 }
