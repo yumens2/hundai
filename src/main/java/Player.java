e3f52728c1f3db27397f707fd184db6a1daaa7f7
Kangboom <jkl6147@naver.com>
2024-05-05 23:21:02 +0900

    feat(Player): Add exception handling

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,48 +1,62 @@
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Player {
+    ArrayList<Integer> userInput = new ArrayList<>();
     public ArrayList<Integer> input(){
         Scanner scan = new Scanner(System.in);
-        ArrayList<Integer> userInput = new ArrayList<Integer>();
-
+        userInput.clear();
+        
         System.out.print("숫자를 입력해 주세요 : ");
         String userString = scan.nextLine();
 
-        check(userString);
+        inputCheck(userString);
         for (int i = 0; i < userString.length(); i++){
             int number = Character.getNumericValue(userString.charAt(i));
             userInput.add(number);
         }
+
         return userInput;
     }
-    public void check(String user){
-        if(user.length() != 3){
-            throw new IllegalArgumentException("잘못된 입력입니다.");
+    public void inputCheck(String userString){
+        if(userString.length() != 3){
+            throw new IllegalArgumentException("3개의 숫자를 입력해주세요.");
         }
         for(int i=0; i<3; i++){
-            if(!Character.isDigit(user.charAt(i))){
-                throw new IllegalArgumentException("잘못된 입력입니다.");
+            char ch = userString.charAt(i);
+
+            if(!Character.isDigit(ch)){
+                throw new IllegalArgumentException("숫자를 입력해주세요");
             }
-            if(user.indexOf(user.charAt(i)) != i){
-                throw new IllegalArgumentException("잘못된 입력입니다.");
+            if(Character.getNumericValue(ch)<1 || Character.getNumericValue(ch) > 9){
+                throw new IllegalArgumentException("1~9사이의 숫자를 입력해주세요");
+            }
+            if(userString.indexOf(ch) != i){
+                throw new IllegalArgumentException("중복된 값 입니다.");
             }
         }
     }
     public boolean restart(){
-        Scanner scan = new Scanner(System.in);
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
-        int flag = scan.nextInt();
-        if (flag == 1){
-            return true;
-        }
-        else if(flag == 2){
-            return false;
-        }
-        else{
-            throw new IllegalArgumentException("잘못된 입력입니다.");
+        try {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
+
+            int flag = scan.nextInt();
+            scan.nextLine();
+
+            if (flag == 1){
+                return true;
+            }
+            else if(flag == 2){
+                return false;
+            }
+            else{
+                throw new IllegalArgumentException("1또는 2를 입력해주세요");
+            }
+        }catch (InputMismatchException e){
+            throw new IllegalArgumentException("1또는 2를 입력해주세요");
         }
     }
 }
