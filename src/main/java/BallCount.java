41e59c02d41c3872ce95ad3a687e57bba504e271
yjhannn <114985804+yjhannn@users.noreply.github.com>
2024-05-06 00:13:13 +0900

    comment add

--- src/main/java/BallCount.java
+++ src/main/java/BallCount.java
@@ -3,10 +3,11 @@ import java.util.List;
 import java.util.Set;
 
 public class BallCount {
-    int Num = 3;
+    int Num = 3;    // 숫자 개수는 총 3개
 
     public boolean Count(String input, List<Integer> CNum) {
         if (!isValidInput(input)) {
+            // 입력 값 형식 올바르지 않을 시 실행
             throw new IllegalArgumentException("숫자를 올바르지 않은 형식으로 입력하셨습니다.");
         }
         int strike = 0, ball = 0;
@@ -33,13 +34,14 @@ public class BallCount {
         return strike == Num;
     }
 
+    // 입력값 올바른지 판단해주는 메서드, boolean으로 구현
     public boolean isValidInput(String input) {
-        if(input.length() != Num) {
+        if(input.length() != Num) { // input의 길이가 일치하지 않을 시 실행
             return false;
         }
         Set<Character> dig = new HashSet<>();
         for (char c : input.toCharArray()) {
-            if (!Character.isDigit(c) || !dig.add(c)) {
+            if (!Character.isDigit(c) || !dig.add(c)) { //숫자 형태가 아님 || 중복된 숫자
                 return false;
             }
         }
--- src/main/java/CreateCnum.java
+++ src/main/java/CreateCnum.java
@@ -5,14 +5,14 @@ import java.util.Random;
 import java.util.Set;
 
 public class CreateCnum {
-    int Num = 3;
+    int Num = 3;    // 숫자 개수는 3
 
     public List<Integer> ComputerNumber() {
         List<Integer> CNum = new ArrayList<>();
         Set<Integer> usedNum = new HashSet<>();
         Random random = new Random();
         while (CNum.size() < Num) {
-            int dig = random.nextInt(9) + 1;
+            int dig = random.nextInt(9) + 1;    // 랜덤으로 숫자 생성
             if (usedNum.add(dig))
                 CNum.add(dig);
         }
