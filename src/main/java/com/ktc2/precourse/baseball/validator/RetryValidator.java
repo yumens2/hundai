af09a0a0e9ebefb67aef0fccb474784ca44bf845
lja3723 <lja3723@gmail.com>
2024-05-05 15:23:17 +0900

    feat: implement RetryValidator 게임 종료 선택지에 대한 사용자 입력 유효성 검사기 구현

--- src/main/java/com/ktc2/precourse/baseball/validator/RetryValidator.java
+++ src/main/java/com/ktc2/precourse/baseball/validator/RetryValidator.java
@@ -1,8 +1,20 @@
 package com.ktc2.precourse.baseball.validator;
 
 public class RetryValidator implements Validator<String> {
+    private static final RetryValidator instance = new RetryValidator();
+
+    private RetryValidator() {}
+
+    public static RetryValidator getInstance() {
+        return instance;
+    }
+
     @Override
     public boolean isValid(String string) {
-        return false;
+        if (string.length() != 1) {
+            return false;
+        }
+
+        return string.matches("[12]");
     }
 }
