74dcf2672ec2fa75f28081216f2588d73a1179c5
unknown <dn06023@naver.com>
2024-05-05 14:05:09 +0900

    feat: Game 클래스 생성
    게임을 실행하고, 3스트라이크 시 재시작 여부를 묻는 클래스.

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Game {
+import java.util.Scanner;
+
+public class Game {
+    Player player;
+    Computer computer;
+    Compare compare;
+
+    public Game() {
+        start();
+    }
+
+    public void start(){
+        this.player = new Player();
+        this.computer = new Computer();
+        this.compare = new Compare();
+        do{
+            player.pickNumber();
+        }
+        while(compare.compareTo(player.getNum(), computer.getNum()) == false);
+
+        System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        Scanner scanner = new Scanner(System.in);
+        int res = scanner.nextInt();
+        if(res == 1) start();
+    }
 }
