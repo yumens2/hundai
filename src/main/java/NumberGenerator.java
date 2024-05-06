4c32fe36b09fe7f913bffaacbb5709084bda31dd
cussle <cussle@kakao.com>
2024-05-06 11:44:03 +0900

    fix: Replace HashSet with LinkedHashSet to maintain insertion order
    컴퓨터가 생성한 숫자를 관리할 때 HashSet이 아닌 LinkedHashSet을 사용하도록 변경합니다.
    HashSet은 순서가 보장되지 않기에 라운드가 진행될 때마다 숫자가 바뀔 수 있습니다.
    따라서 Set의 기능을 그대로 사용할 수 있지만 순서가 유지되는 LinkedHashSet로 변경합니다.

--- src/main/java/NumberGenerator.java
+++ src/main/java/NumberGenerator.java
@@ -1,10 +1,11 @@
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Random;
 import java.util.Set;
 
 public class NumberGenerator {
     public Set<Integer> generateRandomNumbers() {
-        Set<Integer> numbers = new HashSet<>();
+        Set<Integer> numbers = new LinkedHashSet<>();
         Random random = new Random();
         while (numbers.size() < 3) {
             int number = 1 + random.nextInt(9);  // 1 to 9
