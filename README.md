ece848c63cc7271d11bec1620962518a3354976d
minjae <minjae4650@gmail.com>
2024-05-06 10:44:34 +0900

    feat(NumberBaseballGame): 사용자 입력에 대한 결과 출력
    사용자 입력과 컴퓨터가 생성한 숫자를 비교하여 스트라이크와 볼의 수를 계산하고, 게임의 결과를 문자열로 반환한다.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # java-baseball-precourse
 1. random하게 숫자 3개를 생성해내는 기능 추가 (RandomNumbersGenerator)
 2. 사용자의 입력이 올바른지 확인 후, 입력을 List<Integer> 3개로 return 기능 추가 (NumberBaseballGame의 parseInput)
+3. 사용자의 입력과 맞추어야할 숫자를 비교해서 결과를 String으로 출력하는 기능 추가 (NumberBaseballGame의 checkNumbers)
--- src/main/java/NumberBaseballGame.java
+++ src/main/java/NumberBaseballGame.java
@@ -3,6 +3,12 @@ import java.util.List;
 
 public class NumberBaseballGame {
 
+    private final List<Integer> computerNumbers;
+
+    public NumberBaseballGame(List<Integer> computerNumbers) {
+        this.computerNumbers = computerNumbers;
+    }
+
     private List<Integer> parseInput(String input) throws IllegalArgumentException {
         String[] tokens = input.split("\\s+");
         if (tokens.length != 3) throw new IllegalArgumentException("Exactly three numbers are required.");
@@ -15,5 +21,19 @@ public class NumberBaseballGame {
             userNumbers.add(number);
         }
         return userNumbers;
+    }
+
+    private String checkNumbers(List<Integer> userNumbers) {
+        int strikes = 0, balls = 0;
+        for (int i = 0; i < 3; i++) {
+            int userNumber = userNumbers.get(i);
+            if (userNumber == computerNumbers.get(i)) strikes++;
+            else if (computerNumbers.contains(userNumber)) balls++;
+        }
+
+        if (strikes == 0 && balls == 0) return "낫싱";
+        else if(strikes == 0) return balls + " 볼";
+        else if(balls == 0) return strikes + "스트라이크";
+        else return balls + " 볼 " + strikes + "스트라이크";
     }
 }
