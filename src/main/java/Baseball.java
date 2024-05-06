cbf663791b98331bad71ec3ef04492fd4b8594d3
Woojae Jeong <woo6388@naver.com>
2024-05-06 10:25:28 +0900

    사용자의 입력값이 답을 맞췄는지 확인하는 기능

--- src/main/java/Baseball.java
+++ src/main/java/Baseball.java
@@ -1,17 +1,30 @@
 public class Baseball {
     //야구 게임 전반적인 흐름을 처리
-    public static void play(){
+    public static boolean play(){
         Util myutil = new Util();
-        int answer = myutil.generateAnswer(); //숫자 3자리를 생산해냄
-        int input;
+        Input input = new Input();
+        GenerateAnswer answer = new GenerateAnswer();
+
+        int gameAnswer = answer.generateAnswer(); //숫자 3자리를 생산해냄
+        int userInput; //사용자의 입력을 받는 변수
+
+
         while (true) {
             try {
-                input = myutil.getInput(); //사용자의 입력값을 받음.
-            } catch (IllegalArgumentException e){
+                userInput = input.getInput(); //사용자의 입력값을 받음.
+            } catch (IllegalArgumentException e){ //예외 발생시 잘못됐음을 출력하고 게임 종료
                 System.out.println(e.getMessage());
                 break;
             }
 
+            boolean result; //사용자가 답을 맞췄는지 여부를 담는 변수
+
+            result = myutil.process(userInput, gameAnswer);
+            if (result){ //답을 맞춘 경우에는 현재 게임 종료
+                break;
+            }
+            else
+                continue; //그렇지 않은 경우는 답을 맞추기 위해서 다시 시도
         }
     }
 }
--- src/main/java/Util.java
+++ src/main/java/Util.java
@@ -2,65 +2,36 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Util {
-    public int getInput() throws IllegalArgumentException{ //사용자의 입력을 받는 함수
-        System.out.println("숫자를 입력해 주세요");
-        int input;
-        Scanner scan = new Scanner(System.in);
+    public boolean process(int userInput, int gameAnswer){
+        int strike; //스트라이크의 갯수를 담는 변수
+        int ball; //볼의 갯수를 담는 변수
 
-        try { //사용자의 입력이 올바르지 않으면 IllegalArgumentException 던짐
-            input = scan.nextInt();
-        }catch(Exception e){
-            throw new IllegalArgumentException("잘못된 값을 입력하였습니다");
-        }
 
-        boolean isContinue = isValid(input); //사용자의 입력이 중복되지 않은 숫자들로 이루어져 있는지 판단
-        if (isContinue)
-            return input; //올바르면 input을 넘김
-        else
-            throw new IllegalArgumentException("잘못된 값을 입력하였습니다"); //그렇지 않으면 IllegalArgumentException 던지기
-    }
-
-    public boolean isValid(int input){ // 사용자의 입력이 중복되지 않은 숫자들인지 판단하는 함수
-        String str = Integer.toString(input);
-        if(str.length() != 3)
-            return false;
-        int[] arr = new int[3];
-        arr[0] =Character.getNumericValue(str.charAt(0));
-        arr[1] =Character.getNumericValue(str.charAt(1));
-        arr[2] =Character.getNumericValue(str.charAt(2));
-
-        if (arr[0] != arr[1] && arr[0] != arr[2] && arr[1] != arr[2]) //세 숫자가 모두 다른 경우는 정상
+        strike = countStrike(userInput, gameAnswer);
+        if (strike ==3)
             return true;
         else
             return false;
     }
 
-    public int generateAnswer(){ // 숫자 3자리를 만들어내는 함수
-        Random random =new Random();
-        int [] Answer = new int[3]; // 사용자가 맞춰야 하는 정답을 담는 int 배열
-        int temp;
-        Answer[0]=random.nextInt(8)+1;  // 첫 값이 0이 올 수 없으므로 값은 이렇게 지정
+    public int countStrike(int userInput, int gameAnswer){
+        int[] input = new int[3];
+        input[0] = userInput/100;
+        input[1] = userInput/10 - input[0]*100;
+        input[2] = userInput%10;
 
-        while(true) { //두번째 값은 1번째 값과 다른 값이 오게끔
-            temp = random.nextInt(9);
-            if (Answer[0] == temp)
-                continue;
-            else
-                break;
+        int[] answer = new int[3];
+        answer[0] = userInput/100;
+        answer[1] = userInput/10 - answer[0]*100;
+        answer[2] = userInput%10;
+
+        int cnt = 0;
+        for(int i = 0; i< input.length; i++){
+            if(input[i]==answer[i])
+                cnt++;
         }
-        Answer[1] = temp;
 
-        while(true) { //세번째 값은 1,2번째 값과 다른 값이 오게끔
-            temp = random.nextInt(9);
-            if (Answer[0] == temp || Answer[1] == temp)
-                continue;
-            else
-                break;
-        }
-        Answer[2] = temp;
-
-        String str = ""+Answer[0]+Answer[1]+Answer[2]; //
-        return Integer.parseInt(str);
+        return cnt;
     }
 
 }
