3aedf5d8d5891c6f2fea82e71951f4698febc88e
mingjuu <tisu79@naver.com>
2024-05-06 14:12:56 +0900

    게임 시작

---
+++ src/main/java/Application.java
@@ -0,0 +1,7 @@
+import Controller.GameController;
+public class Application {
+    public static void main(String[] args) {
+        GameController gameController = new GameController();
+        gameController.run();
+    }
+}
