1554f674c62c2b1c792ac2858ca25aa2e0f6a243
Woojae Jeong <woo6388@naver.com>
2024-05-06 09:27:27 +0900

    사용자의 입력값이 중복되지 않은 값인지 판단하는 기능

--- src/main/java/Util.java
+++ src/main/java/Util.java
@@ -7,14 +7,33 @@ public class Util {
         int input;
         Scanner scan = new Scanner(System.in);
 
-        try { //사용자의 입력이 올바르지 않으면 IllegalArgumentException을 던짐
+        try { //사용자의 입력이 올바르지 않으면 IllegalArgumentException 던짐
             input = scan.nextInt();
         }catch(Exception e){
             throw new IllegalArgumentException("잘못된 값을 입력하였습니다");
         }
-        return input;
+
+        boolean isContinue = isValid(input); //사용자의 입력이 중복되지 않은 숫자들로 이루어져 있는지 판단
+        if (isContinue)
+            return input; //올바르면 input을 넘김
+        else
+            throw new IllegalArgumentException("잘못된 값을 입력하였습니다"); //그렇지 않으면 IllegalArgumentException 던지기
     }
 
+    public boolean isValid(int input){ // 사용자의 입력이 중복되지 않은 숫자들인지 판단하는 함수
+        String str = Integer.toString(input);
+        if(str.length() != 3)
+            return false;
+        int[] arr = new int[3];
+        arr[0] =Character.getNumericValue(str.charAt(0));
+        arr[1] =Character.getNumericValue(str.charAt(1));
+        arr[2] =Character.getNumericValue(str.charAt(2));
+
+        if (arr[0] != arr[1] && arr[0] != arr[2] && arr[1] != arr[2]) //세 숫자가 모두 다른 경우는 정상
+            return true;
+        else
+            return false;
+    }
 
     public int generateAnswer(){ // 숫자 3자리를 만들어내는 함수
         Random random =new Random();
