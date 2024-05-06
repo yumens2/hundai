118428ae2df26b09315f19f2e18e0ea8d0967d4c
sseooh <sseooh@gmail.com>
2024-05-06 13:08:49 +0900

    fix: Input 기능 수정

--- src/main/java/io/Input.java
+++ src/main/java/io/Input.java
@@ -30,15 +30,6 @@ public class Input {
         do {
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요: ");
             input = scanner.nextInt();
-            // String line = scanner.nextLine();
-            // try {
-            //     input = Integer.parseInt(line);
-            //     if (input != 1 && input != 2) {
-            //         throw new IllegalArgumentException();
-            //     }
-            // } catch (IllegalArgumentException e) {
-            //     throw new IllegalArgumentException("잘못된 입력입니다. 1 또는 2를 입력하세요.");
-            // }
             scanner.nextLine();
             if (input != 1 && input != 2) {
                 throw new IllegalArgumentException("잘못된 입력입니다. 1 또는 2를 입력하세요.");
