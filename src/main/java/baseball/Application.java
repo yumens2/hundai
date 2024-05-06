d3a0975094a6968326c548f4a7df54ee1dc061f7
youngin kim <hardworkingini@gmail.com>
2024-05-06 10:06:02 +0900

    feat : 숫자 야구 게임 실행 시작 클래스 생성

---
+++ src/main/java/baseball/Application.java
@@ -0,0 +1,10 @@
+package baseball;
+
+import baseball.controller.BaseballGameController;
+
+public class Application {
+    public static void main(String[] args) {
+        BaseballGameController controller = new BaseballGameController();
+        controller.run();
+    }
+}
