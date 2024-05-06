06ce0d9bf8499aa66c81f366c0186c2302efe6bf
Shsin9797 <aaa8141@naver.com>
2024-05-06 11:05:37 +0900

    fix: Player 클래스- 게임플레이어로부터 값을 입력받는 메서드 getNum의 입력값 로직 변경

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -2,15 +2,22 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Player {
-    public int getNum(){
+    public String getNum(){
         System.out.println("숫자를 입력해 주세요 : ");
         Scanner sc= new Scanner(System.in);
         try {
-            int inputInt = sc.nextInt();
-            int length = (int)(Math.log10(inputInt)+1);
-            if (length !=3){
+            String inputInt = sc.nextLine();
+
+            if (inputInt.length() !=3){
                 throw new IllegalArgumentException();
             }
+
+            int a = inputInt.charAt(0);
+            int b = inputInt.charAt(1);
+            int c = inputInt.charAt(2);
+
+
+
             return inputInt;
         }
         catch (IllegalArgumentException e){
