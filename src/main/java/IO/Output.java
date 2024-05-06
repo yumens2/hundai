fce544844c6571b68f981bb3dfda4736bd87937f
mingjuu <tisu79@naver.com>
2024-05-06 14:26:11 +0900

    낫싱 기능 추가

--- src/main/java/IO/Output.java
+++ src/main/java/IO/Output.java
@@ -3,7 +3,7 @@ package IO;
 public class Output {
     public static void printGameResult(int strike, int ball) {
         if (strike == 0 && ball == 0) {
-            System.out.println("아웃");
+            System.out.println("낫싱");
         } else {
             if (strike > 0) {
                 System.out.print(strike + "스트라이크");
