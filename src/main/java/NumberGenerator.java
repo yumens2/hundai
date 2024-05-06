2be1446a8e15f893fb0545a99ea671e8f35c2de9
cussle <cussle@kakao.com>
2024-05-06 10:53:07 +0900

    feat(NumberGenerator): Generate three unique random numbers
    1부터 9까지의 숫자 중에서 서로 다른 3개의 숫자를 무작위로 생성합니다.
    구현 접근:
    - java.util.Random을 사용하여 1부터 9까지의 숫자를 랜덤으로 생성
    - HashSet을 이용하여 중복을 피하면서 숫자를 저장
    - 세 개의 숫자가 저장될 때까지 숫자를 생성하고 저장

---
+++ src/main/java/NumberGenerator.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+public class NumberGenerator {
+    public Set<Integer> generateRandomNumbers() {
+        Set<Integer> numbers = new HashSet<>();
+        Random random = new Random();
+        while (numbers.size() < 3) {
+            int number = 1 + random.nextInt(9);  // 1 to 9
+            numbers.add(number);
+        }
+        return numbers;
+    }
+}
