82377c469d7fad50e0d08699002d18afb5500450
donghyuun <bomul0524@gmail.com>
2024-05-06 16:41:17 +0900

    테스트 코드 수정

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,5 +1,9 @@
+import domain.Computer;
 import jdk.jfr.Description;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import utils.Utils;
+
 import static org.assertj.core.api.Assertions.*;
 
 import java.util.*;
@@ -7,26 +11,17 @@ import java.util.*;
 public class test {
 
     @Test
-    @Description("컴퓨터가 랜덤으로 겹치지 않는 3개의 숫자를 뽑아 순서대로 배열에 저장")
-    void makeRandNumArray(){
-        ArrayList<Integer> numArray = new ArrayList<>();
-        while(numArray.size() < 3){
-            Random random = new Random();
-            // 1~9 난수 생성
-            int n = random.nextInt(8) + 1;
-            // 배열에 없는 경우 숫자 추가
-            if(!numArray.contains(n)) {
-                numArray.add(n);
-            }
-        }
+    @DisplayName("컴퓨터가 랜덤으로 겹치지 않는 3개의 숫자를 뽑아 순서대로 배열에 저장")
+    void compRandNumArrayTest(){
+        ArrayList<Integer> numArray = Computer.makeRandNumArray();
         for (Integer integer : numArray) {
-            System.out.println(integer);
+            System.out.print(integer);
         }
     }
 
     @Test
-    @Description("플레이어가 겹치지 않는 3개의 숫자를 입력해 순서대로 배열에 저장")
-    void makeUserNumArray() {
+    @DisplayName("플레이어가 겹치지 않는 3개의 숫자를 입력해 순서대로 배열에 저장")
+    void userInputNumArrayTest() {
         Scanner scanner = new Scanner(System.in);
 //        String nStr = scanner.next(); // 3자리 숫자 입력
         String nStr = "243";
@@ -35,7 +30,7 @@ public class test {
         ArrayList<Integer> userNumArray = new ArrayList<>();
 
         for (char c : charArray) {
-            if(checkDuplicate(charArray, c)){ // 중복인 경우 에러 발생시킴
+            if(Utils.checkDuplicate(charArray, c)){ // 중복인 경우 에러 발생시킴
                 throw new IllegalArgumentException();
             }
             else{
@@ -44,53 +39,30 @@ public class test {
         }
 
         for (Integer integer : userNumArray) {
-            System.out.println(integer);
+            System.out.print(integer);
         }
-//        return userNumArray;
     }
-
-    @Description("플레이어가 입력한 숫자(현 배열)에서 중복된 문자가 있는지 판별")
-    private boolean checkDuplicate(char[] charArray, char c) {
-        int cnt = 0;
-        for (char c1 : charArray) {
-            if(c1 == c){cnt++;}
-        }
-        if(cnt > 1){ return true;}
-        else{return false;}
+    @Test
+    @DisplayName("플레이어가 입력한 숫자(현 배열)에서 중복된 문자가 있는지 판별")
+    void checkDuplicateTest() {
+        boolean b1 = Utils.checkDuplicate(new char[]{'1', '3', '4'}, '1');
+        assertThat(b1).isEqualTo(false);
+        boolean b2 = Utils.checkDuplicate(new char[]{'1', '3', '3'}, '3');
+        assertThat(b2).isEqualTo(true);
     }
 
     @Test
-    @Description("플레이어가 입력한 숫자에 대한 결과 판별")
+    @DisplayName("플레이어가 입력한 숫자에 대한 결과 판별")
     void showResult() {
         ArrayList<Integer> userNumArray = new ArrayList<>(List.of(1, 2, 3));
         ArrayList<Integer> randNumArray = new ArrayList<>(List.of(2, 1, 3));
 
         // 1. 스트라이크 개수 정상 확인
-        assertThat(countStrike(userNumArray, randNumArray)).isEqualTo(1) ;
+        int strikeNum = Utils.countStrike(userNumArray, randNumArray);
+        assertThat(strikeNum).isEqualTo(1);
         // 2. 볼 개수 정상 확인
-        assertThat(countBall(userNumArray, randNumArray)).isEqualTo(2) ;
-    }
-
-    @Description("볼 개수 계산")
-    private static int countBall(ArrayList<Integer> userNumArray, ArrayList<Integer> randNumArray) {
-        int ballCnt = 0;
-        for(int i = 0; i < userNumArray.size(); i++){
-            if(randNumArray.contains(userNumArray.get(i)) && !Objects.equals(randNumArray.get(i), userNumArray.get(i))){
-                ballCnt++;
-            }
-        }
-        return ballCnt;
-    }
-
-    @Description("스트라이크 개수 계산")
-    private static int countStrike(ArrayList<Integer> userNumArray, ArrayList<Integer> randNumArray) {
-        int strikeCnt = 0;
-        for(int i = 0; i < userNumArray.size(); i++){
-            if(Objects.equals(userNumArray.get(i), randNumArray.get(i))){
-                strikeCnt++;
-            }
-        }
-        return strikeCnt;
+        int ballNum = Utils.countBall(userNumArray, randNumArray);
+        assertThat(ballNum).isEqualTo(2) ;
     }
 
 }
