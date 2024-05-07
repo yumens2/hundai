579d2dd1afe19125d2437cff3ff6a8cdcabcb4e1
capDoYeonLee <ghtyu21@naver.com>
2024-05-07 21:00:14 +0900

    fix: 불필요한 메서드 삭제

--- src/main/java/ValidBall.java
+++ src/main/java/ValidBall.java
@@ -3,18 +3,9 @@ import java.util.HashSet;
 public class ValidBall {
 
     public void validate(int answer) {
-        validateInputType(answer);
         validateRange(answer);
         validateDuplication(answer);
     }
-
-    // 아래 validateInputTpye 메서드가 의미가 있을까? MajorLeague에서 결국 인풋 받을 때 Integer.parseInt로 파싱하는데 문자열이 입력되면
-    //결국 거기서 에러가 발생할텐데, 그럼 이건 테스트에 넣자
-//    private <T> void validateInputType(T answer) {
-//        if (!(answer instanceof Integer)) {
-//            throw new IllegalArgumentException("잘못된 입력입니다. 다시 시도하세요.");
-//        }
-//    }
 
     private void validateDuplication(int answer) {
         HashSet<Integer> set = new HashSet<>();
