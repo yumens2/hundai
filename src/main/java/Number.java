ae13935aa64799ed4cb5a39b9c34908ad4b6c493
hyyyh0x <hyyyh0x@gmail.com>
2024-05-09 21:05:18 +0900

    feat : depth reduced & test added

--- src/main/java/Number.java
+++ src/main/java/Number.java
@@ -1,79 +1,80 @@
+import java.util.Random;
 import java.util.Scanner;
+
 public class Number{
     boolean FLAG;
+    String USER_NUMBER;
     int[] RANDOM_NUM=new int[3];
-    int MY_NUM;
+    int[] MY_NUM=new int[3];
     int BALL;
     int STRIKE;
     public Number() {
         FLAG=true;
         BALL=0; STRIKE=0;
         RANDOM_NUM[0]=(int)(10*Math.random());
-        while(true){
+        do{
             RANDOM_NUM[1]=(int)(10*Math.random());
-            if(RANDOM_NUM[0]!=RANDOM_NUM[1]) break;
-        }
-        while(true){
+        }while(RANDOM_NUM[0]==RANDOM_NUM[1]);
+        do{
             RANDOM_NUM[2]=(int)(10*Math.random());
-            if(RANDOM_NUM[0]!=RANDOM_NUM[2]&&RANDOM_NUM[1]!=RANDOM_NUM[2]) break;
-        }
+        }while(RANDOM_NUM[0]==RANDOM_NUM[2]||RANDOM_NUM[1]==RANDOM_NUM[2]);
+        /*System.out.println(RANDOM_NUM[0]);
+        System.out.println(RANDOM_NUM[1]);
+        System.out.println(RANDOM_NUM[2]);*/
+    }
+    public void inputNum(){
+        System.out.print("숫자를 입력해 주세요 : ");
+        Scanner sc=new Scanner(System.in);
+        USER_NUMBER=sc.next();
     }
     public void getNum() throws Exception{
         BALL=0; STRIKE=0;
-        System.out.print("숫자를 입력해 주세요 : ");
-        Scanner sc=new Scanner(System.in);
-        String S=sc.next();
-        if(S.length()!= 3) {
-            FLAG=false;
+        inputNum();
+        if(USER_NUMBER.length()!= 3) {
             throw new Exception("IllegalArgumentException");
         }
-        else {
-            if(!isNumeric(S)) {
-                FLAG=false;
-                throw new Exception("IllegalArgumentException");
-            }
-            else {
-                int TEMP = Integer.parseInt(S);
-                isNumOk(TEMP);
-            }
+        if(!isNumeric(USER_NUMBER)) {
+            throw new Exception("IllegalArgumentException");
         }
+        int TEMP = Integer.parseInt(USER_NUMBER);
+        isNumOk(TEMP);
     }
     private static boolean isNumeric(String str){
         return str != null && str.matches("[0-9.]+");
     }
     public void isNumOk(int NUM) throws Exception{
         if(NUM<0||NUM>999){
-            FLAG=false;
             throw new Exception("IllegalArgumentException");
         }
-        else MY_NUM=NUM;
+        int GET_NUM=NUM;
+        MY_NUM[0]=GET_NUM/100;
+        GET_NUM%=100;
+        MY_NUM[1]=GET_NUM/10;
+        GET_NUM%=10;
+        MY_NUM[2]=GET_NUM;
+        if(MY_NUM[0]==MY_NUM[1]||MY_NUM[0]==MY_NUM[2]||MY_NUM[1]==MY_NUM[2])
+            throw new Exception("IllegalArgumentException");
     }
     public void compareNum(){
-        if(!FLAG)
-            return;
-        int TMP=MY_NUM;
-        int[] A=new int[3];
-        A[0]=TMP/100;
-        TMP%=100;
-        A[1]=TMP/10;
-        TMP%=10;
-        A[2]=TMP;
-        for(int i=0;i<3;i++){
-            for(int j=0;j<3;j++){
-                isEqualNum(i,j,A[j],RANDOM_NUM[i]);
-            }
+        if(!FLAG) return;
+        int[] COUNT_INDEXES_I= {0,0,0,1,1,1,2,2,2};
+        int[] COUNT_INDEXES_J= {0,1,2,0,1,2,0,1,2};
+        for(int i=0;i<9;i++){
+            int x=COUNT_INDEXES_I[i];
+            int y=COUNT_INDEXES_J[i];
+//            System.out.println(MY_NUM[y]);
+//            System.out.println(RANDOM_NUM[x]);
+            isEqualNum(x,y,MY_NUM[y],RANDOM_NUM[x]);
         }
         printWhat();
     }
     public void isEqualNum(int i, int j, int a,int r){
-        if(a==r){
-            if(i==j){
-                STRIKE++;
-            }
-            else{
-                BALL++;
-            }
+        if(a!=r) return;
+        if(i==j){
+            STRIKE++;
+            return;
         }
+        BALL++;
     }
     public void printWhat(){
         if(BALL==0&&STRIKE==0)
@@ -104,12 +105,11 @@ public class Number{
         Scanner sc=new Scanner(System.in);
         String S=sc.next();
         int GET_NUM=Integer.parseInt(S);
-        if(GET_NUM<1||GET_NUM>2) {
-            FLAG = false;
-            throw new Exception("IllegalArgumentException");
-        }
-        else if(GET_NUM==2){
+        if(GET_NUM==1) return;
+        if(GET_NUM==2){
             FLAG=false;
+            return;
         }
+        throw new Exception("IllegalArgumentException");
     }
 }
---
+++ src/test/java/NumberTest.java
@@ -0,0 +1,121 @@
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class NumberTest {
+    public final ByteArrayOutputStream output=new ByteArrayOutputStream();
+    Number number = new Number();
+    protected void systemIn(String input) {
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+    }
+    @BeforeEach
+    void setUpOutputStream() {
+        System.setOut(new PrintStream(output));
+    }
+    @AfterEach
+    void restoreOutputStream() {
+        System.setOut(System.out);
+        output.reset();
+    }
+    @Test
+    @DisplayName("길이 오류 체크.")
+    void getNum_lengthError(){
+        Assertions.assertThrows(java.lang.Exception.class, () -> {
+            systemIn("1234");
+            number.getNum();
+        });
+        Assertions.assertThrows(java.lang.Exception.class, () -> {
+            systemIn("12");
+            number.getNum();
+        });
+    }
+    @Test
+    @DisplayName("숫자가 아닌 값 입력시 오류.")
+    void getNum_numericError(){
+        Assertions.assertThrows(java.lang.Exception.class, () -> {
+            systemIn("aaaa");
+            number.getNum();
+        });
+        Assertions.assertThrows(java.lang.Exception.class, () -> {
+            systemIn("1a");
+            number.getNum();
+        });
+    }
+    @Test
+    @DisplayName("같은 값 입력 시 오류")
+    void getNum_notValid(){
+        Assertions.assertThrows(java.lang.Exception.class, () -> {
+            systemIn("990");
+            number.getNum();
+        });
+    }
+    @Test
+    @DisplayName("게임종료 여부 입력 시 오류")
+    void finishedGameError(){
+        Assertions.assertThrows(java.lang.Exception.class, () -> {
+            systemIn("3");
+            number.finishedGame();
+        });
+    }
+    @Test
+    @DisplayName("입력값 비교 후 출력 값 확인")
+    void printWhatSame(){
+        number.STRIKE=0;
+        number.BALL=2;
+        number.printWhat();
+        assertThat(output.toString().trim())
+            .isEqualTo("2볼");
+        restoreOutputStream();
+        number.STRIKE=1;
+        number.BALL=1;
+        number.printWhat();
+        assertThat(output.toString().trim())
+            .isEqualTo("1볼 1스트라이크");
+        restoreOutputStream();
+        number.STRIKE=0;
+        number.BALL=0;
+        number.printWhat();
+        assertThat(output.toString().trim())
+            .isEqualTo("낫싱");
+        restoreOutputStream();
+        number.STRIKE=2;
+        number.BALL=0;
+        number.printWhat();
+        assertThat(output.toString().trim())
+            .isEqualTo("2스트라이크");
+    }
+    @Test
+    @DisplayName("숫자가 같은지 확인")
+    void compareNumCorrect(){
+        number.MY_NUM[0]=1;
+        number.MY_NUM[1]=2;
+        number.MY_NUM[2]=3;
+        number.RANDOM_NUM[0]=1;
+        number.RANDOM_NUM[1]=2;
+        number.RANDOM_NUM[2]=3;
+        number.compareNum();
+        assertThat(number.STRIKE).isEqualTo(3);
+        assertThat(number.BALL).isEqualTo(0);
+        number.STRIKE=0; number.BALL=0;
+        number.RANDOM_NUM[0]=2;
+        number.RANDOM_NUM[1]=4;
+        number.RANDOM_NUM[2]=1;
+        number.compareNum();
+        assertThat(number.BALL).isEqualTo(2);
+        assertThat(number.STRIKE).isEqualTo(0);
+        number.STRIKE=0; number.BALL=0;
+        number.RANDOM_NUM[0]=1;
+        number.RANDOM_NUM[1]=4;
+        number.RANDOM_NUM[2]=2;
+        number.compareNum();
+        assertThat(number.BALL).isEqualTo(1);
+        assertThat(number.STRIKE).isEqualTo(1);
+    }
+}
